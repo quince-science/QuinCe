38b5af3d08091e3e2f1824ea4ce4bef8023e3925
Steve Jones <steve@squaregoldfish.co.uk>
2015-12-24 07:02:09 -0500

    User can change password.

--- WebApp/src/uk/ac/exeter/QuinCe/web/User/UserProfileBean.java
+++ WebApp/src/uk/ac/exeter/QuinCe/web/User/UserProfileBean.java
@@ -1,13 +1,26 @@
 package uk.ac.exeter.QuinCe.web.User;
 
+import com.mysql.jdbc.StringUtils;
+
+import uk.ac.exeter.QuinCe.database.User.UserDB;
 import uk.ac.exeter.QuinCe.web.BaseManagedBean;
+import uk.ac.exeter.QuinCe.web.system.ServletUtils;
 
 public class UserProfileBean extends BaseManagedBean {
 
+	static {
+		FORM_NAME = "userProfileForm";
+	}
+	
 	/**
 	 * The navigation string for the file list page
 	 */
 	private static final String PAGE_FILE_LIST = "file_list";
+	
+	/**
+	 * The navigation string to return to the profile editor
+	 */
+	private static final String PAGE_EDIT_PROFILE = "edit_profile";
 	
 	/**
 	 * The user's current password - required to make changes
@@ -25,6 +38,28 @@ public class UserProfileBean extends BaseManagedBean {
 	private String newPassword2 = null;
 	
 	/////////// *** METHODS **** ///////////////
+	
+	public String saveProfile() {
+		
+		String result = PAGE_FILE_LIST;
+		
+		try {
+			// Changing the user's password automatically authenticates
+			if (!StringUtils.isEmptyOrWhitespaceOnly(newPassword1) || !StringUtils.isEmptyOrWhitespaceOnly(newPassword2)) {
+				if (!newPassword1.equals(newPassword2)) {
+					setMessage(getComponentID("newPassword2"), "The entered passwords do not match");
+					result = PAGE_EDIT_PROFILE;
+				} else if (!UserDB.changePassword(ServletUtils.getDBDataSource(), getUser(), currentPassword.toCharArray(), newPassword1.toCharArray())) {
+					setMessage(getComponentID("currentPassword"), "You did not enter the correct password");
+					result = PAGE_EDIT_PROFILE;
+				}
+			}
+		} catch (Exception e) { 			 
+			return internalError(e);
+		}
+			
+		return result;
+	}
 	
 	/**
 	 * Cancels editing of the user profile
@@ -80,7 +115,7 @@ public class UserProfileBean extends BaseManagedBean {
 	 * Sets the user's second new password entry
 	 * @param newPassword2 The user's second new password entry
 	 */
-	public void getNewPassword2(String newPassword2) {
+	public void setNewPassword2(String newPassword2) {
 		this.newPassword2 = newPassword2;
 	}
 }
