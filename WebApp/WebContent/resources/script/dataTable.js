b863605dbc95aaf848b9821ba4774d43ddd92d0b
Steve Jones <steve@squaregoldfish.co.uk>
2016-06-09 11:30:13 +0200

    Documentation

--- WebApp/WebContent/resources/script/dataTable.js
+++ WebApp/WebContent/resources/script/dataTable.js
@@ -1,8 +1,11 @@
-var DATA_TABLE_MIN_HEIGHT = 250;
-
+// Keeps track of the split positions as a percentage of the
+// full data area
 var tableSplitPercent = 0;
 var plotSplitPercent = 0;
 
+/*
+ * Initialises the splits, and adds handler for window resizing
+ */
 $(function() {
 	$('#dataScreenContent').split({orientation: 'horizontal', onDragEnd: function(){resizeContent()} });
 	$('#plots').split({orientation: 'vertical', onDragEnd: function(){resizeContent()} });
@@ -14,11 +17,17 @@ $(function() {
 	});
 });
 
+/*
+ * Handles the resizing of individual panels
+ */
 function resizeContent() {
 	tableSplitPercent = '' + $('#dataScreenContent').split().position() / $('#dataScreenContent').height() * 100 + '%';
 	plotSplitPercent = '' + $('#plots').split().position() / $('#dataScreenContent').width() * 100 + '%';
 }
 
+/*
+ * Proportionally resizes the splits when the window is resized
+ */
 function scaleSplits() {
 	$('#dataScreenContent').split().position(tableSplitPercent);
 	$('#plots').split().position(plotSplitPercent);
