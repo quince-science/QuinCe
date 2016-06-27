f44f6e4829c2f4d999c0939a53360d05b0db928f
Steve Jones <steve@squaregoldfish.co.uk>
2016-06-27 12:20:11 +0200

    Removed unused import

--- WebApp/src/uk/ac/exeter/QuinCe/data/ExportConfig.java
+++ WebApp/src/uk/ac/exeter/QuinCe/data/ExportConfig.java
@@ -8,8 +8,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
-import org.primefaces.component.separator.SeparatorRenderer;
-
 import uk.ac.exeter.QuinCe.utils.StringUtils;
 
 /**
