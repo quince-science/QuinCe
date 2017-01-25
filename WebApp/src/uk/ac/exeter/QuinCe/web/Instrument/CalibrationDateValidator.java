f1c8f94b70aaaa20c9de4f6e041194c3104c1fee
Steve Jones <steve@squaregoldfish.co.uk>
2017-01-25 15:18:33 -0100

    Javadoc for CalibrationDateValidator

--- WebApp/src/uk/ac/exeter/QuinCe/web/Instrument/CalibrationDateValidator.java
+++ WebApp/src/uk/ac/exeter/QuinCe/web/Instrument/CalibrationDateValidator.java
@@ -1,17 +1,33 @@
 package uk.ac.exeter.QuinCe.web.Instrument;
 
+/**
+ * An validation class to check that a sensor calibration date does not
+ * clash with an existing calibration. 
+ * 
+ * @author Steve Jones
+ *
+ */
 public class CalibrationDateValidator extends InstrumentDateValidator {
 	
+	/**
+	 * Specifies that the {@code sensor_calibration} table should be searched for clashing dates.
+	 */
 	@Override
 	public String getTable() {
 		return "sensor_calibration";
 	}
 
+	/**
+	 * Specifies that the {@code calibration_date} field should be searched for clashing dates. 
+	 */
 	@Override
 	public String getField() {
 		return "calibration_date";
 	}
-	
+
+	/**
+	 * Returns the message to be displayed if the calibration date is already in use.
+	 */
 	@Override
 	public String getErrorMessage() {
 		return "A calibration with the specified date already exists";
