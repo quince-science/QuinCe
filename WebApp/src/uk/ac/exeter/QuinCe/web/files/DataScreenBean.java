c0db90a0fdf01f555b91ff74c6c9cac87c62891f
Steve Jones <steve@squaregoldfish.co.uk>
2017-02-08 00:04:59 +0100

    Javadoc DataScreenBean

--- WebApp/src/uk/ac/exeter/QuinCe/web/files/DataScreenBean.java
+++ WebApp/src/uk/ac/exeter/QuinCe/web/files/DataScreenBean.java
@@ -21,6 +21,12 @@ import uk.ac.exeter.QuinCe.web.BaseManagedBean;
 import uk.ac.exeter.QuinCe.web.system.ResourceException;
 import uk.ac.exeter.QuinCe.web.system.ServletUtils;
 
+/**
+ * Managed bean to handle data for the main QC screen
+ * 
+ * @author Steve Jones
+ *
+ */
 public class DataScreenBean extends BaseManagedBean {
 
 	static {
@@ -29,61 +35,181 @@ public class DataScreenBean extends BaseManagedBean {
 
 	public static final String CURRENT_FILE_SESSION_ATTRIBUTE = "currentFile";
 	
+	/**
+	 * Navigation result to display the QC screen
+	 */
 	public static final String PAGE_START = "data_screen";
 	
+	/**
+	 * Navigation result to display the file list
+	 */
 	public static final String PAGE_END = "file_list";
 	
+	/**
+	 * Indicator for HTML controls for the plot configuration popup
+	 */
 	private static final String POPUP_PLOT = "plot";
 	
+	/**
+	 * Indicator for HTML controls for the map configuration popup
+	 */
 	private static final String POPUP_MAP = "map";
 	
+	/**
+	 * The database ID of the data file being QCed
+	 */
 	private long fileId;
 	
+	/**
+	 * The details of the file being edited
+	 */
 	private FileInfo fileDetails = null;
 	
+	/**
+	 * The data columns being used in the left plot.
+	 * 
+	 * <p>
+	 *   The columns are stored as a semi-colon separated list. The first
+	 *   column is the X axis, and all subsequent columns will be displayed on the Y axis.
+	 *   The Javascript on the user interface will be responsible for ensuring that
+	 *   the columns are set appropriately; the bean will not perform any checks.
+	 * </p>
+	 */
 	private String leftPlotColumns = null;
 	
+	/**
+	 * The data for the left plot.
+	 * 
+	 * <p>
+	 *   The data is stored as a JSON string, which will be parsed by the Javascript on
+	 *   the user interface. The data is retrieved by the {@link #getPlotData(List)} method.
+	 * </p>
+	 */
 	private String leftPlotData = null;
 	
+	/**
+	 * The data columns being used in the right plot.
+	 * 
+	 * <p>
+	 *   The columns are stored as a semi-colon separated list. The first
+	 *   column is the X axis, and all subsequent columns will be displayed on the Y axis.
+	 *   The Javascript on the user interface will be responsible for ensuring that
+	 *   the columns are set appropriately; the bean will not perform any checks.
+	 * </p>
+	 */
 	private String rightPlotColumns = null;
 	
+	/**
+	 * The data for the right plot.
+	 *
+	 * <p>
+	 *   The data is stored as a JSON string, which will be parsed by the Javascript on
+	 *   the user interface. The data is retrieved by the {@link #getPlotData(List)} method.
+	 * </p>
+	 */
 	private String rightPlotData = null;
 	
+	/**
+	 * The type of CO<sub>2</sub> measurements being viewed.
+	 * Can only be one of {@link RunType#RUN_TYPE_WATER} or {@link RunType#RUN_TYPE_ATMOSPHERIC}.  
+	 */
 	private int co2Type = RunType.RUN_TYPE_WATER;
 	
+	/**
+	 * Indicates that records with the specified flags will also be included in the plot/map.
+	 * 
+	 * <p>
+	 * 	By default, only records with the flags:
+	 * </p>
+	 * <ul>
+	 *   <li>{@link Flag#VALUE_GOOD}</li>
+	 *   <li>{@link Flag#VALUE_ASSUMED_GOOD}</li>
+	 *   <li>{@link Flag#VALUE_QUESTIONABLE}</li>
+	 *   <li>{@link Flag#VALUE_NEEDED}</li>
+	 * </ul>
+	 * 
+	 * <p>
+	 *   are displayed in plots and maps. Records with
+	 *   other flags can be included if they are added
+	 *   to this field.
+	 * </p>
+	 */
 	private List<String> optionalFlags = null;
 	
+	/**
+	 * The current table mode, which indicates which columns are to be displayed
+	 */
 	private String tableMode = "basic";
 	
+	/**
+	 * The data for the table.
+	 * 
+	 * <p>
+	 *   The table data is loaded dynamically from the server in chunks,
+	 *   so the entire data set does not need to be loaded in one go. This
+	 *   field will only contain the data for the currently visible chunk.
+	 * </p>
+	 */
 	private String tableData = null;
 	
+	/**
+	 * The list of table rows that are currently selected.
+	 * 
+	 * <p>
+	 *   The list of rows is stored as a comma-separated list of row numbers from the data file.
+	 * </p>
+	 */
 	private String selectedRows = null;
 	
+	/**
+	 * The WOCE comment entered by the user. This will be applied to the selected records
+	 * when {@link #applyWoceFlag()} is called.
+	 */
 	private String woceComment = null;
 	
+	/**
+	 * The WOCE flag selected by the user. This will be applied to the selected records
+	 * when {@link #applyWoceFlag()} is called.
+	 */
 	private int woceFlag = Flag.VALUE_NEEDED;
 	
+	/**
+	 * The instrument that recorded the current data file
+	 */
 	Instrument instrument;
 	
 	/**
-	 * Required basic constructor. All the actual construction
-	 * is done in start().
+	 * Required basic constructor. This does nothing: all the actual construction
+	 * is done in {@link #start()}.
 	 */
 	public DataScreenBean() {
 		// Do nothing
 	}
 
+	/**
+	 * Initialises the bean with the details of the selected data file.
+	 * Any data from previous data files is removed first.
+	 * @return The navigation to the QC screen
+	 * @throws Exception If any errors occur
+	 */
 	public String start() throws Exception {
 		clearData();
 		loadFileDetails();
 		return PAGE_START;
 	}
 	
+	/**
+	 * Clears all file data when QC for a data file is finished.
+	 * @return The navigation to the data file list
+	 */
 	public String end() {
 		clearData();
 		return PAGE_END;
 	}
 	
+	/**
+	 * Clears all data regarding the current data file from the bean
+	 */
 	private void clearData() {
 		fileDetails = null;
 		leftPlotColumns = null;
@@ -94,112 +220,239 @@ public class DataScreenBean extends BaseManagedBean {
 		tableData = null;
 	}
 	
+	/**
+	 * Returns the database ID of the current data file
+	 * @return The database ID of the data file
+	 */
 	public long getFileId() {
 		return fileId;
 	}
 	
+	/**
+	 * Set the database ID of the current data file
+	 * @param fileId The database ID of the data file
+	 */
 	public void setFileId(long fileId) {
 		this.fileId = fileId;
 	}
 	
+	/**
+	 * Get the file details of the current data file
+	 * @return The file details
+	 */
 	public FileInfo getFileDetails() {
 		return fileDetails;
 	}
 	
+	/**
+	 * Get the columns to be displayed in the left plot. See {@link #leftPlotColumns}.
+	 * @return The columns for the left plot
+	 */
 	public String getLeftPlotColumns() {
 		return leftPlotColumns;
 	}
 	
+	/**
+	 * Set the columns to be displayed in the left plot. See {@link #leftPlotColumns}.
+	 * @param leftPlotColumns The columns for the left plot
+	 */
 	public void setLeftPlotColumns(String leftPlotColumns) {
 		this.leftPlotColumns = leftPlotColumns;
 	}
 	
+	/**
+	 * Get the data for the left plot. See {@link #leftPlotData}.
+	 * @return The data for the left plot
+	 */
 	public String getLeftPlotData() {
 		return leftPlotData;
 	}
 	
+	/**
+	 * Set the data for the left plot. See {@link #leftPlotData}.
+	 * @param leftPlotData The data for the left plot
+	 */
 	public void setLeftPlotData(String leftPlotData) {
 		this.leftPlotData = leftPlotData;
 	}
 	
+	/**
+	 * Get the columns to be displayed in the right plot. See {@link #rightPlotColumns}.
+	 * @return The columns for the right plot
+	 */
 	public String getRightPlotColumns() {
 		return rightPlotColumns;
 	}
 	
+	/**
+	 * Set the columns to be displayed in the right plot. See {@link #rightPlotColumns}.
+	 * @param rightPlotColumns The columns for the right plot
+	 */
 	public void setRightPlotColumns(String rightPlotColumns) {
 		this.rightPlotColumns = rightPlotColumns;
 	}
 	
+	/**
+	 * Get the data for the right plot. See {@link #rightPlotData}.
+	 * @return The data for the right plot
+	 */
 	public String getRightPlotData() {
 		return rightPlotData;
 	}
 	
+	/**
+	 * Set the data for the right plot. See {@link #rightPlotData}.
+	 * @param rightPlotData The data for the right plot
+	 */
 	public void setRightPlotData(String rightPlotData) {
 		this.rightPlotData = rightPlotData;
 	}
 	
+	/**
+	 * Get the type of CO<sub>2</sub> measurement being displayed.
+	 * Will be one of {@link RunType#RUN_TYPE_WATER} or {@link RunType#RUN_TYPE_ATMOSPHERIC}.
+	 * @return The type of measurement being displayed
+	 */
 	public int getCo2Type() {
 		return co2Type;
 	}
 	
+	/**
+	 * Get the type of CO<sub>2</sub> measurement to be displayed.
+	 * Must be one of {@link RunType#RUN_TYPE_WATER} or {@link RunType#RUN_TYPE_ATMOSPHERIC}.
+	 * The behaviour of the user interface is undefined if this is set to anything else.
+	 * 
+	 * @param co2Type The type of measurement
+	 */
 	public void setCo2Type(int co2Type) {
 		this.co2Type = co2Type;
 	}
 	
+	/**
+	 * Get the list of flags for display of records in addition to the default set.
+	 * See {@link #optionalFlags}.
+	 *  
+	 * @return The list of additional flags
+	 */
 	public List<String> getOptionalFlags() {
 		return optionalFlags;
 	}
 	
+	/**
+	 * Set the list of flags for display of records in addition to the default set.
+	 * See {@link #optionalFlags}.
+	 * @param optionalFlags The list of additional flags
+	 */
 	public void setOptionalFlags(List<String> optionalFlags) {
 		this.optionalFlags = optionalFlags;
 	}
 	
+	/**
+	 * Get the table display mode. This determines which columns are displayed in the table.
+	 * @return The table display mode.
+	 */
 	public String getTableMode() {
 		return tableMode;
 	}
 	
+	/**
+	 * Set the table display mode. This determines which columns are displayed in the table.
+	 * @param tableMode The table display mode
+	 */
 	public void setTableMode(String tableMode) {
 		this.tableMode = tableMode;
 	}
 	
+	/**
+	 * Get the currently loaded data for the table. See {@link #tableData}.
+	 * @return The table data
+	 */
 	public String getTableData() {
 		return tableData;
 	}
 	
+	/**
+	 * Set the currently loaded data for the table. See {@link #tableData}.
+	 * @param tableData The table data
+	 */
 	public void setTableData(String tableData) {
 		this.tableData = tableData;
 	}
 	
+	/**
+	 * Get the set of selected table rows. See {@link #selectedRows}.
+	 * @return The selected rows.
+	 */
 	public String getSelectedRows() {
 		return selectedRows;
 	}
 	
+	/**
+	 * Set the selected table rows. See {@link #selectedRows}.
+	 * @param selectedRows The selected rows
+	 */
 	public void setSelectedRows(String selectedRows) {
 		this.selectedRows = selectedRows;
 	}
 	
+	/**
+	 * Get the WOCE comment entered by the user.
+	 * @return The WOCE comment
+	 */
 	public String getWoceComment() {
 		return woceComment;
 	}
 	
+	/**
+	 * Record the WOCE comment entered by the user
+	 * @param woceComment The WOCE comment
+	 */
 	public void setWoceComment(String woceComment) {
 		this.woceComment = woceComment;
 	}
 	
+	/**
+	 * Get the WOCE flag selected by the user
+	 * @return The WOCE flag
+	 */
 	public int getWoceFlag() {
 		return woceFlag;
 	}
 	
+	/**
+	 * Record the WOCE flag selected by the user
+	 * @param woceFlag The WOCE flag
+	 */
 	public void setWoceFlag(int woceFlag) {
 		this.woceFlag = woceFlag;
 	}
 	
+	/**
+	 * Load details of the selected data file into the bean.
+	 * 
+	 * This only loads details used for referencing the data file and its
+	 * general details; the actual data for the plots and table will be loaded
+	 * dynamically at a later stage.
+	 * 
+	 * @throws MissingParamException If any parameters to the underlying data retrieval calls are missing
+	 * @throws DatabaseException If a database error occurs
+	 * @throws ResourceException If the application resources cannot be accessed
+	 * @throws RecordNotFoundException If the selected data file (or any of its related records) cannot be found
+	 */
 	private void loadFileDetails() throws MissingParamException, DatabaseException, ResourceException, RecordNotFoundException {
 		fileDetails = DataFileDB.getFileDetails(ServletUtils.getDBDataSource(), fileId);
 		DataFileDB.touchFile(ServletUtils.getDBDataSource(), fileId);
 		instrument = InstrumentDB.getInstrumentByFileId(ServletUtils.getDBDataSource(), fileId);
 	}
 	
+	/**
+	 * Generate the check boxes to select columns for the data plots.
+	 * @return The HTML for the check boxes
+	 * @throws MissingParamException If any parameters for underlying data retrieval calls are missing
+	 * @throws DatabaseException If a database error occurs
+	 * @throws RecordNotFoundException If any required database records are mising
+	 * @throws ResourceException If the application resources cannot be accessed
+	 */
 	public String getPlotPopupEntries() throws MissingParamException, DatabaseException, RecordNotFoundException, ResourceException {
 		
 		Instrument instrument = InstrumentDB.getInstrument(ServletUtils.getDBDataSource(), fileDetails.getInstrumentId());
@@ -417,14 +670,36 @@ public class DataScreenBean extends BaseManagedBean {
 		return output.toString();
 	}
 	
+	/**
+	 * Generate the HTML for a checkbox in the plot column selection popup
+	 * @param group The group that will contain the checkbox
+	 * @param field The name of the column
+	 * @param label The label for the checkbox
+	 * @return The checkbox HTML
+	 */
 	private String makePlotCheckbox(String group, String field, String label) {
 		return makeCheckbox(POPUP_PLOT, group, field, label);
 	}
 	
+	/**
+	 * Generate the HTML for a checkbox in the map column selection popup
+	 * @param group The group that will contain the checkbox
+	 * @param field The name of the column
+	 * @param label The label for the checkbox
+	 * @return The checkbox HTML
+	 */
 	private String makeMapCheckbox(String group, String field, String label) {
 		return makeCheckbox(POPUP_MAP, group, field, label);
 	}
 	
+	/**
+	 * Generate the HTML for a column selection checkbox
+	 * @param popupType Either {@link #POPUP_PLOT} or {@link #POPUP_MAP}
+	 * @param group The group that will contain the checkbox
+	 * @param field The name of the column
+	 * @param label The label for the checkbox
+	 * @return The checkbox HTML
+	 */
 	private String makeCheckbox(String popupType, String group, String field, String label) {
 
 		String inputID = popupType + "_" + group + "_" + field;
@@ -443,16 +718,27 @@ public class DataScreenBean extends BaseManagedBean {
 		return checkbox.toString();
 	}
 	
+	/**
+	 * Generate the data for the left plot. See {@link #getPlotData(List)}.
+	 */
 	public void generateLeftPlotData() {
 		List<String> columns = StringUtils.delimitedToList(leftPlotColumns);
 		setLeftPlotData(getPlotData(columns)); 
 	}
 
+	/**
+	 * Generate the data for the right plot. See {@link #getPlotData(List)}.
+	 */
 	public void generateRightPlotData() {
 		List<String> columns = StringUtils.delimitedToList(rightPlotColumns);
 		setRightPlotData(getPlotData(columns)); 
 	}
 	
+	/**
+	 * Retrieve the data for a plot from the database as a JSON string.
+	 * @param columns The list of columns for the plot. The first column will be for the X axis, and the subsequent columns will be display on the Y axis.
+	 * @return The plot data
+	 */
 	private String getPlotData(List<String> columns) {
 		
 		String output;
@@ -486,6 +772,10 @@ public class DataScreenBean extends BaseManagedBean {
 		return output;
 	}
 
+	/**
+	 * Retrieve the data for the table from the database as a JSON string.
+	 * The data is stored in {@link #tableData}.
+	 */
 	public void generateTableData() {
 
 		try {
@@ -586,6 +876,10 @@ public class DataScreenBean extends BaseManagedBean {
 		}
 	}
 	
+	/**
+	 * Retrieve the list of column headings for the data table. The result is a JSON string representing a Javascript array.
+	 * @return The list of column headings
+	 */
 	public String getTableHeadings() {
 
 		StringBuffer output = new StringBuffer('[');
@@ -688,6 +982,11 @@ public class DataScreenBean extends BaseManagedBean {
 		return output.toString();
 	}
 
+	/**
+	 * Generate the list of WOCE flags that will be used to select records to be displayed on the data screen.
+	 * Includes the default set of flags plus any other set in {@link #optionalFlags}.
+	 * @return The list of flags
+	 */
 	private List<Integer> getIncludeFlags() {
 		List<Integer> includeFlags = new ArrayList<Integer>();
 		includeFlags.add(Flag.VALUE_GOOD);
@@ -704,10 +1003,17 @@ public class DataScreenBean extends BaseManagedBean {
 		return includeFlags;
 	}
 	
+	/**
+	 * Retrieve the details of the instrument for the current data file
+	 * @return The instrument details
+	 */
 	public Instrument getInstrument() {
 		return instrument;
 	}
 	
+	/**
+	 * Apply the automatically generated QC flags to the rows selected in the table
+	 */
 	public void acceptQCFlags() {
 		try {
 			QCDB.acceptQCFlags(ServletUtils.getDBDataSource(), fileId, getSelectedRows());
@@ -716,6 +1022,9 @@ public class DataScreenBean extends BaseManagedBean {
 		}
 	}
 	
+	/**
+	 * Apply the entered WOCE flag and comment to the rows selected in the table
+	 */
 	public void applyWoceFlag() {
 		try {
 			QCDB.setWoceFlags(ServletUtils.getDBDataSource(), fileId, getSelectedRows(), getWoceFlag(), getWoceComment());
