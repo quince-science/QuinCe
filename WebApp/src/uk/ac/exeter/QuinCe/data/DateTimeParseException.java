24ff2c39599663e820fe81d08c6e5ee6bc4f3553
Steve Jones <steve@squaregoldfish.co.uk>
2017-02-14 17:22:19 +0100

    Javadoc DateTimeParseException

--- WebApp/src/uk/ac/exeter/QuinCe/data/DateTimeParseException.java
+++ WebApp/src/uk/ac/exeter/QuinCe/data/DateTimeParseException.java
@@ -1,9 +1,21 @@
 package uk.ac.exeter.QuinCe.data;
 
+/**
+ * Exception thrown when a date/time string cannot be parsed
+ * @author Steve Jones
+ *
+ */
 public class DateTimeParseException extends Exception {
 	
+	/**
+	 * The serial version UID
+	 */
 	private static final long serialVersionUID = 4894503384132198690L;
 
+	/**
+	 * Simple constructor
+	 * @param message The error message
+	 */
 	public DateTimeParseException(String message) {
 		super(message);
 	}
