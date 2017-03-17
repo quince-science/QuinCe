969edfd6dd7435f0e21bf5b2017bd4d49d64dd43
Steve Jones <steve@squaregoldfish.co.uk>
2017-03-17 13:29:20 +0100

    Javadoc; removed unused exception

--- WebApp/src/uk/ac/exeter/QuinCe/jobs/files/AutoQCJob.java
+++ WebApp/src/uk/ac/exeter/QuinCe/jobs/files/AutoQCJob.java
@@ -20,7 +20,7 @@ import uk.ac.exeter.QuinCe.database.RecordNotFoundException;
 import uk.ac.exeter.QuinCe.database.QC.QCDB;
 import uk.ac.exeter.QuinCe.database.files.DataFileDB;
 import uk.ac.exeter.QuinCe.jobs.InvalidJobParametersException;
-import uk.ac.exeter.QuinCe.jobs.JobException;
+import uk.ac.exeter.QuinCe.jobs.Job;
 import uk.ac.exeter.QuinCe.jobs.JobFailedException;
 import uk.ac.exeter.QuinCe.jobs.JobManager;
 import uk.ac.exeter.QuinCe.jobs.JobThread;
@@ -28,51 +28,98 @@ import uk.ac.exeter.QuinCe.utils.MissingParamException;
 import uk.ac.exeter.QuinCe.web.system.ResourceManager;
 
 /**
- * <p>This {@link Job} class runs the configured set of QC routines on the records for a given data file.</p>
- * 
- * <p>Records that have already had their WOCE flag to {@code BAD} or {@code IGNORE} are excluded from the QC checks.</p>
- * 
- * <p>Once the QC has been completed, the QC Flag and QC Message are set. The QC flag will be set to {@code GOOD},
- *    {@code QUESTIONABLE} or {@code BAD}. In the latter two cases, the QC Message will contain details of the fault(s)
- *    that were found.
- * </p>
- * <p>If the QC flag was set to {@code GOOD}, the WOCE flag for the record will be set to {@code ASSUMED_GOOD}, to
- *    indicate that the software will assume that the record is good unless the user indicates otherwise. Otherwise,
- *    the WOCE Flag will be set to {@code NEEDS_FLAG}. The user will be required to manually choose a value for the WOCE
- *    Flag, either by accepting the suggestion from the QC job, or overriding the flag and choosing their own. The WOCE
- *    Comment will default to being identical to the QC Message, but this can also be changed if required.
+ * <p>
+ *   This {@link Job} class runs a set of QC routines on the records for a given data file.
  * </p>
  * 
- * <p>If the {@code AutoQCJob} has been run before, some WOCE Flags and Comments will have already been set by the user.
- *    These will be dealt with as follows: 
+ * <p>
+ *   This job can be run more than once during file processing, since certain QC routines
+ *   are more suited to different stages. For example, certain QC failures will mean that
+ *   data reduction cannot be completed (e.g. missing required values), while others merely
+ *   indicate that the calculated values may not be trustworthy. The job's parameters
+ *   will indicate where the calculations are in a file's processing, and which job
+ *   should be run next.
  * </p>
- *  <ul>
- *    <li>
- *      If the QC results have not changed from their previous value, the WOCE flags are kept unchanged.
- *    </li>
- *    <li>
- *      If the QC results are different, the WOCE flag is replaced with {@code ASSUMED_GOOD} or {@code NEEDS_FLAG}
- *      as described above. The user will have to re-examine these records and set the WOCE Flag and Comment once more.
- *    </li>
- *  </ul>
+ * 
+ * <p>
+ *   Records that have already had their WOCE flag to {@link Flag#FATAL}, {@link Flag#BAD} or {@link Flag#IGNORED}
+ *   are excluded from the QC checks.
+ * </p>
+ * 
+ * <p>
+ *   Once the QC has been completed, the QC Flag and QC Message are set. The QC flag will be set to {@link Flag#GOOD},
+ *   {@link Flag#QUESTIONABLE} or {@link Flag#BAD}. In the latter two cases, the QC Message will contain details of the fault(s)
+ *   that were found.
+ * </p>
+ * 
+ * <p>
+ *   If the QC flag was set to {@link Flag#GOOD}, the WOCE flag for the record will be set to {@link Flag#ASSUMED_GOOD},
+ *   to indicate that the software will assume that the record is good unless the user overrides it. Otherwise
+ *   the WOCE Flag will be set to {@link Flag#NEEDED}. The user will be required to manually choose a value for the WOCE
+ *   Flag, either by accepting the suggestion from the QC job, or overriding the flag and choosing their own. The WOCE
+ *   Comment will default to being identical to the QC Message, but this can also be changed if required.
+ * </p>
+ * 
+ * <p>
+ *   If the {@code AutoQCJob} has been run before, some WOCE Flags and Comments will have already been set by the user.
+ *   These will be dealt with as follows: 
+ * </p>
+ * 
+ * <ul>
+ *   <li>
+ *     If the QC results have not changed from their previous value, the WOCE flags are kept unchanged.
+ *   </li>
+ *   <li>
+ *     If the QC results are different, the WOCE flag is replaced with {@link Flag#ASSUMED_GOOD} or {@link Flag#NEEDED}
+ *     as described above. The user will have to re-examine these records and set the WOCE Flag and Comment once more.
+ *   </li>
+ * </ul>
  * 
  * @author Steve Jones
- *
+ * @see Flag
+ * @see Message
  */
 public class AutoQCJob extends FileJob {
 
+	/**
+	 * The name of the job parameter that contains the path to the configuration
+	 * for the QC Routines
+	 * @see RoutinesConfig
+	 */
 	private static final String PARAM_ROUTINES_CONFIG = "ROUTINES_CONFIG";
 	
+	/**
+	 * The job code for the job that must be executed after the QC has completed.
+	 * @see FileInfo
+	 */
 	private static final String PARAM_NEXT_JOB_CODE = "NEXT_CODE";
 	
+	/**
+	 * The job code for the job that must be executed if the QC is interrupted
+	 * @see FileInfo
+	 */
 	private static final String PARAM_INTERRUPTED_JOB_CODE = "INTERRUPTED_CODE";
 	
+	/**
+	 * Constructor that allows the {@link JobManager} to create an instance of this job.
+	 * @param resourceManager The application's resource manager
+	 * @param config The application configuration
+	 * @param jobId The database ID of the job
+	 * @param parameters The job parameters
+	 * @throws MissingParamException If any parameters are missing
+	 * @throws InvalidJobParametersException If any of the job parameters are invalid
+	 * @throws DatabaseException If a database occurs
+	 * @throws RecordNotFoundException If any required database records are missing
+	 * @see JobManager#getNextJob(ResourceManager, Properties)
+	 */
 	public AutoQCJob(ResourceManager resourceManager, Properties config, long jobId, Map<String, String> parameters) throws MissingParamException, InvalidJobParametersException, DatabaseException, RecordNotFoundException {
 		super(resourceManager, config, jobId, parameters);
 	}
 
 	/**
-	 * {@inheritDoc}
+	 * Runs the configured QC routines on the file specified in the job parameters.
+	 * @param thread The thread that is running this job
+	 * @see FileJob#FILE_ID_KEY
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
@@ -206,7 +253,20 @@ public class AutoQCJob extends FileJob {
 		}
 	}
 	
-	protected static Map<String, String> getJobParameters(int jobCode, long fileId) throws JobException {
+	/**
+	 * Create a set of parameters for the AutoQC job.
+	 * 
+	 * <p>
+	 *   The job's file id is passed in to the method. The routines
+	 *   and job parameters are set according to the {#link #jobCode},
+	 *   which indicates the current state of processing the data file.
+	 * </p>
+	 * 
+	 * @param jobCode The current job code
+	 * @param fileId The data file's database ID
+	 * @return The parameters for the job
+	 */
+	protected static Map<String, String> getJobParameters(int jobCode, long fileId) {
 		Map<String, String> parameters = new HashMap<String, String>(4);
 		parameters.put(FILE_ID_KEY, String.valueOf(fileId));
 		
