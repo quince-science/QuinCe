34075cc613befa93746819ad33f079550381e4e8
Steve Jones <steve@squaregoldfish.co.uk>
2017-05-09 11:24:56 +0200

    Header fields disabled as option changed

--- WebApp/WebContent/resources/script/newInstrument.js
+++ WebApp/WebContent/resources/script/newInstrument.js
@@ -7,6 +7,9 @@ function showProcessingMessage() {
 	$('#newInstrumentForm\\:guessFileLayout').click();
 }
 
+// Once the file has been processed, display the file contents and
+// format controls
+// (upload_file.xhtml)
 function updateFileContentDisplay(data) {
 	var status = data.status;
 	
@@ -15,18 +18,47 @@ function updateFileContentDisplay(data) {
 		renderSampleFile();
 		$('#fileFormatSpec').show();
 	}
+	
+	updateHeaderFields();
 }
 
+// Render the contents of the uploaded sample file
+// (upload.xhtml)
 function renderSampleFile() {
 	
 	var fileData = $('#newInstrumentForm\\:sampleFileContent').val().split('\n');
-	
-	
 	var fileHtml = '';
 	for (var i = 0; i < fileData.length; i++) {
-		fileHtml += '<pre>' + fileData[i] + '</pre>';
+		fileHtml += '<pre id="line' + (i + 1) + '">' + fileData[i] + '</pre>';
 	}
-	
 	$('#fileContent').html(fileHtml);
+}
+
+function updateHeaderFields() {
 	
+	switch (parseInt($('[id^=newInstrumentForm\\:headerType]:checked').val())) {
+	case 0: {
+		PF('headerEndString').disable();
+		enableSpinner(PF('headerLines'));
+		break;
+	}
+	case 1: {
+		disableSpinner(PF('headerLines'));
+		PF('headerEndString').enable();
+		break;
+	}
+	}
+}
+
+function disableSpinner(spinnerObject) {
+    spinnerObject.input.prop("disabled", true);
+    spinnerObject.jq.addClass("ui-state-disabled");
+    spinnerObject.upButton.unbind('mousedown.spinner')
+    spinnerObject.downButton.unbind('mousedown.spinner')
+}
+
+function enableSpinner(spinnerObject) {
+   spinnerObject.input.prop("disabled", false);
+   spinnerObject.jq.removeClass("ui-state-disabled");
+   spinnerObject.bindEvents()
 }
