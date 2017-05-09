a91b0df309889992c2169ea98d1949083cf4b060
Steve Jones <steve@squaregoldfish.co.uk>
2017-05-09 20:55:33 +0200

    Javadoc and improved error message

--- WebApp/src/uk/ac/exeter/QuinCe/jobs/InvalidJobClassTypeException.java
+++ WebApp/src/uk/ac/exeter/QuinCe/jobs/InvalidJobClassTypeException.java
@@ -3,16 +3,23 @@ package uk.ac.exeter.QuinCe.jobs;
 /**
  * Exception to indicate that the specified job class
  * is not of the correct type. Either it doesn't extend the
- * abstract Job class, or the required constructor couldn't be located.
+ * abstract {@link Job} class, or the required constructor couldn't be located.
  * 
  * @author Steve Jones
  *
  */
 public class InvalidJobClassTypeException extends Exception {
 
+	/**
+	 * The Serial Version UID
+	 */
 	private static final long serialVersionUID = 8855435038251108392L;
 
+	/**
+	 * Constructor
+	 * @param className The name of the invalid class
+	 */
 	public InvalidJobClassTypeException(String className) {
-		super("The specified job class '" + className + "' is not an instance of the Job class");
+		super("The specified job class '" + className + "' is not an instance of the Job class, or does not have the required constructor");
 	}	
 }
