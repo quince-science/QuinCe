b59db8cf05d0816b74c3345f013f71ba2b6b1958
Steve Jones <steve@squaregoldfish.co.uk>
2017-07-17 17:43:02 +0200

    Javadoc

--- WebApp/src/uk/ac/exeter/QuinCe/utils/ParameterException.java
+++ WebApp/src/uk/ac/exeter/QuinCe/utils/ParameterException.java
@@ -1,9 +1,22 @@
 package uk.ac.exeter.QuinCe.utils;
 
+/**
+ * Exception for errors in method parameters
+ * @author Steve Jones
+ *
+ */
 public class ParameterException extends Exception {
 
+	/**
+	 * The serial version UID
+	 */
 	private static final long serialVersionUID = 3826060193745981107L;
 
+	/**
+	 * Constructor
+	 * @param parameterName The name of the parameter for which the exception is being raised
+	 * @param reason The reason for the exception
+	 */
 	public ParameterException(String parameterName, String reason) {
 		super(parameterName + ' ' + reason);
 	}
