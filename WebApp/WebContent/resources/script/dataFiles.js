3b6fca7a051e2adaf860df02a771ad55d11d9bf3
Jonas F Henriksen <jonas.henriksen@uib.no>
2017-12-14 16:29:04 +0100

    #448 - Handle and display messages per file.

--- WebApp/WebContent/resources/script/dataFiles.js
+++ WebApp/WebContent/resources/script/dataFiles.js
@@ -1,16 +1,23 @@
-function processFiles() {
-	extractNext();
-	if (PF('fileUploadWidget').files.length === 0) {
-		refreshFileList();
-	}
-}
+// Hide dialog with escape key
+$(document).on('keydown', function(e) {
+    if (e.keyCode === 27) {
+        PF('msgDialog').hide();
+    }
+});
 
 function renderMessages(messages) {
-	var html = '<ul>';
+	var html = $('<ul/>');
 	for (var i = 0; i < messages.length; i++) {
-		html += '<li>' + messages[i] + '</li>';
+		var row = $('<li/>');
+		row.addClass(messages[i].severity);
+		var summary = $('<span>');
+		if (messages[i].type == "file") {
+			summary = $('<h3/>');
+		}
+		summary.text(messages[i].summary);
+		row.html(summary)
+		html.append(row);
 	}
-	html += '</ul>';
-
-	$('#messages').html(html);
+	$("#messageText").html(html);
+	PF('msgDialog').show();
 }
