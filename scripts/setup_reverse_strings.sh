307641421de9c036e80fc1390f5365c6eb6d1a00
Jonas F. Henriksen <jonas.henriksen@uib.no>
2018-02-07 09:22:21 +0000

    (#702) Handle config for different setups
    - Replace hardcoded paths with placeholders
    - Default setup with default values, possible to override
    - Ignore overridden setup file in .gitignore
    - Scripts to:
    - Replace placeholders from setup in system config files
    - Reverse replacements to system config files
    - Hide changes to system config files
    - Show changes to system config files

---
+++ scripts/setup_reverse_strings.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+############################################################
+#
+# This script reverses changes made to config files by the
+# setup_replace_strings.sh - script. Currently it just
+# checks out the original files. Any changes to these files
+# will thus be overwritten. This regards the files:
+# - quince.properties
+# - context.xml
+# - web.xml
+#
+############################################################
+
+echo "Run scripts/setup_replace_strings.sh to apply setup "\
+     "in either quince.setup.default or quince.setup" >NB-quince_is_not_setup
+
+git checkout WebApp/WebContent/META-INF/context.xml \
+  WebApp/WebContent/WEB-INF/web.xml \
+  configuration/quince.properties
+
+
