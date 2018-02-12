e65d6b61fd3ef69918a5b98fe706ed3a2c00d03d
Steve Jones <steve@squaregoldfish.co.uk>
2018-02-12 14:35:19 +0100

    Some more isNumeric tests

--- WebApp/junit/junit/uk/ac/exeter/QuinCe/utils/StringUtilsTest.java
+++ WebApp/junit/junit/uk/ac/exeter/QuinCe/utils/StringUtilsTest.java
@@ -14,10 +14,42 @@ import uk.ac.exeter.QuinCe.utils.StringUtils;
 public class StringUtilsTest {
 
 	/**
+	 * Test an integer
+	 */
+	@Test
+	public void testIsNumericInteger() {
+		assertTrue(StringUtils.isNumeric("7"));
+	}
+
+	/**
+	 * Test a decimal number
+	 */
+	@Test
+	public void testIsNumericFloat() {
+		assertTrue(StringUtils.isNumeric("67.5"));
+	}
+
+	/**
 	 * Test a completely non-numeric string
 	 */
 	@Test
 	public void testIsNumericNonNumeric() {
 		assertFalse(StringUtils.isNumeric("I am not a number"));
+	}
+
+	/**
+	 * Test a completely non-numeric string
+	 */
+	@Test
+	public void testIsNumericNull() {
+		assertFalse(StringUtils.isNumeric(null));
+	}
+
+	/**
+	 * Test a completely non-numeric string
+	 */
+	@Test
+	public void testIsNumericNan() {
+		assertFalse(StringUtils.isNumeric("NaN"));
 	}
 }
