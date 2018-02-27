f2feecc7145287051e0fca0ff1b38ae766ad7b66
Jonas F Henriksen <jonas.henriksen@uib.no>
2018-02-27 15:22:25 +0100

    (#739) Some useful explanations in comments

--- scripts/full_restore_from_prod.sh
+++ scripts/full_restore_from_prod.sh
@@ -10,13 +10,16 @@
 # - Alert to slack on errors
 # - Start apache with new codebase
 #
+# This script will be run in cron on the test server with
+# the following command:
+# 5 5 * * * . cd ~/QuinCe && scripts/full_restore_from_prod.sh >~/cron.out 2>&1
 ############################################################
 
 # Restore database
 ssh_user=${@:$OPTIND:1}
 
 scripts/db_restore_from_prod.sh $ssh_user || \
-  exit 1;
+  exit 1; # Failed to restore database from production database.
 
 branch=$(scripts/get_setup_property.sh git_test_branch)
 
@@ -45,6 +48,9 @@ tmpfile=/tmp/gradle_build_test_output.txt
 # Clean, test and build the war file. Failing tests are sent to the QuinCe-QC
 # slack #errors channel
 ./gradlew clean test war > $tmpfile 2>&1 || scripts/slackerror.sh -f $tmpfile
+
+#  Run upgrade scripts to get the system up to date with the current version
+scripts/upgrade.sh > $tmpfile 2>&1 || scripts/slackerror.sh -f $tmpfile
 
 # Start tomcat with nohup to allow cron to exit while tomcat keeps running
 nohup ./gradlew appStartWar &
