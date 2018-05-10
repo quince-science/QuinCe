4c5259e5f42d2043d1e020010593f266d3aa1711
Steve Jones <steve@squaregoldfish.co.uk>
2018-05-10 10:21:57 +0200

    (#614) Diagnostic values stored in database

--- src/migrations/db_migrations/V5__diagnostic_values_support_614.sql
+++ src/migrations/db_migrations/V5__diagnostic_values_support_614.sql
@@ -1,19 +1,10 @@
-# Add a diagnostic column to the file_column table
-ALTER TABLE `file_column` ADD `diagnostic` BOOLEAN NOT NULL DEFAULT FALSE;
-
 # Table for diagnostic data - one record per sensor per measurement
 CREATE TABLE `diagnostic`.`diagnostic_data` (
-  `file_definition_id` INT NOT NULL,
-  `file_column` SMALLINT(3) NOT NULL,
   `measurement_id` INT NOT NULL,
+  `sensor_name` VARCHAR(100) NOT NULL,
   `value` DOUBLE NULL,
-  PRIMARY KEY (`file_definition_id`, `file_column`, `measurement_id`),
+  PRIMARY KEY (`measurement_id`, `sensor_name`),
   INDEX `DIAGNOSTIC_DATASETDATA_idx` (`measurement_id` ASC),
-  CONSTRAINT `DIAGNOSTIC_FILECOLUMN`
-    FOREIGN KEY (`file_definition_id` , `file_column`)
-    REFERENCES `diagnostic`.`file_column` (`file_definition_id` , `file_column`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT,
   CONSTRAINT `DIAGNOSTIC_DATASETDATA`
     FOREIGN KEY (`measurement_id`)
     REFERENCES `diagnostic`.`dataset_data` (`id`)
