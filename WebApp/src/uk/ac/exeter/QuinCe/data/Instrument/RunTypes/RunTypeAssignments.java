db8996a73d48ebef314d10d4f5b782822909ba62
Steve Jones <steve@squaregoldfish.co.uk>
2018-05-28 15:29:25 +0200

    (#817) Run types are case insensitive

--- WebApp/src/uk/ac/exeter/QuinCe/data/Instrument/RunTypes/RunTypeAssignments.java
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Instrument/RunTypes/RunTypeAssignments.java
@@ -13,4 +13,35 @@ public class RunTypeAssignments extends TreeMap<String, RunTypeAssignment> {
     super();
   }
 
+  /**
+   * Automatically convert a key to match the internal Map
+   * If the key is a String, convert it to lower case.
+   * Otherwise leave it as it is
+   * @param key The key
+   * @return The converted key
+   */
+  private Object convertKey(Object key) {
+    Object result = key;
+
+    if (key instanceof String) {
+      result = ((String) key).toLowerCase();
+    }
+
+    return result;
+  }
+
+  @Override
+  public RunTypeAssignment put(String key, RunTypeAssignment assignment) {
+    return super.put(key.toLowerCase(), assignment);
+  }
+
+  @Override
+  public RunTypeAssignment get(Object key) {
+    return super.get(convertKey(key));
+  }
+
+  @Override
+  public boolean containsKey(Object key) {
+    return super.containsKey(convertKey(key));
+  }
 }
