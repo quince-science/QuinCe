2a92dbca1e60a295fd283df91831de4c7220bf90
Steve Jones <steve@squaregoldfish.co.uk>
2018-06-11 10:40:42 +0200

    (#882) Catch all exceptions and dump them to the console

---
+++ WebApp/src/uk/ac/exeter/QuinCe/web/system/CustomExceptionHandlerFactory.java
@@ -0,0 +1,31 @@
+package uk.ac.exeter.QuinCe.web.system;
+
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerFactory;
+
+/**
+ * Factory class for custom exception handlers
+ * @author Steve Jones
+ *
+ */
+public class CustomExceptionHandlerFactory extends ExceptionHandlerFactory {
+
+  /**
+   * The parent {@code ExceptionHandlerFactory} provided
+   * by the system
+   */
+  private ExceptionHandlerFactory parent;
+
+  /**
+   * Constructor - grabs a link to the parent {@code ExceptionHandlerFactory}
+   * @param parent The parent factory
+   */
+  public CustomExceptionHandlerFactory(ExceptionHandlerFactory parent) {
+    this.parent = parent;
+  }
+
+  @Override
+  public ExceptionHandler getExceptionHandler() {
+    return new BaseExceptionHandler(parent.getExceptionHandler());
+  }
+}
