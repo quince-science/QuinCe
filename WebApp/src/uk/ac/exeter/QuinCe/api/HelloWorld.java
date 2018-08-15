a11ab9b1c2d4793f6bd80f9b2eda3b67a504fd51
Steve Jones <steve@squaregoldfish.co.uk>
2018-08-15 14:46:26 +0200

    (jax-rs) Initial JAX-RS support

---
+++ WebApp/src/uk/ac/exeter/QuinCe/api/HelloWorld.java
@@ -0,0 +1,17 @@
+package uk.ac.exeter.QuinCe.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/HelloWorld")
+public class HelloWorld {
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String get() {
+    return "Hello World!";
+  }
+
+}
