f1729c8affd746ed94665a182fd607e2c76e891a
Steve Jones <steve@squaregoldfish.co.uk>
2018-08-28 13:19:02 +0200

    Move Hello World to test package

--- WebApp/src/uk/ac/exeter/QuinCe/api/test/HelloWorld.java
+++ WebApp/src/uk/ac/exeter/QuinCe/api/test/HelloWorld.java
@@ -1,11 +1,11 @@
-package uk.ac.exeter.QuinCe.api;
+package uk.ac.exeter.QuinCe.api.test;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/HelloWorld")
+@Path("/test/HelloWorld")
 public class HelloWorld {
 
   @GET
