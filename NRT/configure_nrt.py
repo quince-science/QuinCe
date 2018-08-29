0818e0d4267af238d593473fea879f38939a9d42
Steve Jones <steve@squaregoldfish.co.uk>
2018-08-29 14:15:26 +0200

    (#355) Initialise database if it's missing

--- NRT/configure_nrt.py
+++ NRT/configure_nrt.py
@@ -1,14 +1,17 @@
 import toml
 import urllib.request, urllib.error
 import base64
+import nrtdb
 
 #######################################################
 
 with open("config.toml", "r") as config_file:
   config = toml.loads(config_file.read())
 
-print("Retrieving NRT instruments from QuinCe...")
+print("Connecting to NRT database...")
+dbconn = nrtdb.get_db_conn(config["Database"]["location"])
 
+print("Retrieving NRT instruments from QuinCe...")
 quince_url = config["QuinCe"]["url"]
 user = config["QuinCe"]["user"]
 password = config["QuinCe"]["password"]
@@ -17,8 +20,6 @@ request = urllib.request.Request(quince_url + "/api/nrt/GetInstruments")
 
 auth_string = "%s:%s" % (user, password)
 base64_auth_string = base64.standard_b64encode(auth_string.encode("utf-8"))
-
-
 request.add_header("Authorization", "Basic %s" % base64_auth_string.decode("utf-8"))
 
 try:
@@ -26,5 +27,5 @@ try:
   print(conn.read())
 except urllib.error.HTTPError as e:
   print("%s %s" % (e.code, e.reason))
+  nrtdb.close(dbconn)
   quit()
-
