67177b822da1da6dfdfe617a3464de9e75b8f9a9
Steve Jones <steve@squaregoldfish.co.uk>
2018-09-03 15:30:03 +0200

    (#355) Ask for retriever type on non-configured instruments

--- NRT/DataRetriever.py
+++ NRT/DataRetriever.py
@@ -2,12 +2,12 @@ from abc import ABC, abstractmethod
 
 class DataRetriever(ABC):
 
-	# Empty constructor
-	def __init__(self):
-		pass
+  # Empty constructor
+  def __init__(self):
+    pass
 
-	# Get the configuration type
-	@staticmethod
-	@abstractmethod
-	def get_type():
-		pass
+  # Get the configuration type
+  @staticmethod
+  @abstractmethod
+  def get_type():
+    pass
