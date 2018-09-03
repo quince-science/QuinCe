451ef4b9f05ede584b7b2a9c11143e9599522ac9
Steve Jones <steve@squaregoldfish.co.uk>
2018-09-03 15:08:14 +0200

    (#355) Ask for retriever type on non-configured instruments

---
+++ NRT/retriever_factory.py
@@ -0,0 +1,38 @@
+from DataRetriever import DataRetriever
+import EmailRetriever, FileRetriever
+
+# Factory for DataRetriever instances
+
+# Get the list of retriever types
+def get_retriever_types():
+  result = []
+
+  for clazz in DataRetriever.__subclasses__():
+    result.append(clazz.get_type())
+
+  return result
+
+# Ask the user to select a retriever type
+def ask_retriever_type():
+  entries = []
+
+  for clazz in DataRetriever.__subclasses__():
+    entries.append(clazz.get_type())
+
+  
+  selected = -1
+  while selected == -1:
+    print("Select source for NRT files:")
+    
+    for i in range(0, len(entries)):
+      print("  %d. %s" % (i + 1, entries[i]))
+
+    try:
+      selection = input("Selection: ")
+      selection = int(selection)
+      if selection > 0 and selection <= len(entries):
+        selected = selection
+    except ValueError:
+      pass
+
+  return selected
