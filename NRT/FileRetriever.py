e34c57e8a84bb7fb25a4bb1fddfa418a935e3569
Steve Jones <steve@squaregoldfish.co.uk>
2018-09-04 11:00:57 +0200

    (#355) Main program complete. Configs not complete.

--- NRT/FileRetriever.py
+++ NRT/FileRetriever.py
@@ -2,8 +2,13 @@ from DataRetriever import DataRetriever
 
 class FileConfiguration(DataRetriever):
 
-  def __init__(self):
+  # Constructor. Preset configuration is optional
+  def __init__(self, configuration=None):
     super().__init__()
+    if configuration is None:
+      pass
+    else:
+      self.configuration = configuration
 
   @staticmethod
   def get_type():
