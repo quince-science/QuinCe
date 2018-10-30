1af15db2cb54d4ff5cf6a430e3dd0a9780a943a5
Steve Jones <steve@squaregoldfish.co.uk>
2018-10-30 13:03:18 +0100

    (#355) Remove dummy retrievers; Base IMAP retriever added

--- NRT/ImapRetriever.py
+++ NRT/ImapRetriever.py
@@ -1,16 +1,22 @@
 from DataRetriever import DataRetriever
 
-class EmailConfiguration(DataRetriever):
+class ImapConfiguration(DataRetriever):
 
   def __init__(self, configuration=None):
     super().__init__()
     if configuration is None:
       self.configuration["Server"] = None
+      self.configuration["Port"] = None
       self.configuration["User"] = None
       self.configuration["Password"] = None
+      self.configuration["Source Folder"] = None
+      self.configuration["Downloaded Folder"] = None
     else:
       self.configuration = configuration
 
   @staticmethod
   def get_type():
-    return "Email"
+    return "IMAP Email"
+
+  def _test_configuration(self):
+    pass
