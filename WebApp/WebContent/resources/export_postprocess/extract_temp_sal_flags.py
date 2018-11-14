ce88430eed52dc716643e46c5be671cbbccc7c8f
MarenQuinCe <43780941+MarenQuinCe@users.noreply.github.com>
2018-11-14 13:36:17 +0100

    Update extract_temp_sal_flags.py
    script is now a function. Takes input_file and output_file as variables. return output_file.

--- WebApp/WebContent/resources/export_postprocess/extract_temp_sal_flags.py
+++ WebApp/WebContent/resources/export_postprocess/extract_temp_sal_flags.py
@@ -1,18 +1,22 @@
-''' Finds error flags denoting bad intake temperature and salinity values, creates a corresponding boolean value in a separate column'''
-import pandas as pd
-file='gofl20150410-sst_salinity_qc.csv'
-df=pd.read_csv(file)
+def extract_temp_sal_flags(file_in,file_out):
+    ''' Finds error flags denoting bad intake temperature and salinity values, creates a corresponding boolean value in a separate column
+        file_in: original file
+	file_out: name of new file 
+	returns: file_out'''
+    import pandas as pd
+    file=file_in #'gofl20150410-sst_salinity_qc.csv'
+    df=pd.read_csv(file)
 
-temperature_err_msg=['intake temperature','sst']
-salinity_err_msg=['salinity','sss']
+    temperature_err_msg=['intake temperature','sst']
+    salinity_err_msg=['salinity','sss']
 
-def get_error(row,error_name):
-	if pd.isnull(row['QC Message']):
-		return False
-	return error_name[0]  in row['QC Message'].lower() or error_name[1]  in row['QC Message'].lower()
+    def get_error(row,error_name):
+        if pd.isnull(row['QC Message']):
+            return False
+        return error_name[0]  in row['QC Message'].lower() or error_name[1]  in row['QC Message'].lower()
 
-df['Temperature error']=df.apply(lambda row: get_error(row,temperature_err_msg),axis=1)
+    df['Temperature error']=df.apply(lambda row: get_error(row,temperature_err_msg),axis=1)
+    df['Salinity error']=df.apply(lambda row: get_error(row,salinity_err_msg),axis=1)
 
-df['Salinity error']=df.apply(lambda row: get_error(row,salinity_err_msg),axis=1)
-
-df.to_csv('out.csv')
+    df.to_csv(file_out)
+    return file_out
