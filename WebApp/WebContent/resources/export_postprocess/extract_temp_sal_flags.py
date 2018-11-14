6b73a2884e300da547637d764ce6e334ecde3280
MarenQuinCe <43780941+MarenQuinCe@users.noreply.github.com>
2018-11-14 14:32:43 +0100

    Update extract_temp_sal_flags.py

--- WebApp/WebContent/resources/export_postprocess/extract_temp_sal_flags.py
+++ WebApp/WebContent/resources/export_postprocess/extract_temp_sal_flags.py
@@ -1,8 +1,11 @@
 import sys
 import pandas as pd
-''' Finds error flags denoting bad intake temperature and salinity values, creates a corresponding boolean value in a separate column'''
+''' Finds error flags denoting bad intake temperature and salinity values, creates a corresponding boolean value in a separate column, needs two
+variables as system input arguments, read_from_file and write_to_file'''
 try:
-    if len(sys.argv)==3:
+    if len(sys.argv)!=3:
+        print('< Excpected inputfile and outputfile as system arguments. > \n< Please try again adding \'read_from_file\' \'write_to_file\' at the end of your command. >')
+    else:
         inputfile=sys.argv[1]
         outputfile=sys.argv[2]
 
@@ -19,8 +22,6 @@ try:
         df['Temperature error']=df.apply(lambda row: get_error(row,temperature_err_msg),axis=1)
         df['Salinity error']=df.apply(lambda row: get_error(row,salinity_err_msg),axis=1)
 
-        df.to_csv(outputfile)
-    else:
-        print('< Excpected inputfile and outputfile as system arguments. > \n< Please try again adding \'read_from_file\' \'write_to_file\' at the end of your command. >')
+        df.to_csv(outputfile)        
 except Exception as e:
     print('Failed to extract temperature and salinity flags:,', e)
