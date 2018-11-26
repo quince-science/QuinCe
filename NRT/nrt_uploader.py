eb1e83bbbf17182b572e86df0f71dbbda08aa720
Steve Jones <steve@squaregoldfish.co.uk>
2018-11-26 13:08:18 +0100

    (#355) QuinCe uploader basically working

--- NRT/nrt_uploader.py
+++ NRT/nrt_uploader.py
@@ -6,10 +6,9 @@ from io import BytesIO
 import nrtftp, quince
 
 ftpconn = None
-logger = None
 
 # Log a message for a specific instrument
-def log_instrument(instrument_id, level, message):
+def log_instrument(logger, instrument_id, level, message):
   logger.log(level, str(instrument_id) + ":" + message)
 
 def main():
@@ -34,14 +33,24 @@ def main():
       logger.warning("FTP folder " + instrument_id
         + " does not match a QuinCe instrument")
     else:
-      logger.info("Processing files for instrument " + instrument_id)
+      logger.info("Processing instrument " + instrument_id)
       files = nrtftp.get_instrument_files(ftpconn, config["FTP"], instrument_id)
       for file in files:
         file_content = nrtftp.get_file(ftpconn, config["FTP"],
           instrument_id, file)
 
-        quince.upload_file(config, instrument_id, file, file_content)
-        exit()
+        log_instrument(logger, instrument_id, logging.DEBUG, "Uploading file " \
+          + file)
+
+        upload_result = quince.upload_file(config, instrument_id, file, file_content)
+        if upload_result == 200:
+          log_instrument(logger, instrument_id, logging.DEBUG, \
+          	"Upload succeeded")
+          nrtftp.upload_succeeded(ftpconn, config["FTP"], instrument_id, file)
+        else:
+          log_instrument(logger, instrument_id, logging.ERROR, \
+          	"Upload failed (status code " + str(upload_result) + ")")
+          nrtftp.upload_failed(ftpconn, config["FTP"], instrument_id, file)
 
   # Close down
   ftpconn.close()
