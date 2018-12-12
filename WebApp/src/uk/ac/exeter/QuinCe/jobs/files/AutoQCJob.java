6ebc01cf7de5ac4eae210c4e90a32adab3578a06
Steve Jones <steve@squaregoldfish.co.uk>
2018-12-12 13:39:23 +0100

    (#1060) Dataset status set correctly after Auto QC

--- WebApp/src/uk/ac/exeter/QuinCe/jobs/files/AutoQCJob.java
+++ WebApp/src/uk/ac/exeter/QuinCe/jobs/files/AutoQCJob.java
@@ -138,6 +138,7 @@ public class AutoQCJob extends Job {
       dataSet = DataSetDB.getDataSet(conn, datasetId);
       dataSet.setStatus(DataSet.STATUS_AUTO_QC);
       DataSetDB.updateDataSet(conn, dataSet);
+      boolean userQcNeeded = false;
 
       CalculationDB calculationDB = CalculationDBFactory.getCalculationDB();
 
@@ -241,6 +242,9 @@ public class AutoQCJob extends Job {
 
         if (writeRecord) {
           calculationDB.storeQC(conn, (CalculationRecord) record);
+          if (qcRecord.getUserFlag().equals(Flag.NEEDED)) {
+            userQcNeeded = true;
+          }
         }
       }
 
@@ -253,8 +257,9 @@ public class AutoQCJob extends Job {
           if (dataSet.isNrt()) {
             dataSet.setStatus(DataSet.STATUS_READY_FOR_EXPORT);
           } else {
-            dataSet.setStatus(DataSet.STATUS_USER_QC);
-            if (dataSet.getNeedsFlagCount() == 0) {
+            if (userQcNeeded) {
+              dataSet.setStatus(DataSet.STATUS_USER_QC);
+            } else {
               dataSet.setStatus(DataSet.STATUS_READY_FOR_SUBMISSION);
             }
           }
