70f77234adee4fff7386e1394580f418345e1beb
Steve Jones <steve@squaregoldfish.co.uk>
2019-01-31 16:32:56 +0100

    (#1089) Change file_column table to reference sensor_types

--- WebApp/junit/resources/db_migrations/V99__add_sensor_types.sql
+++ WebApp/junit/resources/db_migrations/V99__add_sensor_types.sql
@@ -51,6 +51,10 @@ INSERT INTO sensor_types (name, parent, depends_on, depends_question, internal_c
 INSERT INTO sensor_types (name, parent, depends_on, depends_question, internal_calibration)
   VALUES ('CO₂ in gas', NULL, NULL, NULL, 1);
 
+-- Run Type - requirement triggered by internal_calibration field
+INSERT INTO sensor_types (name, parent, depends_on, depends_question, internal_calibration)
+  VALUES ('Run Type', NULL, NULL, NULL, 0);
+
 -- Atmospheric Pressure
 INSERT INTO sensor_types (name, parent, depends_on, depends_question, internal_calibration)
   VALUES ('Atmospheric Pressure', NULL, NULL, NULL, 0);
@@ -141,3 +145,42 @@ INSERT INTO variable_sensors (variable_id, sensor_type, core, questionable_casca
     (SELECT id FROM measured_variables),
     (SELECT id FROM sensor_types WHERE name = 'CO₂ in gas'),
     0, 3, 4);
+
+
+-- --------------------------------
+-- Rejig file_column table
+
+-- Rename existing sensor type column
+ALTER TABLE file_column CHANGE COLUMN sensor_type sensor_type_old varchar(100);
+
+-- Create new sensor_type foreign key column (no constraints yet)
+ALTER TABLE file_column ADD COLUMN sensor_type INT NULL AFTER `primary_sensor`;
+
+-- Migrate all values
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Intake Temperature') WHERE sensor_type_old = 'Intake Temperature';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Salinity') WHERE sensor_type_old = 'Salinity';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Equilibrator Temperature') WHERE sensor_type_old = 'Equilibrator Temperature';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Equilibrator Pressure (absolute)') WHERE sensor_type_old = 'Equilibrator Pressure (absolute)';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Equilibrator Pressure (differential)') WHERE sensor_type_old = 'Equilibrator Pressure (differential)';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Ambient Pressure') WHERE sensor_type_old = 'Ambient Pressure';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'xH₂O in gas') WHERE sensor_type_old = 'xH2O';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'CO₂ in gas') WHERE sensor_type_old = 'CO2';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Run Type') WHERE sensor_type_old = 'Run Type';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Diagnostic Temperature') WHERE sensor_type_old = 'Diagnostic: Temperature';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Diagnostic Pressure') WHERE sensor_type_old = 'Diagnostic: Pressure';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Diagnostic Air Flow') WHERE sensor_type_old = 'Diagnostic: Air Flow';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Diagnostic Water Flow') WHERE sensor_type_old = 'Diagnostic: Water Flow';
+UPDATE file_column SET sensor_type = (SELECT id FROM sensor_types WHERE name = 'Diagnostic Voltage') WHERE sensor_type_old = 'Diagnostic: Voltage';
+
+-- Apply constraints to new sensor type column
+ALTER TABLE file_column ALTER COLUMN sensor_type SET NOT NULL;
+
+ALTER TABLE file_column
+ADD CONSTRAINT FILECOLUMN_SENSORTYPE
+  FOREIGN KEY (sensor_type)
+  REFERENCES sensor_types(id)
+  ON DELETE RESTRICT
+  ON UPDATE RESTRICT;
+
+-- Remove the old column
+ALTER TABLE file_column DROP COLUMN sensor_type_old;
