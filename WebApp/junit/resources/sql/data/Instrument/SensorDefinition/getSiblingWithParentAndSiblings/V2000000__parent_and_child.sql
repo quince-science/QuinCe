4871bdcefd66f3c005b5d003b96c8aa482ca271c
Steve Jones <steve@squaregoldfish.co.uk>
2019-02-08 09:40:55 +0100

    (#1089) Sensor Assignment changes and tests

--- WebApp/junit/resources/sql/data/Instrument/SensorDefinition/getSiblingWithParentAndSiblings/V2000000__parent_and_child.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO sensor_types (name, parent) VALUES ('Test Parent', null);
-
-INSERT INTO sensor_types (name, parent)
-  VALUES ('Test Child', (SELECT id FROM sensor_types WHERE name = 'Test Parent'));
-
-  INSERT INTO sensor_types (name, parent)
-  VALUES ('Sibling 1', (SELECT id FROM sensor_types WHERE name = 'Test Parent'));
-INSERT INTO sensor_types (name, parent)
-  VALUES ('Sibling 2', (SELECT id FROM sensor_types WHERE name = 'Test Parent'));
--- WebApp/junit/resources/sql/data/Instrument/SensorDefinition/newSensorAssignments/V2000000__instrument_with_variables.sql
+++
@@ -1,7 +0,0 @@
--- The instrument
-INSERT INTO instrument (id, owner, name)
-  VALUES (1, 1, 'Test Instrument');
-  
--- Instrument uses the basic marine pCO2 variable
-INSERT INTO instrument_variables (instrument_id, variable_id)
-  VALUES (1, (SELECT id FROM variables WHERE name = 'Underway Marine pCO₂'));
