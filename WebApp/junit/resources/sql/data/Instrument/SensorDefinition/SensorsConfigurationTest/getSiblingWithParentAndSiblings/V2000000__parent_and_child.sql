77e40181a138879cdbd1aea2bc6afede9442b20f
Steve Jones <steve@squaregoldfish.co.uk>
2019-02-25 11:38:23 +0100

    (#1089) Add variable groups to sensor types in the datbase

--- WebApp/junit/resources/sql/data/Instrument/SensorDefinition/SensorsConfigurationTest/getSiblingWithParentAndSiblings/V2000000__parent_and_child.sql
+++ WebApp/junit/resources/sql/data/Instrument/SensorDefinition/SensorsConfigurationTest/getSiblingWithParentAndSiblings/V2000000__parent_and_child.sql
@@ -1,9 +1,9 @@
-INSERT INTO sensor_types (name, parent) VALUES ('Test Parent', null);
+INSERT INTO sensor_types (name, vargroup, parent) VALUES ('Test Parent', 'Group', null);
 
-INSERT INTO sensor_types (name, parent)
-  VALUES ('Test Child', (SELECT id FROM sensor_types WHERE name = 'Test Parent'));
+INSERT INTO sensor_types (name, vargroup, parent)
+  VALUES ('Test Child', 'Group', (SELECT id FROM sensor_types WHERE name = 'Test Parent'));
 
-  INSERT INTO sensor_types (name, parent)
-  VALUES ('Sibling 1', (SELECT id FROM sensor_types WHERE name = 'Test Parent'));
-INSERT INTO sensor_types (name, parent)
-  VALUES ('Sibling 2', (SELECT id FROM sensor_types WHERE name = 'Test Parent'));
+  INSERT INTO sensor_types (name, vargroup, parent)
+  VALUES ('Sibling 1', 'Group', (SELECT id FROM sensor_types WHERE name = 'Test Parent'));
+INSERT INTO sensor_types (name, vargroup, parent)
+  VALUES ('Sibling 2', 'Group', (SELECT id FROM sensor_types WHERE name = 'Test Parent'));
