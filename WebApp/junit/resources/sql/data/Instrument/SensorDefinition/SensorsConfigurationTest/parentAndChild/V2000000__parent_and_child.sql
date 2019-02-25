4ebf56e3da771e815105b0896e9efd8d58748755
Steve Jones <steve@squaregoldfish.co.uk>
2019-02-25 11:38:23 +0100

    (#1089) Add variable groups to sensor types in the datbase

--- WebApp/junit/resources/sql/data/Instrument/SensorDefinition/SensorsConfigurationTest/parentAndChild/V2000000__parent_and_child.sql
+++ WebApp/junit/resources/sql/data/Instrument/SensorDefinition/SensorsConfigurationTest/parentAndChild/V2000000__parent_and_child.sql
@@ -1,15 +1,14 @@
-INSERT INTO sensor_types (name, parent, depends_on, depends_question, internal_calibration, diagnostic)
-  VALUES ('Grandparent', NULL, NULL, NULL, 0, 0);
+INSERT INTO sensor_types (name, vargroup, parent, depends_on, depends_question, internal_calibration, diagnostic)
+  VALUES ('Grandparent', 'Group', NULL, NULL, NULL, 0, 0);
   
-INSERT INTO sensor_types (name, parent, depends_on, depends_question, internal_calibration, diagnostic)
-  VALUES ('Parent', (SELECT id FROM sensor_types WHERE name = 'Grandparent'), NULL, NULL, 0, 0);
+INSERT INTO sensor_types (name, vargroup, parent, depends_on, depends_question, internal_calibration, diagnostic)
+  VALUES ('Parent', 'Group', (SELECT id FROM sensor_types WHERE name = 'Grandparent'), NULL, NULL, 0, 0);
 
-INSERT INTO sensor_types (name, parent, depends_on, depends_question, internal_calibration, diagnostic)
-  VALUES ('Aunt', (SELECT id FROM sensor_types WHERE name = 'Grandparent'), NULL, NULL, 0, 0);
+INSERT INTO sensor_types (name, vargroup, parent, depends_on, depends_question, internal_calibration, diagnostic)
+  VALUES ('Aunt', 'Group', (SELECT id FROM sensor_types WHERE name = 'Grandparent'), NULL, NULL, 0, 0);
 
-  
-INSERT INTO sensor_types (name, parent, depends_on, depends_question, internal_calibration, diagnostic)
-  VALUES ('Child', (SELECT id FROM sensor_types WHERE name = 'Parent'), NULL, NULL, 0, 0);
+INSERT INTO sensor_types (name, vargroup, parent, depends_on, depends_question, internal_calibration, diagnostic)
+  VALUES ('Child', 'Group', (SELECT id FROM sensor_types WHERE name = 'Parent'), NULL, NULL, 0, 0);
 
-  INSERT INTO sensor_types (name, parent, depends_on, depends_question, internal_calibration, diagnostic)
-  VALUES ('Child 2', (SELECT id FROM sensor_types WHERE name = 'Parent'), NULL, NULL, 0, 0);
+INSERT INTO sensor_types (name, vargroup, parent, depends_on, depends_question, internal_calibration, diagnostic)
+  VALUES ('Child 2', 'Group', (SELECT id FROM sensor_types WHERE name = 'Parent'), NULL, NULL, 0, 0);
