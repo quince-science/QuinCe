60a84cdc99d404616668a319a2989a3cf7792442
Jonas F Henriksen <jonas.henriksen@uib.no>
2019-03-11 14:42:14 +0100

    (#901) Use GSON lib instead of manually writing JSON

--- WebApp/src/uk/ac/exeter/QuinCe/web/html/HtmlUtils.java
+++ WebApp/src/uk/ac/exeter/QuinCe/web/html/HtmlUtils.java
@@ -2,6 +2,8 @@ package uk.ac.exeter.QuinCe.web.html;
 
 import java.util.List;
 
+import com.google.gson.Gson;
+
 /**
  * Various HTML-related utilities and constants
  * @author Steve Jones
@@ -14,28 +16,7 @@ public class HtmlUtils {
   public static final String CLASS_INFO = "info";
 
   public static String makeJSONArray(List<String> lines) {
-    StringBuilder output = new StringBuilder();
-
-    output.append('[');
-
-    for (int i = 0; i < lines.size(); i++) {
-      output.append('"');
-      output.append(lines.get(i)
-          .replace("\r", "")
-          .replace("\n", "")
-          .replace("\t", "\\t")
-          .replace("\\", "\\\\")
-          .replace("\"", "\\\""));
-
-      output.append('"');
-
-      if (i < (lines.size() - 1)) {
-        output.append(',');
-      }
-    }
-
-    output.append(']');
-
-    return output.toString();
+    Gson json = new Gson();
+    return json.toJson(lines);
   }
 }
