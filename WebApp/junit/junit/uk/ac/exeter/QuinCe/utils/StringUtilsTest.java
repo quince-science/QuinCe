3c47cbf3b7eb220c2dbe54e5fa09b8e43ba1aa46
Jonas F Henriksen <jonas.henriksen@uib.no>
2019-03-11 14:33:26 +0100

    Tests for functions with JSON handling

--- WebApp/junit/junit/uk/ac/exeter/QuinCe/utils/StringUtilsTest.java
+++ WebApp/junit/junit/uk/ac/exeter/QuinCe/utils/StringUtilsTest.java
@@ -2,7 +2,13 @@ package junit.uk.ac.exeter.QuinCe.utils;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.json.JSONException;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import uk.ac.exeter.QuinCe.utils.StringUtils;
 
@@ -52,4 +58,46 @@ public class StringUtilsTest {
   public void testIsNumericNan() {
     assertFalse(StringUtils.isNumeric("NaN"));
   }
+
+  @Test
+  public void testGetPropertiesAsJsonNull() throws JSONException {
+    assertEquals("null", StringUtils.getPropertiesAsJson(null));
+  }
+
+  @Test
+  public void testGetPropertiesAsJsonStrings() throws JSONException {
+    Properties props = new Properties();
+    String expectedJson = "{\"anykey\": \"anyvalue\", \"anykey.dot\": \"anothervalue\"}";
+    props.put("anykey", "anyvalue");
+    props.put("anykey.dot", "anothervalue");
+    JSONAssert.assertEquals(
+        expectedJson,
+        StringUtils.getPropertiesAsJson(props),
+        JSONCompareMode.LENIENT
+    );
+  }
+
+  @Test
+  public void testIntListToJsonArray() throws JSONException {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(new Integer(1));
+    list.add(new Integer(2));
+    JSONAssert.assertEquals("[1,2]", StringUtils.intListToJsonArray(list),
+        JSONCompareMode.LENIENT);
+  }
+
+  @Test
+  public void testJsonArrayToIntList() throws JSONException {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(new Integer(1));
+    list.add(new Integer(2));
+    assertEquals(list, StringUtils.jsonArrayToIntList("[1,2]"));
+  }
+
+  @Test
+  public void testJsonArrayToIntListEmpty() throws JSONException {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    assertEquals(list, StringUtils.jsonArrayToIntList("[]"));
+  }
+
 }
