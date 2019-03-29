c20c914b45553b95e79d058c7073ea356d357526
Steve Jones <steve@squaregoldfish.co.uk>
2019-03-29 09:12:15 +0100

    (#1121) Add ID and QC flags to sensor_values

--- src/migrations/db_migrations/V100__sensor_values.sql
+++ src/migrations/db_migrations/V100__sensor_values.sql
@@ -1,8 +1,13 @@
 CREATE TABLE sensor_values (
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   dataset_id INT NOT NULL,
   file_column INT NOT NULL,
   date BIGINT(20) NOT NULL,
   value VARCHAR(100) NULL,
+  auto_qc_flag SMALLINT(2) DEFAULT -1000,
+  auto_qc_message VARCHAR(255),
+  user_qc_flag SMALLINT(2) DEFAULT -1000,
+  user_qc_message VARCHAR(255),
   CONSTRAINT SENSORVALUE_DATASET
     FOREIGN KEY (dataset_id)
     REFERENCES dataset (id)
