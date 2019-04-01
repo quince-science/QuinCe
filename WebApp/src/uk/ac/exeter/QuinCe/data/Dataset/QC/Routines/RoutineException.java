1cc87b4dff1c57d8bcede79f428608a8b6e468e2
Steve Jones <steve@squaregoldfish.co.uk>
2019-04-01 15:24:05 +0200

    (#1121) Spotless fix

--- WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/QC/Routines/RoutineException.java
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/QC/Routines/RoutineException.java
@@ -7,25 +7,25 @@ package uk.ac.exeter.QuinCe.data.Dataset.QC.Routines;
  */
 public class RoutineException extends Exception {
 
-	/**
-	 * The serial version UID
-	 */
-	private static final long serialVersionUID = -9197093855452751110L;
+  /**
+   * The serial version UID
+   */
+  private static final long serialVersionUID = -9197093855452751110L;
 
-	/**
-	 * Simple constructor for a basic error message
-	 * @param message The error message
-	 */
-	public RoutineException(String message) {
-		super(message);
-	}
-	
-	/**
-	 * Constructor for an error with an underlying cause
-	 * @param message The error message
-	 * @param cause The underlying cause
-	 */
-	public RoutineException(String message, Throwable cause) {
-		super(message, cause);
-	}
+  /**
+   * Simple constructor for a basic error message
+   * @param message The error message
+   */
+  public RoutineException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructor for an error with an underlying cause
+   * @param message The error message
+   * @param cause The underlying cause
+   */
+  public RoutineException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
