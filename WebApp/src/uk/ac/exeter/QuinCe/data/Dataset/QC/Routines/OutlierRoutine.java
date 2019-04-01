48bda96d8f9df1cec2ead26077fceefba5bb22bf
Steve Jones <steve@squaregoldfish.co.uk>
2019-04-01 16:56:37 +0200

    (#1121) Migrated all QC routines that we need right now

---
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/QC/Routines/OutlierRoutine.java
@@ -0,0 +1,91 @@
+package uk.ac.exeter.QuinCe.data.Dataset.QC.Routines;
+
+import java.util.List;
+
+import uk.ac.exeter.QuinCe.data.Dataset.SensorValue;
+import uk.ac.exeter.QuinCe.data.Dataset.QC.Flag;
+
+public class OutlierRoutine extends Routine {
+
+  /**
+   * The maximum number of standard deviations away from the mean a
+   * value can be before it is considered an outlier.
+   */
+  private double stdevLimit;
+
+  /**
+   * Basic constructor
+   * @param parameters The parameters
+   * @throws QCRoutinesConfigurationException If the parameters are invalid
+   */
+  public OutlierRoutine(List<String> parameters)
+    throws RoutineException {
+    super(parameters);
+  }
+
+  @Override
+  protected void validateParameters() throws RoutineException {
+    if (parameters.size() != 1) {
+      throw new RoutineException("Incorrect number of parameters. Must be <stdevLimit>");
+    }
+
+    try {
+      stdevLimit = Double.parseDouble(parameters.get(0));
+    } catch (NumberFormatException e) {
+      throw new RoutineException("Standard deviation limit parameter must be numeric");
+    }
+
+    if (stdevLimit <= 0) {
+      throw new RoutineException("Standard deviation limit must be greater than zero");
+    }
+  }
+
+  @Override
+  public void qcValues(List<SensorValue> values) throws RoutineException {
+
+    int valueCount = 0;
+    double mean = 0.0;
+    double stdev = 0.0;
+
+    for (SensorValue sensorValue: values) {
+      Double value = sensorValue.getDoubleValue();
+      if (!value.isNaN()) {
+        valueCount++;
+
+        if (valueCount == 1) {
+          mean = value;
+        } else {
+          double d = value - mean;
+          stdev += (valueCount - 1)*d*d/valueCount;
+          mean += d/valueCount;
+        }
+      }
+    }
+
+    if (valueCount > 0) {
+      // Finalise the stdev calculation
+      stdev = Math.sqrt(stdev / valueCount);
+
+      // Check all values to see if they're outside the limit
+      for (SensorValue sensorValue : values) {
+        if (!sensorValue.isNaN()) {
+          double diffFromMean = Math.abs(sensorValue.getDoubleValue() - mean);
+
+          if (diffFromMean > (stdev * stdevLimit)) {
+            addFlag(sensorValue, Flag.BAD, stdevLimit, stdev);
+          }
+        }
+      }
+    }
+  }
+
+  @Override
+  public String getShortMessage() {
+    return "Standard deviation is too large";
+  }
+
+  @Override
+  public String getLongMessage(String requiredValue, String actualValue) {
+    return "Standard deviation is " + actualValue + ", should be <= " + requiredValue;
+  }
+}
