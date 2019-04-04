91e9824a8a27fa40873ee5eb839782cb7ee03370
Steve Jones <steve@squaregoldfish.co.uk>
2019-04-04 08:28:25 +0200

    (#1121) Finalise SQL migrations; Fix JUnit tests

--- WebApp/junit/resources/sql/testbase/instrument/V1000001__instruments.sql
+++ WebApp/junit/resources/sql/testbase/instrument/V1000001__instruments.sql
@@ -18,20 +18,20 @@ INSERT INTO file_definition VALUES
 -- File Columns
 
 -- Intake temperature   
-INSERT INTO file_column VALUES (1,1,14,1,1,'SWTemp',0,0,'','2019-01-28 13:31:21','2019-01-28 14:31:21');
+INSERT INTO file_column VALUES (1,1,14,1,1,'SWTemp',0,'','2019-01-28 13:31:21','2019-01-28 14:31:21');
 
 -- Salinity
-INSERT INTO file_column VALUES (2,1,63,1,2,'SBSal',1,0,'','2019-01-28 13:31:21','2019-01-28 14:31:21');
+INSERT INTO file_column VALUES (2,1,63,1,2,'SBSal',0,'','2019-01-28 13:31:21','2019-01-28 14:31:21');
 
 -- Equilibrator Temperature
-INSERT INTO file_column VALUES (3,1,19,1,3,'PT100_1',2,0,'','2019-01-28 13:31:21','2019-01-28 14:31:21');
+INSERT INTO file_column VALUES (3,1,19,1,3,'PT100_1',0,'','2019-01-28 13:31:21','2019-01-28 14:31:21');
 
 -- Equilibrator Pressure (absolute)
-INSERT INTO file_column VALUES (4,1,36,1,5,'Pres2',3,0,'','2019-01-28 13:31:21','2019-01-28 14:31:21');
+INSERT INTO file_column VALUES (4,1,36,1,5,'Pres2',0,'','2019-01-28 13:31:21','2019-01-28 14:31:21');
 
 -- CO2
-INSERT INTO file_column VALUES (5,1,40,1,9,'LCO2D',4,0,'','2019-01-28 13:31:21','2019-01-28 14:31:21');
+INSERT INTO file_column VALUES (5,1,40,1,9,'LCO2D',0,'','2019-01-28 13:31:21','2019-01-28 14:31:21');
 
 -- Run Type
-INSERT INTO file_column VALUES (6,1,2,1,-1,'Run Type',5,0,NULL,'2019-01-28 13:31:21','2019-01-28 14:31:21');
+INSERT INTO file_column VALUES (6,1,2,1,-1,'Run Type',0,NULL,'2019-01-28 13:31:21','2019-01-28 14:31:21');
 
