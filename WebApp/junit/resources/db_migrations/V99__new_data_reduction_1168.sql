be5869d685ccb64d040ba6ea231acabea996ba0b
Steve Jones <steve@squaregoldfish.co.uk>
2019-04-10 10:00:34 +0200

    (#1168) Stub ChooseSensorValues job

--- WebApp/junit/resources/db_migrations/V99__new_data_reduction_1168.sql
+++ WebApp/junit/resources/db_migrations/V99__new_data_reduction_1168.sql
@@ -12,4 +12,21 @@ CREATE TABLE measurements (
     ON DELETE RESTRICT
     ON UPDATE RESTRICT);
 
-CREATE INDEX measurement_dataset_idx ON measurements(`dataset_id` ASC);
+CREATE INDEX measurement_dataset_idx ON measurements(dataset_id ASC);
+
+CREATE TABLE measurement_values (
+  measurement_id BIGINT(20) NOT NULL,
+  sensor_value_id BIGINT(20) NOT NULL,
+  PRIMARY KEY (measurement_id, sensor_value_id),
+  CONSTRAINT MEASVAL_MEASUREMENT
+    FOREIGN KEY (measurement_id)
+    REFERENCES measurements (id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
+ CONSTRAINT MEASVAL_SENSORVALUE
+    FOREIGN KEY (sensor_value_id)
+    REFERENCES sensor_values (id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT);
+
+CREATE INDEX measval_measurements_idx ON measurement_values(measurement_id ASC);
