caeaf3203a635051b1f53195402713783417d2f3
Steve Jones <steve@squaregoldfish.co.uk>
2019-04-10 10:00:34 +0200

    (#1168) Stub ChooseSensorValues job

--- WebApp/src/uk/ac/exeter/QuinCe/jobs/files/ChooseSensorValuesJob.java
+++ WebApp/src/uk/ac/exeter/QuinCe/jobs/files/ChooseSensorValuesJob.java
@@ -1,26 +1,15 @@
 package uk.ac.exeter.QuinCe.jobs.files;
 
 import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
-import uk.ac.exeter.QCRoutines.messages.Flag;
-import uk.ac.exeter.QCRoutines.messages.Message;
 import uk.ac.exeter.QuinCe.data.Dataset.DataSet;
 import uk.ac.exeter.QuinCe.data.Dataset.DataSetDB;
-import uk.ac.exeter.QuinCe.data.Dataset.DataSetDataDB;
-import uk.ac.exeter.QuinCe.data.Dataset.SensorValue;
-import uk.ac.exeter.QuinCe.data.Dataset.QC.Routines.QCRoutinesConfiguration;
-import uk.ac.exeter.QuinCe.data.Dataset.QC.Routines.Routine;
 import uk.ac.exeter.QuinCe.data.Instrument.Instrument;
 import uk.ac.exeter.QuinCe.data.Instrument.InstrumentDB;
-import uk.ac.exeter.QuinCe.data.Instrument.SensorDefinition.SensorAssignments;
-import uk.ac.exeter.QuinCe.data.Instrument.SensorDefinition.SensorType;
 import uk.ac.exeter.QuinCe.jobs.InvalidJobParametersException;
 import uk.ac.exeter.QuinCe.jobs.Job;
 import uk.ac.exeter.QuinCe.jobs.JobFailedException;
@@ -33,35 +22,15 @@ import uk.ac.exeter.QuinCe.utils.RecordNotFoundException;
 import uk.ac.exeter.QuinCe.web.system.ResourceManager;
 
 /**
- * <p>
- *   This {@link Job} class runs a set of QC routines on the sensor values for
- *   a given data set.
- * </p>
- *
- * <p>
- *   Once the QC has been completed, the QC Flag and QC Message are set. The QC flag will be set to {@link Flag#GOOD},
- *   {@link Flag#QUESTIONABLE} or {@link Flag#BAD}. In the latter two cases, the QC Message will contain details of the fault(s)
- *   that were found.
- * </p>
- *
- * <p>
- *   If the QC flag was set to {@link Flag#GOOD}, the WOCE flag for the record will be set to {@link Flag#ASSUMED_GOOD},
- *   to indicate that the software will assume that the record is good unless the user overrides it. Otherwise
- *   the WOCE Flag will be set to {@link Flag#NEEDED}. The user will be required to manually choose a value for the WOCE
- *   Flag, either by accepting the suggestion from the QC job, or overriding the flag and choosing their own. The WOCE
- *   Comment will default to being identical to the QC Message, but this can also be changed if required.
- * </p>
- *
- * <p>
- *   If the {@code AutoQCJob} has been run before, some WOCE Flags and Comments will have already been set by the user.
- *   If the user QC flag is anything other than {@link Flag#ASSUMED_GOOD} or {@link Flag#NEEDED}, it will not be checked.
- * </p>
+ * Job to locate the SensorValues to use for each measurement extracted
+ * in the LocateMeasurementsJob. At the moment this simply finds one
+ * SensorValue of each type with the same timestamp as the measurement.
+ * In future it can get more sophisticated.
  *
  * @author Steve Jones
- * @see Flag
- * @see Message
+ *
  */
-public class AutoQCJob extends Job {
+public class ChooseSensorValuesJob extends Job {
 
   /**
    * The parameter name for the data set id
@@ -71,7 +40,7 @@ public class AutoQCJob extends Job {
   /**
    * Name of the job, used for reporting
    */
-  private final String jobName = "Automatic Quality Control";
+  private final String jobName = "Choose Sensor Values";
 
   /**
    * The data set being processed by the job
@@ -95,18 +64,15 @@ public class AutoQCJob extends Job {
    * @throws RecordNotFoundException If any required database records are missing
    * @see JobManager#getNextJob(ResourceManager, Properties)
    */
-  public AutoQCJob(ResourceManager resourceManager, Properties config, long jobId, Map<String, String> parameters) throws MissingParamException, InvalidJobParametersException, DatabaseException, RecordNotFoundException {
+  public ChooseSensorValuesJob(ResourceManager resourceManager, Properties config,
+    long jobId, Map<String, String> parameters)
+      throws MissingParamException, InvalidJobParametersException,
+        DatabaseException, RecordNotFoundException {
     super(resourceManager, config, jobId, parameters);
   }
 
-  /**
-   * Runs the configured QC routines on the file specified in the job parameters.
-   * @param thread The thread that is running this job
-   * @see FileJob#FILE_ID_KEY
-   */
   @Override
   protected void execute(JobThread thread) throws JobFailedException {
-
     Connection conn = null;
 
     try {
@@ -120,37 +86,19 @@ public class AutoQCJob extends Job {
         ResourceManager.getInstance().getSensorsConfiguration(),
         ResourceManager.getInstance().getRunTypeCategoryConfiguration());
 
-      SensorAssignments sensorAssignments = instrument.getSensorAssignments();
 
-      QCRoutinesConfiguration qcRoutinesConfig =
-        ResourceManager.getInstance().getQCRoutinesConfiguration();
 
-      // Get the sensor values grouped by data file column
-      Map<Long, List<SensorValue>> sensorValues =
-        DataSetDataDB.getSensorValuesByColumn(conn, dataSet.getId());
-
-      // Run the routines for each column
-      for (Map.Entry<Long, List<SensorValue>> entry : sensorValues.entrySet()) {
-        SensorType sensorType = sensorAssignments.getSensorTypeForDBColumn(entry.getKey());
-        for (Routine routine : qcRoutinesConfig.getRoutines(sensorType)) {
-          routine.qcValues(entry.getValue());
-        }
-      }
-
-      // Store all the sensor values
-      List<SensorValue> allValues = new ArrayList<SensorValue>();
-      sensorValues.values().forEach(allValues::addAll);
-      DataSetDataDB.storeSensorValues(conn, allValues);
 
       // Trigger the Build Measurements job
+      /*
       dataSet.setStatus(DataSet.STATUS_DATA_REDUCTION);
       DataSetDB.updateDataSet(conn, dataSet);
       Map<String, String> jobParams = new HashMap<String, String>();
       jobParams.put(LocateMeasurementsJob.ID_PARAM, String.valueOf(Long.parseLong(parameters.get(ID_PARAM))));
-      JobManager.addJob(dataSource, JobManager.getJobOwner(dataSource, id), LocateMeasurementsJob.class.getCanonicalName(), jobParams);
+      JobManager.addJob(dataSource, JobManager.getJobOwner(dataSource, id), ChooseSensorValuesJob.class.getCanonicalName(), jobParams);
+      */
 
       conn.commit();
-
     } catch (Exception e) {
       e.printStackTrace();
       DatabaseUtils.rollBack(conn);
@@ -172,11 +120,13 @@ public class AutoQCJob extends Job {
     } finally {
       DatabaseUtils.closeConnection(conn);
     }
+
   }
 
   @Override
   protected void validateParameters() throws InvalidJobParametersException {
     // TODO Auto-generated method stub
+
   }
 
   @Override
