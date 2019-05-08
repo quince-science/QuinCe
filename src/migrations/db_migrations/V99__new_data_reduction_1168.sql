249261dcbd2211a368617d372712d09076f830fb
Steve Jones <steve@squaregoldfish.co.uk>
2019-05-08 18:49:45 +0200

    Data reduction results stored in database

--- src/migrations/db_migrations/V99__new_data_reduction_1168.sql
+++ src/migrations/db_migrations/V99__new_data_reduction_1168.sql
@@ -17,19 +17,25 @@ CREATE TABLE measurements (
 
 CREATE TABLE measurement_values (
   measurement_id BIGINT(20) NOT NULL,
+  variable_id INT(11) NOT NULL,
   sensor_value_id BIGINT(20) NOT NULL,
-  PRIMARY KEY (measurement_id, sensor_value_id),
+  PRIMARY KEY (measurement_id, variable_id, sensor_value_id),
   INDEX MEASVAL_MEASUREMENT_idx (measurement_id ASC),
   CONSTRAINT MEASVAL_MEASUREMENT
     FOREIGN KEY (measurement_id)
     REFERENCES measurements (id)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT,
+  CONSTRAINT MEASVAL_VARIABLE
+    FOREIGN KEY (variable_id)
+    REFERENCES variables (id)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
   CONSTRAINT MEASVAL_SENSORVALUE
     FOREIGN KEY (sensor_value_id)
     REFERENCES sensor_values (id)
     ON DELETE RESTRICT
-    ON UPDATE NO ACTION);
+    ON UPDATE RESTRICT);
 
 CREATE TABLE data_reduction (
   measurement_id BIGINT(20) NOT NULL,
