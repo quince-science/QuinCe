cf906ac8fc3d52c24d297659e3f7f0bf78b3cfe3
Steve Jones <steve@squaregoldfish.co.uk>
2019-05-16 20:46:12 +0200

    (qc) Fix delete_instrument script for new database structure

--- scripts/database_maintenance/delete_instrument.sh
+++ scripts/database_maintenance/delete_instrument.sh
@@ -71,9 +71,9 @@ then
   read -ra file_def_ids <<< `mysql -u$db_user -p"$db_password" $db_name -B --skip-column-names -e "select id from file_definition where instrument_id = $instrument_id"`
 
   mysql -u$db_user -p"$db_password" $db_name <<EOF
-    DELETE FROM equilibrator_pco2 WHERE measurement_id IN (SELECT id FROM dataset_data WHERE dataset_id IN (SELECT id FROM dataset WHERE instrument_id = $instrument_id));
-    -- DELETE FROM dataset_data WHERE dataset_id IN (SELECT id FROM dataset WHERE instrument_id = $instrument_id);
-    -- DELETE FROM calibration_data WHERE dataset_id IN (SELECT id FROM dataset WHERE instrument_id = $instrument_id);
+    DELETE FROM data_reduction WHERE measurement_id IN (SELECT id FROM measurements WHERE dataset_id IN (SELECT id FROM dataset WHERE instrument_id = $instrument_id));
+    DELETE FROM measurement_values WHERE measurement_id IN (SELECT id FROM measurements WHERE dataset_id IN (SELECT id FROM dataset WHERE instrument_id = $instrument_id));
+    DELETE FROM measurements WHERE dataset_id IN (SELECT id FROM dataset WHERE instrument_id = $instrument_id);
 
     DELETE FROM sensor_values WHERE dataset_id IN (SELECT id FROM dataset WHERE instrument_id = $instrument_id);
 
