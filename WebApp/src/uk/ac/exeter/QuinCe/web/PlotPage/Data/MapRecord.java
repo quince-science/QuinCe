2b7a6efb8ca7c88051230230bd61db0c4407cfda
Steve Jones <steve@squaregoldfish.co.uk>
2019-06-13 18:34:54 +0100

    (qc) Maps working - mostly...

---
+++ WebApp/src/uk/ac/exeter/QuinCe/web/PlotPage/Data/MapRecord.java
@@ -0,0 +1,35 @@
+package uk.ac.exeter.QuinCe.web.PlotPage.Data;
+
+import org.primefaces.json.JSONArray;
+
+import uk.ac.exeter.QuinCe.data.Dataset.Position;
+import uk.ac.exeter.QuinCe.utils.MathUtils;
+import uk.ac.exeter.QuinCe.web.PlotPage.FieldValue;
+
+public class MapRecord implements Comparable<MapRecord> {
+
+  protected final Position position;
+  protected final long id;
+  protected final FieldValue value;
+
+  protected MapRecord(Position position, long id, FieldValue value) {
+    this.position = position;
+    this.id = id;
+    this.value = value;
+  }
+
+  @Override
+  public int compareTo(MapRecord o) {
+    return Long.compare(id, o.id);
+  }
+
+  public JSONArray getJsonArray() {
+    JSONArray json = new JSONArray();
+    json.put(MathUtils.round(position.lon, 3)); // Lon
+    json.put(MathUtils.round(position.lat, 3)); // Lat
+    json.put(id); // Date/Time (doubles as ID)
+    json.put(value.getQcFlag().getFlagValue()); // Flag
+    json.put(MathUtils.round(value.getValue(), 3)); // Value
+    return json;
+  }
+}
