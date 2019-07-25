155f4e87f8a5bb99cb40cc394dcd2c92d17ee894
Steve Jones <steve@squaregoldfish.co.uk>
2019-07-25 15:49:26 +0200

    (#1275) Fix old SQL migrations

--- src/migrations/db_migrations/V100__dataset_bounds.sql
+++ src/migrations/db_migrations/V100__dataset_bounds.sql
@@ -1,5 +1,4 @@
-ALTER TABLE `quince_v2`.`dataset` 
-ADD COLUMN `min_longitude` DOUBLE NULL AFTER `end`,
-ADD COLUMN `max_longitude` DOUBLE NULL AFTER `min_longitude`,
-ADD COLUMN `min_latitude` DOUBLE NULL AFTER `max_longitude`,
-ADD COLUMN `max_latitude` DOUBLE NULL AFTER `min_latitude`;
+ALTER TABLE dataset ADD COLUMN min_longitude DOUBLE NULL AFTER `end`;
+ALTER TABLE dataset ADD COLUMN max_longitude DOUBLE NULL AFTER min_longitude;
+ALTER TABLE dataset ADD COLUMN min_latitude DOUBLE NULL AFTER max_longitude;
+ALTER TABLE dataset ADD COLUMN max_latitude DOUBLE NULL AFTER min_latitude;
