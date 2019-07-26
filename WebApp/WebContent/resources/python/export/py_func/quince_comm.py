01d38e3c0147af3c0fd256238d3289a1d44f6eb3
Maren Kjos Karlsen <maren.kjos@gmail.com>
2019-07-26 13:51:40 +0200

    (#1192) Added Carbon Portal Export functionality and documentation

--- WebApp/WebContent/resources/python/export/py_func/quince_comm.py
+++ WebApp/WebContent/resources/python/export/py_func/quince_comm.py
@@ -5,6 +5,7 @@ import toml
 import json
 
 def make_quince_call(config, call, dataset_id=-1):
+    ''' sends request to QuinCe'''
     quince_url = config["QuinCe"]["url"]
     user = config["QuinCe"]["user"]
     password = config["QuinCe"]["password"]
@@ -32,24 +33,25 @@ def make_quince_call(config, call, dataset_id=-1):
     return quince_response
 
 def get_export_list(config):
-    '''
-    retrieves list of datasets ready for export from QuinCe. config file 
-    is .toml-file containing log-in information.
+    ''' Retrieves list of datasets ready for export from QuinCe. 
 
+    config: .toml-file containing log-in information.
     returns: array containing name, instrument and id for each dataset 
     ready to be downloaded.
     '''
     logging.info('Retrieving exportList  from QuinCe')
 
     export_list = make_quince_call(config,'exportList')
-    logging.info('{} dataset(s) ready for export'.format(export_list.decode('utf8').count('id')))
+    logging.info('{} dataset(s) ready for export'.format(
+        export_list.decode('utf8').count('id')))
 
     return json.loads(export_list.decode('utf-8'))
 
 def get_export_dataset(config,dataset_id):
-    '''
-    Retrieves .zip fil from QuinCe containing metadata, raw data and 
-    datasets associated with dataset-id retrieved using the 
+    ''' Retrieves .zip file from QuinCe 
+
+    zip-file contains metadata, raw data and datasets 
+    associated with dataset-id retrieved using the 
     'get_exportList' function config is login information. id is 
     QuinCe-id associated with our desired dataset.
     returns .zipfile.
@@ -63,29 +65,15 @@ def get_export_dataset(config,dataset_id):
     return export_dataset
 
 def report_complete_export(config,dataset_id):
-    '''
-    Reports to QuinCe that export was successful for dataset with 
-    id: dataset_id
-    '''
+    ''' Reports to successful export '''
     logging.info(f'Export complete for dataset with QuinCe id: {dataset_id}')
     complete_export = make_quince_call(config, 'completeExport', dataset_id)
 
 def report_abandon_export(config,dataset_id):
-    '''
-    Reports to QuinCe that export was unsuccessful for dataset with 
-    id: dataset_id, and that it should be returned to the list of sets 
-    ready for export.
-    '''
-    logging.info(
-        'Abandoning export of dataset with QuinCe id: {:d}'
-        .format(dataset_id))
-     
+    ''' Reports unsuccessful export '''
+    logging.info(f'Abandoning export of dataset with QuinCe id: {dataset_id}')
     abandon_export = make_quince_call(config, 'abandonExport', dataset_id)
 
-
 def report_touch_export(config,dataset_id):
-    '''
-    Reports to QuinCe that work is still being to dataset_id and to not 
-    returns the dataset to list of sets ready for export. 
-    '''    
+    ''' Reports still processing to QuinCe  '''    
     touch_export = make_quince_call(config, 'touchExport', dataset_id)
