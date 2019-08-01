a6101ac0a5dc02bf8934f76168821198082b0de1
Steve Jones <steve@squaregoldfish.co.uk>
2019-08-01 14:05:47 +0100

    (#1275) Exporting works. Tweaks to be done.

---
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/CalculationParameter.java
@@ -0,0 +1,33 @@
+package uk.ac.exeter.QuinCe.data.Dataset.DataReduction;
+
+import uk.ac.exeter.QuinCe.data.Export.ColumnHeader;
+
+public class CalculationParameter {
+
+  private String name;
+
+  private ColumnHeader columnHeader;
+
+  private boolean result;
+
+  public CalculationParameter(String name, String columnName,
+    String columnCode, String units, boolean result) {
+
+    this.name = name;
+    this.columnHeader = new ColumnHeader(columnName, columnCode, units);
+    this.result = result;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public ColumnHeader getColumnHeader() {
+    return columnHeader;
+  }
+
+  public boolean isResult() {
+    return result;
+  }
+
+}
