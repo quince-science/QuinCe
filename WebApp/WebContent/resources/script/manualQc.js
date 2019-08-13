c58254d65c2bb39bc3b0ae116debcc7e63028034
Steve Jones <steve@squaregoldfish.co.uk>
2019-08-13 10:03:49 +0100

    (#1306) Fix UI handling for NRT/non-NRT datasets

--- WebApp/WebContent/resources/script/manualQc.js
+++ WebApp/WebContent/resources/script/manualQc.js
@@ -235,8 +235,8 @@ function disablePlotSelect(index) {
   // TODO This works by messing with the CSS, because the version
   // of PrimeFaces we're using doesn't work properly.
   // Sort it out when we upgrade.
-  
-  if (!nrt()) { 
+
+  if (!nrt()) {
     PF('plot' + index + 'SelectMode').jq.find('input:radio[value="zoom"]').trigger('click');
     PF('plot' + index + 'SelectMode').buttons.eq(1).addClass('ui-state-disabled');
   }
