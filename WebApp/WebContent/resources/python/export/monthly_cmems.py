ee4042d052a2e803a95a25a01c7828639910cef4
Maren Kjos Karlsen <maren.kjos@gmail.com>
2019-09-09 14:00:54 +0200

    Fixed logging functionality, Carbon export

--- WebApp/WebContent/resources/python/export/monthly_cmems.py
+++ WebApp/WebContent/resources/python/export/monthly_cmems.py
@@ -12,39 +12,45 @@ import netCDF4
 import os
 import numpy as np
 import datetime
+import logging
+import sys
 
-month = 'gosars_july' 
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+
+yearMonth = 201908
+vessel = 'LMEL' #G.O. Sars
+#vessel = 'OXYH2' #Nuka Arctica
+directory = '/home/mve036/Desktop/201908_LMEL' 
 dim_tot = {}
-# Get list of daily netCDF files
-for root, dirs, files in os.walk('latest/' + month, topdown=False):
+
+logging.info(f'Creating monthly netCDF file  for {vessel}, {yearMonth}')
+
+logging.debug('Retrieving list of daily netCDF files')
+for root, dirs, files in os.walk( directory, topdown=False):
     dim_tot=0
     for name in sorted(files):
-        #print(name)
+        logging.debug(f'reading filename: {name}')
         dataset = netCDF4.Dataset(os.path.join(root, name))
         dim_depth = len(dataset.dimensions['DEPTH'])
         dim_time = len(dataset.dimensions['TIME'])
         dim_lat = len(dataset.dimensions['LATITUDE'])
         dim_lon = len(dataset.dimensions['LONGITUDE'])
         dim_pos = len(dataset.dimensions['POSITION'])
-        #print(dim_tot[root], dim_time)
-
         dim_tot += dim_time
 
-
-
-# Create new empty monthly file
-nc_name = 'latest/'+month + '.nc'
+nc_name = 'GL_' + str(yearMonth) + '_TS_TS_'  + vessel + '.nc'
+logging.debug(f'Creating new empty monthly file: {nc_name}')
 dataset_m = netCDF4.Dataset(nc_name,'w',format='NETCDF4_CLASSIC')
 
-# Assign dimensions
+logging.debug('Assigning dimensions')
 depth_dim = dataset_m.createDimension('DEPTH',1)
 time_dim = dataset_m.createDimension('TIME',dim_tot)
 lat_dim = dataset_m.createDimension('LATITUDE',dim_tot)
 lon_dim = dataset_m.createDimension('LONGITUDE',dim_tot)
 pos_dim = dataset_m.createDimension('POSITION',dim_tot)
 
-
-# Creating variables
+logging.debug('Assigning attributes/variables')
 variables = dataset.variables.keys()
 for var in variables:
     fill = dataset[var]._FillValue
@@ -56,23 +62,19 @@ for var in variables:
         attr_val = dataset[var].getncattr(attr)
         set_attr[attr] = attr_val
     variable.setncatts(set_attr)
-    #print(variable.ncattrs())
 
-# populate file
-for root, dirs, files in os.walk('latest/' + month, topdown=False):
+logging.debug('populate file')
+for root, dirs, files in os.walk(directory, topdown=False):
     start = {}
     end = {}
     for var in dataset.variables.keys():        
-        #print(root, name, var)
         start[var]=0
         for file in sorted(files):
-            #print(name, var)
             dataset_d = netCDF4.Dataset(os.path.join(root, file))
             dim_len = len(dataset_d[var].dimensions) 
             if dim_len == 1:
                 array = dataset_d[var][:]
                 end[var] = start[var] + len(array)
-                #print('s:i:e',start[var],end[var],len(array))
                 dataset_m[var][start[var]:end[var]] = array 
                 start[var] = end[var]
             elif dim_len == 2:
@@ -84,7 +86,6 @@ for root, dirs, files in os.walk('latest/' + month, topdown=False):
 set_gattr = {}
 for gattr in dataset.ncattrs():
     set_gattr[gattr] = dataset.getncattr(gattr)
-    #print(gattr,' : ', dataset.getncattr(gattr))
 
 start_date = (datetime.datetime(1950,1,1,0,0) + datetime.timedelta(min(dataset_m['TIME'][:]))).strftime("%Y-%m-%dT%H:%M:%SZ")
 end_date = (datetime.datetime(1950,1,1,0,0) + datetime.timedelta(max(dataset_m['TIME'][:]))).strftime("%Y-%m-%dT%H:%M:%SZ")
@@ -96,5 +97,6 @@ set_gattr['time_coverage_start'] = start_date
 set_gattr['time_coverage_end'] = end_date
 set_gattr['update_interval']='void'
 dataset_m.setncatts(set_gattr)
-
 dataset_m.close()
+
+logging.info('nc-file complete')
