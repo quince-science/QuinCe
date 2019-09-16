543ad6532a17b6191c36b79fc5ed45797ac1499e
Steve Jones <steve@squaregoldfish.co.uk>
2019-09-16 16:31:14 +0200

    (#1356) Improve database queries for data reduction data
    Restrict the query to only search for the specified dates; Add a dataset ID/date index to the measurements table

--- src/migrations/db_migrations/V99__performance_for_plot_pages_1356.sql
+++ src/migrations/db_migrations/V99__performance_for_plot_pages_1356.sql
@@ -1,8 +1,10 @@
 CREATE INDEX USERQCFLAG ON sensor_values (user_qc_flag);
 CREATE INDEX DATASETID_DATE ON sensor_values (dataset_id, date);
+CREATE INDEX DATASETID_DATE ON measurements (dataset_id, date);
 
 
 -- ROLLBACK
 
 -- DROP INDEX USERQCFLAG ON sensor_values;
 -- DROP INDEX DATASETID_DATE ON sensor_values;
+-- DROP INDEX DATASETID_DATE ON measurements;
