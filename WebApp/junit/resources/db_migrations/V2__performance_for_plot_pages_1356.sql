ec95106b7e88bb772b38f5780549c859e261e2b9
Steve Jones <steve@squaregoldfish.co.uk>
2019-09-26 13:46:34 +0200

    (#hotfix) Fix database migration for H2 junit database

--- WebApp/junit/resources/db_migrations/V2__performance_for_plot_pages_1356.sql
+++ WebApp/junit/resources/db_migrations/V2__performance_for_plot_pages_1356.sql
@@ -1,12 +1,12 @@
 CREATE INDEX USERQCFLAG ON sensor_values (user_qc_flag);
-CREATE INDEX DATASETID_DATE ON sensor_values (dataset_id, date);
+CREATE INDEX SV_DATASETID_DATE ON sensor_values (dataset_id, date);
 CREATE INDEX FILECOLUMN ON sensor_values (file_column);
-CREATE INDEX DATASETID_DATE ON measurements (dataset_id, date);
+CREATE INDEX MEAS_DATASETID_DATE ON measurements (dataset_id, date);
 
 
 -- ROLLBACK
 
 -- DROP INDEX USERQCFLAG ON sensor_values;
--- DROP INDEX DATASETID_DATE ON sensor_values;
+-- DROP INDEX SV_DATASETID_DATE ON sensor_values;
 -- DROP INDEX FILECOLUMN ON sensor_values;
--- DROP INDEX DATASETID_DATE ON measurements;
+-- DROP INDEX MEAS_DATASETID_DATE ON measurements;
