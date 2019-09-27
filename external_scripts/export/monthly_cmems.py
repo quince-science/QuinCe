816eab01af510caa84b5f6ddd3fe78bbfa5bf1c8
Maren <maren.kjos@gmail.com>
2019-09-27 10:07:41 +0200

    (#1413) Structure outlined

--- external_scripts/export/monthly_cmems.py
+++ external_scripts/export/monthly_cmems.py
@@ -8,52 +8,71 @@ Populates new netCDF file.
 
 '''
 
-import netCDF4
-import os
-import numpy as np
-import datetime
 import logging
+import os
 import sys
+import datetime
+import numpy as np
+import netCDF4
+import sqlite3
+import ftputil 
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 curr_month = datetime.datetime.today().strftime('%Y%m') 
 vesselnames= {'LMEL': 'G.O.Sars','OXYH2':'Nuka Arctica'} 
 vessels = ['LMEL', 'OXYH2']
-directory = 'latest' 
+source_dir = 'latest' 
 dim_tot = {}
 file_nr = {}
 daily_files = {}
 
+cmems_db = 'files_cmems.db'
+
+dnt_datetime_format = '%Y%m%dT%H%M%SZ'
+
+server_location = 'ftp://nrt.cmems-du.eu/Core'
+
+product_id = 'INSITU_GLO_CARBON_NRT_OBSERVATIONS_013_049'
+nc_dir = '/' + product_id + '/NRT_201904/monthly'
+dnt_dir = '/' + product_id + '/DNT'
+index_dir = '/' + product_id + '/NRT_201904'
+local_folder = 'monthly'
+
+
 def main():
   logging.debug('Retrieving list of daily netCDF files')
   for vessel in vessels:
-    daily_files[vessel], file_nr[vessel], dataset, dim_tot = (get_daily_files(directory,curr_month,vessel))
+    daily_files[vessel], file_nr[vessel], dataset, dim_tot = (get_daily_files(source_dir,curr_month,vessel))
     if file_nr[vessel] > 0:
       logging.info(f'Creating monthly netCDF file for {vesselnames[vessel]} [{vessel}], month: {curr_month}')
-      dataset_m = create_empty_dataset(curr_month,vessel,dim_tot)
+      nc_name, dataset_m = create_empty_dataset(curr_month,vessel,dim_tot)
       dataset_m = assign_attributes(dataset,dataset_m)
-      dataset_m = populate_netCDF(dataset,dataset_m,daily_files[vessel],directory)
+      dataset_m = populate_netCDF(dataset,dataset_m,daily_files[vessel],source_dir)
       dataset_m = set_global_attributes(dataset,dataset_m)
       dataset_m.close()
       logging.info(f'Monthly netCDF file for {vesselnames[vessel]} completed')
 
-      # Create Index file
+      # Create SQL entry
 
-      # Create DNT file
+  # Add to SQL database
 
-      # Upload files to CMEMS-FTP
+  # Create Index file
+
+  # Create DNT file
+
+  # Upload files to CMEMS-FTP
 
 
 
-def get_daily_files(directory,curr_month,vessel):
-  '''Fetches applicable daily files from directory
+def get_daily_files(source_dir,curr_month,vessel):
+  '''Fetches applicable daily files from source_dir
   returns filenames, number of files, last dataset 
   and combined dimension of all files
   '''
   file_nr = 0
   filenames =[]
-  for root, dirs, files in os.walk( directory, topdown=False):
+  for root, dirs, files in os.walk( source_dir, topdown=False):
     dim_tot=0
     for name in sorted(files):
       if curr_month in name and vessel in name:
@@ -85,7 +104,7 @@ def create_empty_dataset(curr_month,vessel,dim_tot):
   lon_dim = dataset_m.createDimension('LONGITUDE',dim_tot)
   pos_dim = dataset_m.createDimension('POSITION',dim_tot)
 
-  return dataset_m
+  return nc_name,dataset_m
 
 def assign_attributes(dataset,dataset_m):
   ''' Assigns attributes to dataset_m
@@ -106,7 +125,7 @@ def assign_attributes(dataset,dataset_m):
   return dataset_m
       
 
-def populate_netCDF(dataset,dataset_m,daily_files,directory):
+def populate_netCDF(dataset,dataset_m,daily_files,source_dir):
   ''' For each variable in each daily netCDF file:
   extract data from daily file
   save data to monthly dataset.
@@ -117,7 +136,7 @@ def populate_netCDF(dataset,dataset_m,daily_files,directory):
   for var in dataset.variables.keys():        
     start[var]=0
     for file in sorted(daily_files):
-      dataset_d = netCDF4.Dataset(os.path.join(directory, file))
+      dataset_d = netCDF4.Dataset(os.path.join(source_dir, file))
       dim_len = len(dataset_d[var].dimensions) 
       if dim_len == 1:
         array = dataset_d[var][:]
@@ -155,6 +174,114 @@ def set_global_attributes(dataset,dataset_m):
   dataset_m.setncatts(set_gattr)
 
   return dataset_m
+
+def build_index(results_uploaded):
+  '''
+  Creates index-file over CMEMS source_dir.
+
+  Lists all files currently uploaded to the CMEMS server. 
+  '''
+
+  date_header = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+
+  index_header = ('# Title : Carbon in-situ observations catalog \n'\
+    + '# Description : catalog of available in-situ observations per platform.\n'\
+    + '# Project : Copernicus \n# Format version : 1.0 \n'\
+    + '# Date of update : ' + date_header +'\n'
+    + '# catalog_id,file_name,geospatial_lat_min,geospatial_lat_max,'\
+    + 'geospatial_lon_min,geospatial_lon_max,time_coverage_start,'\
+    + 'time_coverage_end,provider,date_update,data_mode,parameters\n')
+
+  index_info = ''
+  for file in results_uploaded:
+    local_filepath = file[2]
+    ftp_filepath = file[6]
+
+    nc = netCDF4.Dataset(local_filepath,mode='r')
+
+    lat_min = nc.geospatial_lat_min
+    lat_max = nc.geospatial_lat_max
+    lon_min = nc.geospatial_lon_min
+    lon_max = nc.geospatial_lon_max
+    time_start = nc.time_coverage_start
+    time_end  = nc.time_coverage_end
+    date_update = nc.date_update
+
+    #get list of parameters from netCDF file
+    var_list = nc.variables.keys()
+    var_list = list(filter(lambda x: '_' not in x, var_list))
+    var_list = list(filter(lambda x: 'TIME' not in x, var_list))
+    var_list = list(filter(lambda x: 'LATITUDE' not in x, var_list))
+    var_list = list(filter(lambda x: 'LONGITUDE' not in x, var_list))
+    nc.close()
+    parameters = ' '.join(var_list)
+
+    index_info += ('COP-GLOBAL-01,' + server_location + ftp_filepath + ',' 
+                + lat_min + ',' + lat_max + ',' + lon_min + ',' + lon_max + ',' 
+                + time_start + ',' + time_end  
+                + ',University of Bergen Geophysical Institute,' 
+                + date_update + ',R,' + parameters + '\n')
+
+  index = index_header + index_info
+
+  index_filename = 'index_monthly.txt'
+  with open(index_filename,'wb') as f: f.write(index.encode())
+ 
+  logging.debug('index file:\n' + index)
+
+  return index_filename
+
+
+def create_connection(DB):
+  ''' creates connection and database if not already created '''
+  conn = sqlite3.connect(DB, isolation_level=None)
+  c = conn.cursor()
+  c.execute('''CREATE TABLE IF NOT EXISTS latest (
+              filename TEXT PRIMARY KEY,
+              hashsum TEXT NOT NULL,
+              filepath TEXT NOT NULL UNIQUE,
+              nc_date TEXT,
+              dataset TEXT,
+              uploaded INTEGER,
+              ftp_filepath TEXT,
+              dnt_file TEXT,
+              comment TEXT,
+              export_date TEXT
+              )''')
+  return c
+
+def sql_commit(nc_dict):
+  '''  creates SQL table if non exists
+  adds new netCDF files, listed in nc_dict, to new or existing SQL-table 
+  '''
+  c = create_connection(cmems_db)
+  date = datetime.datetime.now().strftime(dnt_datetime_format)
+
+  for key in nc_dict:
+    if nc_dict[key]['uploaded']: 
+      uploaded = 1
+    else: 
+      uploaded = 0
+    c.execute("SELECT * FROM latest WHERE filename=? ",[key])
+    filename_exists = c.fetchone()
+    
+    if filename_exists: # if netCDF file already in database
+      logging.info(f'Updating: {key}')
+      sql_req = "UPDATE latest SET filename=?,hashsum=?,filepath=?,nc_date=?,\
+        dataset=?,uploaded=?,ftp_filepath=?,dnt_file=?,comment=?,export_date=? \
+        WHERE filename=?"
+      sql_param = ([key,nc_dict[key]['hashsum'],nc_dict[key]['filepath'],
+        nc_dict[key]['date'],nc_dict[key]['dataset'],uploaded,None,None,None,key,date])
+    else:
+      logging.debug(f'Adding new entry {key}')
+      sql_req = "INSERT INTO latest(filename,hashsum,filepath,nc_date,\
+        dataset,uploaded,ftp_filepath,dnt_file,comment,export_date) \
+        VALUES (?,?,?,?,?,?,?,?,?,?)"
+      sql_param = ([key,nc_dict[key]['hashsum'],nc_dict[key]['filepath'],
+        nc_dict[key]['date'],nc_dict[key]['dataset'],uploaded,None,None,None,date])
+
+    c.execute(sql_req,sql_param)
+
 
 
 if __name__ == '__main__':
