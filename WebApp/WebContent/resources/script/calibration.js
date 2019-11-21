5f382109e286a434d7bc4aa6c7ea86ce107b34dd
Steve Jones <steve@squaregoldfish.co.uk>
2019-11-21 10:39:39 +0100

    (#1067) Add datasets to timeline

--- WebApp/WebContent/resources/script/calibration.js
+++ WebApp/WebContent/resources/script/calibration.js
@@ -4,26 +4,9 @@ function addDeployment() {
 }
 
 function editDeployment(item) {
-  if (null != item.item) {
-
+  // Ignore dataset clicks (they have a string id)
+  if (null != item.item && typeof(item.item) === 'number') {
     $('#deploymentForm\\:calibrationId').val(item.item);
     loadCalibration(); // PF RemoteCommand
-/*    
-    // Find the selected item
-    calibration = calibrationsJson.find(c => c.id == item.item);
-    
-    // Fill and show the deployment dialog
-    $('#deploymentForm\\:deploymentId').val(calibration.id);
-    PF('deploymentDate').setDate(calibration.start);
-    PF('target').selectValue(calibration.target);
-    
-    for (var i = 0; i < calibration.coefficients.length; i++) {
-      if (PF('coefficient-' + i)) {
-        PF('coefficient-' + i).setValue(calibration.coefficients[i]);
-      }
-    }
-    
-    PF('deploymentDialog').show();
-*/
   }
 }
