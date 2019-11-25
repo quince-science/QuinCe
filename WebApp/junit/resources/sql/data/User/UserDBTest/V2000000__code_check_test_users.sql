bdda2d6166c4a7ed0251608a34e6a26e0aa4de9b
Steve Jones <steve@squaregoldfish.co.uk>
2019-11-25 09:39:07 +0100

    (junit) Valid/invalid email/password code checks

--- WebApp/junit/resources/sql/data/User/UserDBTest/V2000000__code_check_test_users.sql
+++ WebApp/junit/resources/sql/data/User/UserDBTest/V2000000__code_check_test_users.sql
@@ -2,4 +2,17 @@
 
 -- No codes
 INSERT INTO user (email, salt, password, firstname, surname, email_code, email_code_time, password_code, password_code_time)
-  VALUES('nocodes@test.com', 'FF', 'FF', 'Test', 'Test', NULL, NULL, NULL, NULL)
+  VALUES('nocodes@test.com', 'FF', 'FF', 'Test', 'Test', NULL, NULL, NULL, NULL);
+
+-- Email code only
+INSERT INTO user (email, salt, password, firstname, surname, email_code, email_code_time, password_code, password_code_time)
+  VALUES('emailcode@test.com', 'FF', 'FF', 'Test', 'Test', 'IAMACODE', CURRENT_TIMESTAMP(), NULL, NULL);
+
+-- Password code only
+INSERT INTO user (email, salt, password, firstname, surname, email_code, email_code_time, password_code, password_code_time)
+  VALUES('passwordcode@test.com', 'FF', 'FF', 'Test', 'Test', NULL, NULL, 'IAMACODE', CURRENT_TIMESTAMP());
+
+-- Email and password codes
+INSERT INTO user (email, salt, password, firstname, surname, email_code, email_code_time, password_code, password_code_time)
+  VALUES('bothcodes@test.com', 'FF', 'FF', 'Test', 'Test', 'IAMACODE', CURRENT_TIMESTAMP(), 'IAMACODE', CURRENT_TIMESTAMP());
+
