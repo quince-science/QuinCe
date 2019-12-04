9ed355986bc4107bf6a511d2a7fb874fc2bf976c
Steve Jones <steve@squaregoldfish.co.uk>
2019-12-04 14:40:38 +0100

    (junit) Complete isNumeric tests

--- WebApp/junit/junit/uk/ac/exeter/QuinCe/utils/StringUtilsTest.java
+++ WebApp/junit/junit/uk/ac/exeter/QuinCe/utils/StringUtilsTest.java
@@ -4,53 +4,90 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import junit.uk.ac.exeter.QuinCe.TestBase.BaseTest;
 import uk.ac.exeter.QuinCe.utils.StringUtils;
 
 /**
  * Tests for the StringUtils class
+ *
  * @author Steve Jones
  *
  */
-public class StringUtilsTest {
+public class StringUtilsTest extends BaseTest {
 
   /**
-   * Test an integer
+   * Test that {@link StringUtils#isNumeric(String)} correctly identifies an
+   * integer as numeric.
    */
   @Test
-  public void testIsNumericInteger() {
+  public void isNumericIntegerTest() {
     assertTrue(StringUtils.isNumeric("7"));
   }
 
   /**
-   * Test a decimal number
+   * Test that {@link StringUtils#isNumeric(String)} correctly identifies a
+   * negative integer as numeric.
    */
   @Test
-  public void testIsNumericFloat() {
+  public void isNumericNegativeIntegerTest() {
+    assertTrue(StringUtils.isNumeric("-7"));
+  }
+
+  /**
+   * Test that {@link StringUtils#isNumeric(String)} correctly identifies a
+   * decimal number as numeric.
+   */
+  @Test
+  public void isNumericFloatTest() {
     assertTrue(StringUtils.isNumeric("67.5"));
   }
 
   /**
-   * Test a completely non-numeric string
+   * Test that {@link StringUtils#isNumeric(String)} correctly identifies a
+   * negative decimal number as numeric.
    */
   @Test
-  public void testIsNumericNonNumeric() {
+  public void isNumericNegativeFloatTest() {
+    assertTrue(StringUtils.isNumeric("-67.5"));
+  }
+
+  /**
+   * Test that {@link StringUtils#isNumeric(String)} correctly identifies a
+   * string as non-numeric.
+   */
+  @Test
+  public void isNumericNonNumericTest() {
     assertFalse(StringUtils.isNumeric("I am not a number"));
   }
 
   /**
-   * Test a completely non-numeric string
+   * Test that {@link StringUtils#isNumeric(String)} correctly identifies a
+   * {@code null} as non-numeric.
    */
   @Test
-  public void testIsNumericNull() {
+  public void isNumericNullTest() {
     assertFalse(StringUtils.isNumeric(null));
   }
 
   /**
-   * Test a completely non-numeric string
+   * Test that {@link StringUtils#isNumeric(String)} correctly identifies
+   * {@code "NaN"} as non-numeric.
    */
   @Test
-  public void testIsNumericNan() {
+  public void isNumericNanTest() {
     assertFalse(StringUtils.isNumeric("NaN"));
+  }
+
+  /**
+   * Test that {@link StringUtils#isNumeric(String)} correctly identifies empty
+   * values as non-numeric.
+   */
+  @ParameterizedTest
+  @MethodSource("createNullEmptyStrings")
+  public void isNumericEmptyTest(String empty) {
+    assertFalse(StringUtils.isNumeric(empty));
   }
 }
