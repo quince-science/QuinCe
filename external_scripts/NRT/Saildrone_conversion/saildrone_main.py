a226c134ea4de9925a4b144b4ef8fb934356b93e
Camilla <camilla.landa@uib.no>
2019-12-13 09:43:11 +0100

    (#1409) Create data folder if does not exist

--- external_scripts/NRT/Saildrone_conversion/saildrone_main.py
+++ external_scripts/NRT/Saildrone_conversion/saildrone_main.py
@@ -18,15 +18,23 @@ import shutil
 #------------------------------------------------------------------------------
 ### Handling directories
 
+# Store path to the main script directory
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+# Create a data directory if it does not already exist
+# (Commented out until API part of script works)
+#if not os.path.isdir('./data_files'):
+#	os.mkdir(os.path.join(script_dir,'data_files'))
+
 # Store path to the data directory
-data_dir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-	'data_files')
+data_dir = os.path.join(script_dir,'data_files')
 
 # Create new archive directry with current timestamp as name
 now = datetime.now()
 dt_string = now.strftime("%Y%m%dT%H%M%S")
-new_archive_path = data_dir_path + '\\' + 'archive' + '\\' + str(dt_string)
+new_archive_path = data_dir + '\\' + str(dt_string)
 os.mkdir(new_archive_path)
+
 
 #------------------------------------------------------------------------------
 ### API
@@ -38,52 +46,28 @@ os.mkdir(new_archive_path)
 #------------------------------------------------------------------------------
 ### Convert to csv format
 
-# Extract name of content in the data directory (should be "archive" and some
-# json files)
-filenames = os.listdir(data_dir_path)
+# Extract name of content in the data directory
+filenames = os.listdir(data_dir)
 
 # Run each json file through the function which converts to csv. Then move the
 # json file to the archive folder.
 for file in filenames:
 	if '.json' in file:
-		data_file_path = data_dir_path + '\\' + file
-		saildrone_module.convert_to_csv(data_file_path)
-		shutil.move(data_dir_path + '\\' + file, new_archive_path + '\\' + file)
+		json_file_path = os.path.join(data_dir, file)
+		saildrone_module.convert_to_csv(json_file_path)
+		shutil.move(json_file_path, os.path.join(new_archive_path, file))
+
 
 #------------------------------------------------------------------------------
 ### Merge biogeo and ocean csv file
 
-# This is temporary. Create a beter way to get these csv paths! (This depends
-# on how we receive the data from the API)
-ocean_csv_path = data_dir_path + '\\' + "1030_oceanographic.csv"
-biogeo_csv_path =  data_dir_path + '\\' + "1030_biogeo.csv"
+
+# Store csv file paths.
+# Change this when know how data is received from the API.
+ocean_csv_path = os.path.join(data_dir, '1030_oceanographic.csv')
+biogeo_csv_path = os.path.join(data_dir, '1030_biogeo.csv')
 
 # Call function in the saildrone module which merges the ocean and biogeo file.
 saildrone_module.merge_ocean_bio(ocean_csv_path, biogeo_csv_path)
 
-
 # Move the original ocean and biogeo csv files to the archive folder.
-
-
-#------------------------------------------------------------------------------
-### Send to QuinCe ????
-
-
-
-
-
-
-
-
-
-
-# Extract name of content in the data directory (should be "archive" and some
-# csv files)
-#filenames = os.listdir(data_dir_path)
-
-# Create the merge input list, which consist of the path to the csv files
-#merge_input = []
-#for file in filenames:
-#	if '.csv' in file:
-#		data_file_path = data_dir_path + '\\' + file
-#		merge_input.append(data_file_path)
