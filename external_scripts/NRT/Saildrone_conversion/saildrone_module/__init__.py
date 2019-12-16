98e3c3f526ce064ce469d9dabe6a8785921be7e9
Camilla <camilla.landa@uib.no>
2019-12-16 07:57:51 +0100

    (#1409) Added merge function

--- external_scripts/NRT/Saildrone_conversion/saildrone_module/__init__.py
+++ external_scripts/NRT/Saildrone_conversion/saildrone_module/__init__.py
@@ -1,4 +1,4 @@
 from .convert import convert_to_csv
-from .merge import merge_ocean_bio
+from .merge import merge_ocean_biogeo
 
-__all__=["convert_to_csv", "merge_ocean_bio"]
+__all__=["convert_to_csv", "merge_ocean_biogeo"]
--- external_scripts/NRT/Saildrone_conversion/saildrone_module/merge.py
+++ external_scripts/NRT/Saildrone_conversion/saildrone_module/merge.py
@@ -1,19 +1,52 @@
 ###############################################################################
-### CONVERT JSON TO CSV                                                     ###
+### MERGE OCEAN AND BIOGEO FILE                                                    ###
 ###############################################################################
 
 ### Description:
-# This function converts a json file from saildrone to a csv file.
+# This function merges the ocean and biogeo file. Outer full merge so that all
+# values are kept in the merged output file.
 
 #------------------------------------------------------------------------------
 import pandas as pd
 
 
-def merge_ocean_bio(ocean_path, biogeo_path):
+def add_header_suffix(df, string):
+	new_header = ['time_interval']
+	for header in list(df.columns):
+		if header != 'time_interval':
+			header_edited = header + string
+			new_header.append(header_edited)
+	df.columns = new_header
+	return df
 
-	# Read the csv files
+
+def set_datetime(df):
+	df.loc[:,'time_interval'] = pd.to_datetime(
+		df.loc[:,'time_interval'], format = '%Y-%m-%dT%H:%M:%S.%fZ')
+
+
+def merge_ocean_biogeo(ocean_path, biogeo_path):
+
 	ocean_df = pd.read_csv(ocean_path)
 	biogeo_df = pd.read_csv(biogeo_path)
 
-	print(ocean_df.head(4))
-	print(biogeo_df.head(4))
+	set_datetime(ocean_df)
+	set_datetime(biogeo_df)
+
+	ocean_df = add_header_suffix(ocean_df, '_oceanFile')
+	biogeo_df = add_header_suffix(biogeo_df, '_biogeoFile')
+
+	merged_df = biogeo_df.merge(
+		ocean_df, on='time_interval', how='outer', sort= True)
+
+	return merged_df
+
+
+
+
+
+
+
+
+
+
