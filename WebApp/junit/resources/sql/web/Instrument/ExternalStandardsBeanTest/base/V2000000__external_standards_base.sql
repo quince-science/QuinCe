2980cc446101eb0972ff1819a77911928c8a7561
Steve Jones <steve@squaregoldfish.co.uk>
2019-12-17 13:02:59 +0100

    (#1067) Stub ExternalStandardsBean test

---
+++ WebApp/junit/resources/sql/web/Instrument/ExternalStandardsBeanTest/base/V2000000__external_standards_base.sql
@@ -0,0 +1,23 @@
+-- A test instrument
+
+-- User
+INSERT INTO user (id, email, salt, password)
+  VALUES (1000000, 'test@test.com', 'FF', 'FF');
+
+-- Instrument
+INSERT INTO instrument (id, owner, name)
+  VALUES (1000000, 1000000, 'Test Instrument');
+
+-- File definition
+INSERT INTO file_definition (id, instrument_id, description, column_separator,
+  header_type, column_header_rows, column_count)
+  
+  VALUES (1000000, 1000000, 'File', ',', 0, 0, 12);
+  
+-- External standard run types
+INSERT INTO run_type (file_definition_id, run_name, category_code)
+  VALUES (1000000, 'std2', -3);
+INSERT INTO run_type (file_definition_id, run_name, category_code)
+  VALUES (1000000, 'std3', -3);
+INSERT INTO run_type (file_definition_id, run_name, category_code)
+  VALUES (1000000, 'std4', -3);
