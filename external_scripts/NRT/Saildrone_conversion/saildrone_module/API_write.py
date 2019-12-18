79ee3d5bbd8a18c9fd7bbcb50868facad217a544
Camilla <camilla.landa@uib.no>
2019-12-18 10:43:27 +0100

    (#1409) Create functions for authentication and data availability check

--- external_scripts/NRT/Saildrone_conversion/saildrone_module/API_write.py
+++ external_scripts/NRT/Saildrone_conversion/saildrone_module/API_write.py
@@ -20,6 +20,36 @@ def to_dict(request_output):
 	return dictionary
 
 
+def auth():
+	auth_url = 'https://developer-mission.saildrone.com/v1/auth'
+	our_header = {'Content-Type':'application/json', 'Accept':'application/json'}
+	our_data = json.dumps({'key':'XbXS9f7TfZepb7nD',
+		'secret':'dGL99eMuBcsJYm5guq29AtKeCGHCT2kP'}).encode()
+
+	auth_request = urllib.request.Request(
+		url=auth_url, headers=our_header,
+		data=our_data, method='POST')
+
+	auth_response_dict = saildrone_module.to_dict(auth_request)
+
+	token = auth_response_dict['token']
+	return token
+
+
+def check_available(token):
+	check_available_url = 'https://developer-mission.saildrone.com/v1/auth/'\
+	+ 'access?token=' + token
+
+	check_available_request = urllib.request.Request(
+		check_available_url, method='GET')
+
+	available_dict = saildrone_module.to_dict(check_available_request)
+
+	data = available_dict['data']
+	access_list = data['access']
+	return access_list
+
+
 def write_json(data_dir, header, drone_id, dataset, start, end, token):
 
 	get_data_url = 'https://developer-mission.saildrone.com/v1/timeseries/'\
@@ -29,7 +59,7 @@ def write_json(data_dir, header, drone_id, dataset, start, end, token):
 	data_request = urllib.request.Request(
 		get_data_url, headers=header, method='GET')
 
-	data_dict = saildrone_module.to_dict(data_request)  # Does this work?
+	data_dict = saildrone_module.to_dict(data_request)
 
 	output_file_name = str(drone_id) + '_' + dataset + '.json'
 	output_file_path = os.path.join(data_dir, output_file_name)
