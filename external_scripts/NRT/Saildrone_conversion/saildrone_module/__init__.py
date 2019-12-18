a835af4f4235b3c7c8128011496c4f8adfbc1a96
Camilla <camilla.landa@uib.no>
2019-12-18 11:12:01 +0100

    (#1409) Saildrone package cleanup

--- external_scripts/NRT/Saildrone_conversion/saildrone_module/__init__.py
+++ external_scripts/NRT/Saildrone_conversion/saildrone_module/__init__.py
@@ -1,9 +1,6 @@
 from .convert import convert_to_csv
 from .merge import merge_ocean_biogeo
-from .API_write import to_dict
-from .API_write import write_json
-from .API_write import auth
-from .API_write import check_available
+from .api import to_dict, write_json, auth, check_available
 
 __all__ = ['convert_to_csv', 'merge_ocean_biogeo', 'to_dict', 'write_json',
  'auth', 'check_available']
