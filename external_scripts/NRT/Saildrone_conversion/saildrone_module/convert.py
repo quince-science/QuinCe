0441b4577702715778e60087fd79d53c3e87e6d5
Camilla <camilla.landa@uib.no>
2019-12-18 09:49:18 +0100

    (#1409) Improve argument name and add return of paths

--- external_scripts/NRT/Saildrone_conversion/saildrone_module/convert.py
+++ external_scripts/NRT/Saildrone_conversion/saildrone_module/convert.py
@@ -9,12 +9,12 @@
 import csv
 import json
 
-def convert_to_csv(filename):
+def convert_to_csv(file_path):
 
 	# Read the json file and create the 'data_list' variable. This variable
 	# contains a list of dictionaries. Each dictionary contains several
 	# parameter-value pairs, always including one timestamp parameter-value.
-	with open(filename) as json_file:
+	with open(file_path) as json_file:
 		json_loaded = json.load(json_file)
 	data_list = json_loaded['data']
 
@@ -41,10 +41,12 @@ def convert_to_csv(filename):
 				distinct_parameters.append(parameter)
 
 	# Create output file name and write to the file
-	csv_filename = filename.replace('.json','') + ".csv"
-	with open(csv_filename, 'w', newline = '') as output_csv:
+	csv_file_path = file_path.replace('.json','') + ".csv"
+	with open(csv_file_path, 'w', newline = '') as output_csv:
 		output_writer = csv.DictWriter(
 			output_csv, fieldnames = distinct_parameters)
 		output_writer.writeheader()
 		for dictionary in data_list_edited:
 			output_writer.writerow(dictionary)
+
+	return csv_file_path
