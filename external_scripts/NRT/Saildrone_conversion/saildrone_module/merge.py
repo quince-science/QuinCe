b37804ecf4627ac2faa433521ca4f250839dc2ea
Camilla <camilla.landa@uib.no>
2019-12-19 15:39:20 +0100

    (#1409) Merge now includes atmos file. Merged outputfile gets sorted.

--- external_scripts/NRT/Saildrone_conversion/saildrone_module/merge.py
+++ external_scripts/NRT/Saildrone_conversion/saildrone_module/merge.py
@@ -24,19 +24,29 @@ def set_datetime(df):
 		df.loc[:,'time_interval'], format = '%Y-%m-%dT%H:%M:%S.%fZ')
 
 
-def merge_ocean_biogeo(ocean_path, biogeo_path):
+# This function need to change!! Will merge lines with different time together.
+def merge_ocean_biogeo(ocean_path, biogeo_path, atmos_path):
 
 	ocean_df = pd.read_csv(ocean_path)
 	biogeo_df = pd.read_csv(biogeo_path)
+	atmos_df = pd.read_csv(atmos_path)
 
 	set_datetime(ocean_df)
 	set_datetime(biogeo_df)
+	set_datetime(atmos_df)
 
 	ocean_df = add_header_suffix(ocean_df, '_oceanFile')
 	biogeo_df = add_header_suffix(biogeo_df, '_biogeoFile')
+	atmos_df = add_header_suffix(atmos_df, '_atmosFile')
 
-	merged_df = biogeo_df.merge(ocean_df,
-		left_on='time_interval_biogeoFile', right_on='time_interval_oceanFile',
+	initial_merge = biogeo_df.merge(ocean_df,
+		left_on='time_interval_biogeoFile',
+		right_on='time_interval_oceanFile',
+		how='outer', sort= True)
+
+	merged_df = initial_merge.merge(atmos_df,
+		left_on = 'time_interval_biogeoFile',
+		right_on='time_interval_atmosFile',
 		how='outer', sort= True)
 
 	return merged_df
