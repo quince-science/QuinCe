239e8740381487f162ccd0c65d12068361c28e8d
Maren <maren.kjos@gmail.com>
2019-12-19 11:22:57 +0100

    (#1571) Exceptions/error msg now sent to slack

--- external_scripts/export/QuinCetoCarbonPortal.py
+++ external_scripts/export/QuinCetoCarbonPortal.py
@@ -9,6 +9,7 @@ import toml
 import sys
 import os
 import traceback
+from slacker import Slacker
 
 from py_func.quince_comm import get_export_list, report_abandon_export, report_complete_export
 from py_func.meta_handling import process_dataset 
@@ -16,16 +17,14 @@ from py_func.carbon import get_auth_cookie, export_file_to_cp
 from py_func.copernicus import build_dataproduct, upload_to_copernicus
 
 
-config_file_quince = 'config_quince.toml'
+config_file = 'config.toml'
 config_file_copernicus = 'config_copernicus.toml'
 config_file_carbon = 'config_carbon.toml'
-config_file_meta = 'config_meta.toml'
 platform_lookup_file = 'platforms.toml'
 
-with open(config_file_quince) as f: config_quince = toml.load(f)
+with open(config_file) as f: basicConfig = toml.load(f)
 with open(config_file_copernicus) as f: config_copernicus = toml.load(f)
 with open(config_file_carbon) as f: config_carbon = toml.load(f)
-with open(config_file_meta) as f: config_meta = toml.load(f)
 with open(platform_lookup_file) as f: platform = toml.load(f)
 
 if not os.path.isdir('log'): os.mkdir('log')
@@ -33,12 +32,14 @@ logging.basicConfig(filename='log/console.log',format='%(asctime)s %(message)s',
 #logging.basicConfig(filename='log/console.log',format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 upload = True # for debugging purposes, when False no data is exported.
+slack = Slacker(config['slack']['api_token'])
 
 def main():
   logging.debug('Obtaining IDs of datasets ready for export from QuinCe')
   export_list = get_export_list(config_quince)
 
   try:
+    1/0
     if not export_list:
       logging.info('Terminating script, no datasets to be exported.')
     else: 
@@ -94,17 +95,17 @@ def main():
           report_abandon_export(config_quince,dataset['id'])
 
   except Exception as e: 
-    print(e); 
-    logging.info('Failed to run. Encountered: %s ',e);
     exc_type, exc_obj, exc_tb = sys.exc_info()
-    traceback.print_exc()
-
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    logging.debug(f'type: {exc_type}')
-    logging.debug(f'file name: {fname}')
-    logging.debug(f'line number: {exc_tb.tb_lineno}')
+    err_msg = f'Failed to run. Encountered: {e} \n type: {exc_type} \n file name: {fname} \n line number: {exc_tb.tb_lineno}'
     
-    report_abandon_export(config_quince,dataset['id'])
+    slack.chat.post_message('#errors',err_msg)
+    logging.error(err_msg)
+    try:
+      if export_list:
+        report_abandon_export(config_quince,dataset['id'])
+    except Exception as e:
+      slack.chat.post_message(config['slack']['workspace'],f'Failed to report to QuinCe {e}')
 
 
 if __name__ == '__main__':
