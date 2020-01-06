a7a2da67117d5951a4bde3dfd48ea28e1417729a
Steve Jones <steve@squaregoldfish.co.uk>
2020-01-06 15:58:28 +0100

    (#1067) Tests for invalid getAffectedDataSets calls

---
+++ WebApp/junit/junit/uk/ac/exeter/QuinCe/web/Instrument/GetAffectedDatasetsTests.java
@@ -0,0 +1,16 @@
+package junit.uk.ac.exeter.QuinCe.web.Instrument;
+
+import junit.uk.ac.exeter.QuinCe.TestBase.TestSetTest;
+import uk.ac.exeter.QuinCe.web.Instrument.CalibrationBean;
+
+/**
+ * Tests for various combinations of valid calls to
+ * {@link CalibrationBean#getAffectedDataSets(long, java.time.LocalDateTime, String)}.
+ * Invalid
+ *
+ * @author Steve Jones
+ *
+ */
+public class GetAffectedDatasetsTests extends TestSetTest {
+
+}
