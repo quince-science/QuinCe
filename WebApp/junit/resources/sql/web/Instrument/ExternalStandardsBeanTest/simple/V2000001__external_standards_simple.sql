63f53238cdc639568de5996e0f1c5bc240962a4f
Steve Jones <steve@squaregoldfish.co.uk>
2020-01-06 14:13:28 +0100

    (#1067) Revised CalibrationBean tests

--- WebApp/junit/resources/sql/web/Instrument/ExternalStandardsBeanTest/simple/V2000001__external_standards_simple.sql
+++ WebApp/junit/resources/sql/web/Instrument/ExternalStandardsBeanTest/simple/V2000001__external_standards_simple.sql
@@ -3,15 +3,15 @@
 -- 2019-06-03T00:00:00 to 2019-06-05T00:00:00
 INSERT INTO dataset (id, instrument_id, name, start, end, status, status_date)
   VALUES (1001, 1000000, 'A', 1559520000000, 1559692800000, 4, 0);
-  
+
 -- 2019-06-01T00:00:00
 INSERT INTO calibration (id, instrument_id, type, target, deployment_date, coefficients, class)
-  VALUES (1001, 1000000, 'EXTERNAL_STANDARD', 'std2', 1559347200000, '200.0;0.0', 'ExternalStandard');
-  
+  VALUES (1001, 1000000, 'CALIBRATION_TEST', 'TARGET1', 1559347200000, '200.0;0.0', 'ExternalStandard');
+
 -- 2019-06-01T00:00:00
 INSERT INTO calibration (id, instrument_id, type, target, deployment_date, coefficients, class)
-  VALUES (1002, 1000000, 'EXTERNAL_STANDARD', 'std3', 1559347200000, '400.0;0.0', 'ExternalStandard');
-  
+  VALUES (1002, 1000000, 'CALIBRATION_TEST', 'TARGET2', 1559347200000, '400.0;0.0', 'ExternalStandard');
+
 -- 2019-06-01T00:00:00
 INSERT INTO calibration (id, instrument_id, type, target, deployment_date, coefficients, class)
-  VALUES (1003, 1000000, 'EXTERNAL_STANDARD', 'std4', 1559347200000, '500.0;0.0', 'ExternalStandard');
+  VALUES (1003, 1000000, 'CALIBRATION_TEST', 'TARGET3', 1559347200000, '500.0;0.0', 'ExternalStandard');
