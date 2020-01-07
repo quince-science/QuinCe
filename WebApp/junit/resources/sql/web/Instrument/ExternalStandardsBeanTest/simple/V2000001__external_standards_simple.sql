a2d8a2e8ba4e1f85ec2c501a2fb25e2bd0cbcf09
Steve Jones <steve@squaregoldfish.co.uk>
2020-01-07 11:05:26 +0100

    (#1067) Use test stub instead of mocks
    Tests will now use the ExternalStandards as their base rather than creating a whole new setup specifically for the tests.

--- WebApp/junit/resources/sql/web/Instrument/ExternalStandardsBeanTest/simple/V2000001__external_standards_simple.sql
+++ WebApp/junit/resources/sql/web/Instrument/ExternalStandardsBeanTest/simple/V2000001__external_standards_simple.sql
@@ -6,12 +6,12 @@ INSERT INTO dataset (id, instrument_id, name, start, end, status, status_date)
 
 -- 2019-06-01T00:00:00
 INSERT INTO calibration (id, instrument_id, type, target, deployment_date, coefficients, class)
-  VALUES (1001, 1000000, 'CALIBRATION_TEST', 'TARGET1', 1559347200000, '200.0;0.0', 'ExternalStandard');
+  VALUES (1001, 1000000, 'EXTERNAL_STANDARD', 'TARGET1', 1559347200000, '200.0;0.0', 'ExternalStandard');
 
 -- 2019-06-01T00:00:00
 INSERT INTO calibration (id, instrument_id, type, target, deployment_date, coefficients, class)
-  VALUES (1002, 1000000, 'CALIBRATION_TEST', 'TARGET2', 1559347200000, '400.0;0.0', 'ExternalStandard');
+  VALUES (1002, 1000000, 'EXTERNAL_STANDARD', 'TARGET2', 1559347200000, '400.0;0.0', 'ExternalStandard');
 
 -- 2019-06-01T00:00:00
 INSERT INTO calibration (id, instrument_id, type, target, deployment_date, coefficients, class)
-  VALUES (1003, 1000000, 'CALIBRATION_TEST', 'TARGET3', 1559347200000, '500.0;0.0', 'ExternalStandard');
+  VALUES (1003, 1000000, 'EXTERNAL_STANDARD', 'TARGET3', 1559347200000, '500.0;0.0', 'ExternalStandard');
