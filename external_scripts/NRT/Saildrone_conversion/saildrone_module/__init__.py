f0d3b42ab0f019b6d831614bf02261d71f120d3c
Camilla <camilla.landa@uib.no>
2020-01-30 10:56:20 +0100

    (#1409) Merge allows changing number of datasets

--- external_scripts/NRT/Saildrone_conversion/saildrone_module/__init__.py
+++ external_scripts/NRT/Saildrone_conversion/saildrone_module/__init__.py
@@ -1,6 +1,6 @@
 from .convert import convert_to_csv
-from .merge import merge_ocean_biogeo
+from .merge import merge_datasets
 from .api import to_dict, write_json, auth, get_available, check_next_request
 
-__all__ = ['convert_to_csv', 'merge_ocean_biogeo', 'to_dict', 'write_json',
+__all__ = ['convert_to_csv', 'merge_datasets', 'to_dict', 'write_json',
  'auth', 'get_available', 'check_next_request']
