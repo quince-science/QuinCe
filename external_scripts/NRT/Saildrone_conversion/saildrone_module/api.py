f9ea1a895e527f72751d42e79a0852a7120248b8
Camilla <camilla.landa@uib.no>
2020-02-03 11:17:36 +0100

    (#1409) Add request loop in order to download full request

--- external_scripts/NRT/Saildrone_conversion/saildrone_module/api.py
+++ external_scripts/NRT/Saildrone_conversion/saildrone_module/api.py
@@ -12,6 +12,7 @@ import urllib
 import saildrone_module as saildrone
 import os
 import pandas as pd
+from datetime import datetime
 
 our_header = {'Content-Type':'application/json', 'Accept':'application/json'}
 
@@ -99,25 +100,51 @@ def check_next_request(next_request, access_list, datasets):
 
 def write_json(data_dir, drone_id, dataset, start, end, token):
 
-	# !!! Add while loop: continue download until got the whole
-	# period requested.
+	# Since we can only receive 1000 records per download request we need to
+	# keep requesting until we do not receive any data.
+	more_to_request = True
+	count = 1
+	data_list_concat = []
+	while (more_to_request is True):
 
-	get_data_url = 'https://developer-mission.saildrone.com/v1/timeseries/'\
-	+ f'{drone_id}?data_set={dataset}&interval=1&start_date={start}&end_date='\
-	+ f'{end}&order_by=asc&limit=1000&offset=0&token={token}'
+		# Define the request URL
+		get_data_url = 'https://developer-mission.saildrone.com/v1/timeseries/'\
+		+ f'{drone_id}?data_set={dataset}&interval=1&start_date={start}&end_date='\
+		+ f'{end}&order_by=asc&limit=1000&offset=0&token={token}'
 
-	data_request = urllib.request.Request(
-		get_data_url, headers=our_header, method='GET')
+		# Send request
+		data_request = urllib.request.Request(
+			get_data_url, headers=our_header, method='GET')
 
-	data_dict = saildrone.to_dict(data_request)
+		# Store output from request in dictionary
+		data_dict = saildrone.to_dict(data_request)
 
+		# If request is empty: stop sending requests. If not, add the new data
+		# to the concatenated data list. And add one second to the last record
+		# recieved (used as start date for the next request).
+		if len(data_dict['data']) == 0:
+			more_to_request = False
+		else:
+			data_list_concat = data_list_concat + data_dict['data']
+
+			last_dict = data_dict['data'][-1:]
+			last_record = datetime.strptime(last_dict[0]['time_interval'],
+			 "%Y-%m-%dT%H:%M:%S.000Z")
+			start = (last_record +
+				pd.Timedelta("1 minute")).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+
+		count += 1
+
+
+	# Replace the data section of the last json file received with the
+	# concatenated data list.
+	data_dict['data'] = data_list_concat
+
+	# Write the dictionary to a json file
 	output_file_name = str(drone_id) + '_' + dataset + '.json'
 	output_file_path = os.path.join(data_dir, output_file_name)
-
 	with open(output_file_path, 'w') as outfile:
 		json.dump(data_dict, outfile,
 			sort_keys=True, indent=4, separators=(',',': '))
 
-	# !!! Add the first time to the return. This will be used to check if need
-	# !!! to continue the request for the given drone and dataset type
 	return output_file_path
