ed9becdda1a3d7f879dd53138b91095d7eb17b0f
Steve Jones <steve@squaregoldfish.co.uk>
2020-02-17 21:14:15 -0800

    (#1067) More junit tests for affected datasets check

---
+++ WebApp/junit/resources/sql/web/Instrument/CalibrationBeanTest/priorsNotRequiredSingleCalibrationSingleDataset/V2000001__priors_not_required_single_calibration_single_dataset.sql
@@ -0,0 +1,7 @@
+-- Dataset: 2019-06-03T00:00:00 to 2019-06-05T00:00:00
+INSERT INTO dataset (id, instrument_id, name, start, end, status, status_date)
+  VALUES (1001, 1000000, 'A', 1559520000000, 1559692800000, 4, 0);
+
+-- Calibration: 2019-06-02T00:00:00
+INSERT INTO calibration (id, instrument_id, type, target, deployment_date, coefficients, class)
+  VALUES (1001, 1000000, 'SENSOR_CALIBRATION', '1001', 1559433600000, '0.0;0.0;0.0;0.0;1.0;0.0', 'PolynomialSensorCalibration');
