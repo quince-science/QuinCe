ae37718fed13a29e70b5f7385ff1f77b26d7b132
Steve Jones <steve@squaregoldfish.co.uk>
2020-04-20 11:27:14 +0200

    (#1540) Position in one column; number formatting on back end

--- WebApp/src/uk/ac/exeter/QuinCe/web/datasets/plotPage/PlotPageTableColumnSerializer.java
+++ WebApp/src/uk/ac/exeter/QuinCe/web/datasets/plotPage/PlotPageTableColumnSerializer.java
@@ -2,8 +2,6 @@ package uk.ac.exeter.QuinCe.web.datasets.plotPage;
 
 import java.lang.reflect.Type;
 
-import org.apache.commons.lang3.math.NumberUtils;
-
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
@@ -18,12 +16,7 @@ public class PlotPageTableColumnSerializer
 
     JsonObject json = new JsonObject();
 
-    if (NumberUtils.isCreatable(src.getValue())) {
-      json.addProperty("value", Double.parseDouble(src.getValue()));
-    } else {
-      json.addProperty("value", src.getValue());
-    }
-
+    json.addProperty("value", src.getValue());
     json.addProperty("used", src.getUsed());
     json.addProperty("qcFlag", src.getQcFlag().getFlagValue());
     json.addProperty("qcMessage", src.getQcMessage());
