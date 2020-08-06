3c6bdcee094eaca1383895f4ca18cf26f2733207
Steve Jones <steve@squaregoldfish.co.uk>
2020-08-06 16:51:18 +0200

    (junit) sortByLength tests

--- WebApp/src/uk/ac/exeter/QuinCe/utils/DescendingLengthComparator.java
+++ WebApp/src/uk/ac/exeter/QuinCe/utils/DescendingLengthComparator.java
@@ -2,11 +2,33 @@ package uk.ac.exeter.QuinCe.utils;
 
 import java.util.Comparator;
 
+/**
+ * Compares {@link String}s by descending order of length.
+ *
+ * <p>
+ * {@code null} strings are considered to be shorter than zero-length strings.
+ * Strings of equal length are not sorted further.
+ * </p>
+ *
+ * @author Steve Jones
+ *
+ */
 public class DescendingLengthComparator implements Comparator<String> {
 
   @Override
   public int compare(String o1, String o2) {
-    return o2.length() - o1.length();
-  }
+    int result;
 
+    if (null == o1 && null == o2) {
+      result = 0;
+    } else if (null == o1) {
+      result = 1;
+    } else if (null == o2) {
+      result = -1;
+    } else {
+      result = o2.length() - o1.length();
+    }
+
+    return result;
+  }
 }
