6887761c98d8d2e95cb603b389a3166e7155ec1c
Steve Jones <steve@squaregoldfish.co.uk>
2020-09-15 11:08:39 +0200

    (#1455) File columns have sample values

--- WebApp/src/uk/ac/exeter/QuinCe/web/Instrument/newInstrument/FileColumn.java
+++ WebApp/src/uk/ac/exeter/QuinCe/web/Instrument/newInstrument/FileColumn.java
@@ -1,5 +1,8 @@
 package uk.ac.exeter.QuinCe.web.Instrument.newInstrument;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Simple class to hold a column's name and an example value
  *
@@ -11,8 +14,7 @@ public class FileColumn {
   /**
    * Common missing values to ignore when looking for example values
    */
-  protected static final String[] MISSING_VALUES = { "", "NaN", "-999",
-    "-9999" };
+  private static ArrayList<String> MISSING_VALUES;
 
   /**
    * The column index
@@ -28,6 +30,15 @@ public class FileColumn {
    * An example value extracted from the file
    */
   private final String exampleValue;
+
+  // Set up the list of missing values
+  static {
+    MISSING_VALUES = new ArrayList<String>();
+    MISSING_VALUES.add("");
+    MISSING_VALUES.add("nan");
+    MISSING_VALUES.add("-999");
+    MISSING_VALUES.add("-9999");
+  }
 
   /**
    * Simple constructor to set all fields.
@@ -70,5 +81,25 @@ public class FileColumn {
    */
   public String getExampleValue() {
     return exampleValue;
+  }
+
+  protected static List<FileColumn> makeFileColumns(List<String> names,
+    List<String> exampleValues) {
+
+    if (names.size() != exampleValues.size()) {
+      throw new IndexOutOfBoundsException("Lists are not the same size");
+    }
+
+    List<FileColumn> result = new ArrayList<FileColumn>(names.size());
+
+    for (int i = 0; i < names.size(); i++) {
+      result.add(new FileColumn(i, names.get(i), exampleValues.get(i)));
+    }
+
+    return result;
+  }
+
+  protected static boolean isMissingValue(String value) {
+    return MISSING_VALUES.contains(value.toLowerCase());
   }
 }
