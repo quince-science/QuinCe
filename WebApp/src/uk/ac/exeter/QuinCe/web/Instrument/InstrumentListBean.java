5b6a907f039bfa993873c273646c3087a0bb7535
Steve Jones <steve@squaregoldfish.co.uk>
2020-09-21 17:56:21 +0200

    (#1867) Instrument list loaded on demand

--- WebApp/src/uk/ac/exeter/QuinCe/web/Instrument/InstrumentListBean.java
+++ WebApp/src/uk/ac/exeter/QuinCe/web/Instrument/InstrumentListBean.java
@@ -2,7 +2,6 @@ package uk.ac.exeter.QuinCe.web.Instrument;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -56,26 +55,20 @@ public class InstrumentListBean extends BaseManagedBean {
   private InstrumentStub chosenInstrument = null;
 
   /**
-   * Initialises the bean by pre-loading the list of instruments
-   */
-  @PostConstruct
-  public void init() {
-    // Load the instrument list
-    try {
-      instrumentList = InstrumentDB
-        .getInstrumentList(ServletUtils.getDBDataSource(), getUser());
-    } catch (MissingParamException | DatabaseException | ResourceException e) {
-      e.printStackTrace();
-      throw new RuntimeException(e.getMessage(), e);
-    }
-  }
-
-  /**
    * Returns a list of the instruments owned by the current user
    *
    * @return The instruments owned by the current user
+   * @throws ResourceException
+   * @throws DatabaseException
+   * @throws MissingParamException
    */
-  public List<InstrumentStub> getInstrumentList() {
+  public List<InstrumentStub> getInstrumentList()
+    throws MissingParamException, DatabaseException, ResourceException {
+    if (null == instrumentList) {
+      instrumentList = InstrumentDB
+        .getInstrumentList(ServletUtils.getDBDataSource(), getUser());
+    }
+
     return instrumentList;
   }
 
