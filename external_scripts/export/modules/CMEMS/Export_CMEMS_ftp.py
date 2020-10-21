56cc462c45fc01b579856c38de246d1004298adc
Maren <maren.kjos@gmail.com>
2020-10-21 17:25:21 +0200

    (#1903) Restructured CMEMS export

--- external_scripts/export/modules/CMEMS/Export_CMEMS_ftp.py
+++ external_scripts/export/modules/CMEMS/Export_CMEMS_ftp.py
@@ -1,53 +1,5 @@
 
-'''
-Functions specific to communication with Copernicus.
 
-Files to Copernicus must be on netcdf format.
-
-Files are sent to Copernicus by FTP. 
-
-The Copernicus FTP requires an Index file and a DNT file describing all 
-files uploaded to the server to complete the ingestion. 
-The index file reflects all the files in the FTP folder.
-The DNT file triggers the ingestion. 
-
-Example of DNT file format provided by mail from Antoine.Queric@ifremer.fr 2019-03-07
-<?xml version="1.0" ?>
-<delivery PushingEntity="CopernicusMarine-InSitu-Global" date="20190306T070107Z" product="INSITU_GLO_CARBON_NRT_OBSERVATIONS_013_049">
-  <dataset DatasetName="NRT">
-    <file Checksum="936999b6a47731e8aa763ec39b3af641" FileName="latest/20190306/A.nc" FinalStatus="Delivered" StartUploadTime="20190306T070107Z" StopUploadTime="20190306T070107Z"/>
-    <file Checksum="d763859d86284add3395067fe9f8e3a0" FileName="latest/20190306/B.nc" FinalStatus="Delivered" StartUploadTime="20190306T070108Z" StopUploadTime="20190306T070108Z"/>
-
-    <file FileName="latest/20190306/C.nc">
-      <KeyWord>Delete</KeyWord>
-    </file>
-
-  </dataset>
-</delivery> 
-
-Example of index file format provided by Corentin.Guyot@ifremer.fr 2019-03-06
-# Title : Carbon in-situ observations catalog 
-# Description : catalog of available in-situ observations per platform. 
-# Project : Copernicus 
-# Format version : 1.0 
-# Date of update : 20190305080103 
-# catalog_id,file_name,geospatial_lat_min,geospatial_lat_max,geospatial_lon_min,geospatial_lon_max,time_coverage_start,time_coverage_end,provider,date_update,data_mode,parameters 
-COP-GLOBAL-01,ftp://nrt.cmems-du.eu/Core/INSITU_GLO_CARBON_NRT_OBSERVATIONS_013_049/nrt/latest/20190221/GL_LATEST_PR_BA_7JXZ_20190221.nc,19.486,19.486,-176.568,-176.568,2019-02-21T17:50:00Z,2019-02-21T17:50:00Z,Unknown institution,2019-02-24T04:10:11Z,R,DEPH TEMP
-
-To delete en empty directory, you can use the following syntax inside your DNT file :
-
-        <directory DestinationFolderName="" SourceFolderName="directoryName">
-          <KeyWord>Delete</KeyWord>
-        </directory>
-
-
-To move an existing file, you can use following syntax in your DNT file :
-
-        <file Checksum="fileChecksum" FileName="path/to/existing/file.nc" NewFileName="path/to/new_folder/file.nc">
-            <KeyWord>Move</KeyWord>
-        </file>
-
-'''
 import logging 
 import ftputil 
 import os
@@ -65,15 +17,18 @@ import xml.etree.ElementTree as ET
 import sqlite3
 import json
 import time
-import toml
 
-with open('config_copernicus.toml') as f: config_copernicus = toml.load(f)
+# Upload result codes
+UPLOAD_OK = 0
+FILE_EXISTS = 2
 
 # Response codes
 UPLOADED = 1
 NOT_UPLOADED = 0
 FAILED_INGESTION = -1
 
+dnt_datetime_format = '%Y-%m-%dT%H:%M:%SZ'
+server_location = 'ftp://nrt.cmems-du.eu/Core'
 
 log_file = 'log/cmems_log.txt'
 not_ingested = 'log/log_uningested_files.csv'
@@ -81,91 +36,14 @@ cmems_db = 'files_cmems.db'
 
 product_id = 'INSITU_GLO_CARBON_NRT_OBSERVATIONS_013_049'
 dataset_id = 'NRT_202003'
+institution = 'University of Bergen Geophysical Institute'
+institution_edmo = '4595'
 
 nrt_dir = '/' + product_id + '/' + dataset_id + '/latest'
 dnt_dir = '/' + product_id + '/DNT'
 index_dir = '/' + product_id + '/' + dataset_id
 
 local_folder = 'latest'
-
-def build_dataproduct(dataset_zip,dataset,key,destination_filename,platform):
-  '''
-  transforms csv-file to daily netCDF-files.
-  Creates dictionary containing info on each netCDF file extracted
-  requires: zip-folder, dataset-name and specific filename of csv-file.
-  returns: dictionary
-  '''
-  # BUILD netCDF FILES
-
-  data_filename = (dataset['name'] + '/dataset/' + "Copernicus" + key 
-  + dataset['name'] + '.csv')
-
-  # Load field config
-  fieldconfig = pd.read_csv('fields.csv', delimiter=',', quotechar='\'')
-
-  csv_file = get_file_from_zip(dataset_zip, destination_filename)  
-  
-  curr_date = datetime.datetime.now().strftime("%Y%m%d")
-  
-  if not os.path.exists(local_folder): os.mkdir(local_folder)
-
-  logging.info(f'Creating netcdf-files based on {csv_file} to send to CMEMS')
-
-  filedata = pd.read_csv(csv_file, delimiter=',')
-  nc_files = buildnetcdfs(dataset_name, fieldconfig, filedata,platform)
-   
-  nc_dict = {}
-  for nc_file in nc_files:
-    (nc_filename, nc_content) = nc_file
-    hashsum = hashlib.md5(nc_content).hexdigest()
-    logging.debug(f'Processing netCDF file {nc_filename}')
-
-    # ASSIGN DATE-VARIABLES TO netCDF FILE
-    nc_filepath = local_folder + '/' + nc_filename + '.nc'   
-
-    with open(nc_filepath,'wb') as f: f.write(nc_content)
-
-    # reading netCDF file to memory
-    nc = netCDF4.Dataset(nc_filepath,mode = 'r+')
-    datasetdate = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-
-    nc.date_update = datasetdate
-    nc.history = datasetdate + " : Creation"
-
-    platform_code = nc.platform_code
-    last_lat = nc.last_latitude_observation
-    last_lon = nc.last_longitude_observation
-    last_dt = nc.last_date_observation 
-
-    #get list of parameters from netCDF file
-    var_list = nc.variables.keys()
-    var_list = list(filter(lambda x: '_' not in x, var_list))
-    var_list = list(filter(lambda x: 'TIME' not in x, var_list))
-    var_list = list(filter(lambda x: 'LATITUDE' not in x, var_list))
-    var_list = list(filter(lambda x: 'LONGITUDE' not in x, var_list))
-    parameters = ' '.join(var_list)
-    nc.close()
-
-    # create dictionary object
-    date = nc_filename.split('_')[-1]
-    date = datetime.datetime.strptime(date,'%Y%m%d')
-    hashsum = hashlib.md5(nc_content).hexdigest()
-    nc_dict[nc_filename] = ({
-      'filepath':nc_filepath, 
-      'hashsum': hashsum, 
-      'date': date, 
-      'dataset':dataset_name,
-      'uploaded':False,
-      'platform': platform_code,
-      'parameters':parameters,
-      'last_lat':last_lat,
-      'last_lon':last_lon,
-      'last_dt':last_dt})
-
-  logging.debug(f'Commiting metadata to local SQL database {cmems_db}')
-  sql_commit(nc_dict)
-  return str(curr_date)
-
 
 def upload_to_copernicus(ftp_config,server,dataset,curr_date,platform):
   '''
@@ -363,3 +241,162 @@ def upload_to_copernicus(ftp_config,server,dataset,curr_date,platform):
         error_msg = abort_upload(error, ftp, nrt_dir, c, curr_date)
         
     return status, error_msg
+
+def upload_to_ftp(ftp, ftp_config, filepath):
+  ''' Uploads file with location 'filepath' to an ftp-server, 
+  server-location set by 'directory' parameter and config-file, 
+  ftp is the ftp-connection
+
+  returns 
+  upload_result: upload_ok or file_exists
+  dest_filepath: target filepath on ftp-server
+  start_upload_time and stop_upload_time: timestamps of upload process
+  '''
+ 
+  upload_result = UPLOAD_OK
+  if filepath.endswith('.nc'):
+    filename = filepath.rsplit('/',1)[-1]
+    date = filename.split('_')[-1].split('.')[0]
+    ftp_folder = nrt_dir + '/' + date     
+    ftp_filepath = ftp_folder + '/' +  filename
+
+  elif filepath.endswith('.xml'):
+    ftp_folder = dnt_dir
+    ftp_filepath = ftp_folder + '/' + filepath.rsplit('/',1)[-1]
+
+  elif filepath.endswith('.txt'):
+    with open(filepath,'rb') as f: 
+      file_bytes = f.read() 
+    ftp_folder = index_dir
+    ftp_filepath = ftp_folder + '/' + filepath.rsplit('/',1)[-1]
+
+
+  start_upload_time = datetime.datetime.now().strftime(dnt_datetime_format)
+  if not ftp.path.isdir(ftp_folder):
+    ftp.mkdir(ftp_folder)
+    ftp.upload(filepath, ftp_filepath)
+  elif ftp.path.isfile(ftp_filepath):
+    upload_result = FILE_EXISTS
+  else:
+    ftp.upload(filepath, ftp_filepath)
+  stop_upload_time = datetime.datetime.now().strftime(dnt_datetime_format)
+
+  return upload_result, ftp_filepath, start_upload_time, stop_upload_time
+
+def get_response(ftp,dnt_filepath,folder_local):
+  '''  Retrieves the status of any file uploaded to CMEMS server
+  returns the string of the xml responsefile generated by the CMEMS server. 
+  '''
+  source = (dnt_filepath.split('.')[0]
+    .replace('DNT','DNT_response') + '_response.xml')
+  target = folder_local + '/' +  source.split('/')[-1]
+
+  ftp.download(source,target)
+  with open(target,'r') as response_file:
+    response = response_file.read()
+  return responsedef abort_upload(error,ftp,nrt_dir,c,curr_date):
+  # Remove currently updated files on ftp-server
+  uningested_files = clean_directory(ftp, nrt_dir)
+
+  c.execute("SELECT * FROM latest WHERE (dnt_file = ?)",[curr_date])
+  failed_ingestion = c.fetchall()
+  
+  logging.debug(f'failed ingestion: \n{failed_ingestion}')
+  logging.debug(f'uningested files: \n {uningested_files}')
+
+  # Update database : set uploaded to 0 where index-file is current date
+  error_msg = "failed ingestion: " + error 
+  sql_req = ("UPDATE latest \
+    SET uploaded = ?, ftp_filepath = ?, dnt_file = ?, comment = ? \
+    WHERE dnt_file = ?")
+  sql_var = [0,None,None,error_msg, curr_date]
+
+  c.execute(sql_req,sql_var)
+
+  return error_msg
+
+def evaluate_response_file(ftp,dnt_filepath,folder_local,cmems_db):
+  '''  Retrieves response from cmems-ftp server.
+  '''
+  response_received = False
+  loop_iter = 0
+  upload_response_log = ''
+  rejected_list = []
+
+  logging.debug('waiting for dnt-response')
+  while response_received == False and loop_iter < 50 :
+    time.sleep(10)
+    logging.debug('checking for dnt-response ' + str(loop_iter*10))
+    try:
+      cmems_response = get_response(ftp,dnt_filepath,folder_local)
+      response_received = True
+      logging.debug('cmems response: ' + cmems_response)
+    except:
+      response_received = False
+      logging.debug('no response found')
+    loop_iter += 1
+
+  if response_received == False: 
+    return 'No response received'
+  else:
+    if not 'Ingested="True"' in cmems_response: #ingestion failed or partial
+      rejected = re.search(
+        'FileName=(.+?)RejectionReason=(.+?)Status',cmems_response)
+      if rejected:
+        rejected_file, rejected_reason = [rejected.group(1), rejected.group(2)]
+        logging.info('Rejected: {}, {}'.format(rejected_file, rejected_reason))
+
+        rejected_list += [[rejected_file,rejected_reason]] 
+        rejected_filename = rejected_file.split('/')[-1].split('.')[0]
+
+        c = create_connection(cmems_db)
+        sql_req = "UPDATE latest SET uploaded=?,comment=? WHERE filename=?"
+        sql_var = ([-1,rejected_reason, rejected_filename])
+        c.execute(sql_req,sql_var)
+
+    else:
+      logging.info('All files ingested')
+
+    date = datetime.datetime.now().strftime('%Y-%m-%d')
+    upload_response_log += (date + ',' + folder_local + ',' + 
+      dnt_filepath + ',' + cmems_response + ',\n')
+  
+  if os.path.isfile(log_file):
+    with open(log_file,'a+') as log: 
+      log.write(upload_response_log)
+  else: 
+    with open(log_file,'w') as log: 
+      log.write('date, local filepath, cmems filepath, cmems response\n')
+      log.write(upload_response_log)
+      logging.debug(f'log-message: {upload_response_log}')
+
+  return rejected_list
+  
+def check_directory(ftp, nrt_dir):
+  '''   Cleans out empty folders, checks if main directory is empty. 
+  returns True when empty 
+  '''
+  uningested_files = clean_directory(ftp, nrt_dir)
+  with open (not_ingested,'a+') as f:
+    for item in uningested_files:
+      f.write(str(datetime.datetime.now()) + ': ' + str(item) + '\n')
+  if ftp.listdir(nrt_dir):
+    logging.warning('ftp-folder is not empty')
+    return True 
+  else:
+    return False
+
+def clean_directory(ftp,nrt_dir):
+  ''' removes empty directories from ftp server '''
+  uningested_files = []
+  for dirpath, dirnames, files in ftp.walk(nrt_dir+'/'):
+    if not dirnames and not files and not dirpath.endswith('/latest/'):
+      logging.debug(f'removing EMPTY DIRECTORY: {str(dirpath)}') 
+      ftp.rmdir(dirpath)
+    elif files:
+      uningested_files += (
+        [[('dirpath',dirpath),('dirnames',dirnames),('files',files)]])
+      logging.debug(f'UNINGESTED: \
+        dirpath: {dirpath}, \ndirnames: {dirnames}, \nfiles: {files}')
+  return uningested_files
+
