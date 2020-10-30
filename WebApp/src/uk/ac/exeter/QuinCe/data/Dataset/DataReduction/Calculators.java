22cc9b4bf56ce64ed4cd7b956dcc79560e06e9b4
Steve Jones <steve@squaregoldfish.co.uk>
2020-10-30 16:06:17 +0100

    (junit) Tests for Calculators class

--- WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/Calculators.java
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/Calculators.java
@@ -13,10 +13,24 @@ public class Calculators {
    */
   private static final double PASCALS_TO_ATMOSPHERES = 0.00000986923266716013;
 
+  /**
+   * Convert a temperature in °C to °K
+   *
+   * @param celsius
+   *          Celsius temperature
+   * @return Kelvin temperature
+   */
   public static Double kelvin(Double celsius) {
     return celsius + 273.15;
   }
 
+  /**
+   * Convert a pressure in hPa it atmospheres
+   *
+   * @param hPa
+   *          Pressure in hPa
+   * @return Pressure in atmospheres
+   */
   public static Double hPaToAtmospheres(Double hPa) {
     return hPa * 100 * PASCALS_TO_ATMOSPHERES;
   }
@@ -41,11 +55,9 @@ public class Calculators {
     Double B = -1636.75 + 12.0408 * kelvin - 0.0327957 * Math.pow(kelvin, 2)
       + (3.16528 * 1e-5) * Math.pow(kelvin, 3);
     Double delta = 57.7 - 0.118 * kelvin;
-    Double eqpAtmospheres = (pressure * 100) * PASCALS_TO_ATMOSPHERES;
 
-    return pco2 * Math.exp(
-      ((B + 2 * Math.pow(1 - xCO2InGas * 1e-6, 2) * delta) * eqpAtmospheres)
-        / (82.0575 * kelvin));
+    return pco2 * Math.exp(((B + 2 * Math.pow(1 - xCO2InGas * 1e-6, 2) * delta)
+      * hPaToAtmospheres(pressure)) / (82.0575 * kelvin));
   }
 
   /**
@@ -53,15 +65,15 @@ public class Calculators {
    *
    * @param xCO2
    *          The dry, calibrated xCO<sub>2</sub> value
-   * @param eqp
+   * @param pressure
    *          The pressure
    * @param pH2O
    *          The water vapour pressure
    * @return pCO<sub>2</sub> in water
    */
-  public static Double calcpCO2TEWet(Double xCO2, Double eqp, Double pH2O) {
-    Double eqp_atm = eqp * PASCALS_TO_ATMOSPHERES * 100;
-    return xCO2 * (eqp_atm - pH2O);
+  public static Double calcpCO2TEWet(Double xCO2, Double pressure,
+    Double pH2O) {
+    return xCO2 * (hPaToAtmospheres(pressure) - pH2O);
   }
 
   /**
@@ -70,12 +82,12 @@ public class Calculators {
    *
    * @param salinity
    *          Salinity
-   * @param eqt
-   *          Equilibrator temperature (in celsius)
+   * @param temperature
+   *          Temperature (in celsius)
    * @return The calculated pH2O value
    */
-  public static Double calcPH2O(Double salinity, Double eqt) {
-    double kelvin = Calculators.kelvin(eqt);
+  public static Double calcPH2O(Double salinity, Double temperature) {
+    double kelvin = Calculators.kelvin(temperature);
     return Math.exp(24.4543 - 67.4509 * (100 / kelvin)
       - 4.8489 * Math.log(kelvin / 100) - 0.000544 * salinity);
   }
