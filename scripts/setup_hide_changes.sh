5dfbfb3a8058c48d40bbad371881a4b21c88419e
Steve Jones <steve@squaregoldfish.co.uk>
2020-11-25 10:00:02 +0100

    (#1903) replace_strings et al no longer mess with export configs

--- scripts/setup_hide_changes.sh
+++ scripts/setup_hide_changes.sh
@@ -14,7 +14,4 @@ git update-index --assume-unchanged      \
   WebApp/WebContent/META-INF/context.xml \
   WebApp/WebContent/WEB-INF/web.xml      \
   configuration/quince.properties        \
-  external_scripts/NRT/config.toml \
-  external_scripts/export/config_carbon.toml \
-  external_scripts/export/config.toml \
-  external_scripts/export/config_copernicus.toml
+  external_scripts/NRT/config.toml
