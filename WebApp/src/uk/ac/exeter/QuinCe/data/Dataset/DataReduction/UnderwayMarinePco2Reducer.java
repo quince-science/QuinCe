2fd811252f8e2e9d499c7ba5ac684f6cfc33cb51
Steve Jones <steve@squaregoldfish.co.uk>
2021-02-06 10:16:03 +0100

    (#1455) Fix calculation parameter IDs
    Underway Marine pCO2 Reducer

--- WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/UnderwayMarinePco2Reducer.java
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/UnderwayMarinePco2Reducer.java
@@ -91,32 +91,32 @@ public class UnderwayMarinePco2Reducer extends DataReducer {
   @Override
   public List<CalculationParameter> getCalculationParameters() {
     if (null == calculationParameters) {
-      calculationParameters = new ArrayList<CalculationParameter>(11);
+      calculationParameters = new ArrayList<CalculationParameter>(7);
 
-      calculationParameters.add(new CalculationParameter(makeParameterId(4),
+      calculationParameters.add(new CalculationParameter(makeParameterId(1),
         "ΔT", "Water-Equilibrator Temperature Difference", "DELTAT", "°C",
         false, false));
 
       calculationParameters
-        .add(new CalculationParameter(makeParameterId(5), "pH₂O",
+        .add(new CalculationParameter(makeParameterId(2), "pH₂O",
           "Marine Water Vapour Pressure", "RH2OX0EQ", "hPa", false, false));
 
-      calculationParameters.add(new CalculationParameter(makeParameterId(6),
+      calculationParameters.add(new CalculationParameter(makeParameterId(3),
         "Calibrated CO₂", "xCO₂ In Water - Calibrated In Dry Air", "XCO2DECQ",
         "μmol mol-1", false, false));
 
-      calculationParameters.add(new CalculationParameter(makeParameterId(7),
+      calculationParameters.add(new CalculationParameter(makeParameterId(4),
         "pCO₂ TE Wet", "pCO₂ In Water - Equilibrator Temperature", "PCO2IG02",
         "μatm", false, false));
 
-      calculationParameters.add(new CalculationParameter(makeParameterId(8),
+      calculationParameters.add(new CalculationParameter(makeParameterId(5),
         "fCO₂ TE Wet", "fCO₂ In Water - Equilibrator Temperature", "FCO2IG02",
         "μatm", false, false));
 
-      calculationParameters.add(new CalculationParameter(makeParameterId(9),
+      calculationParameters.add(new CalculationParameter(makeParameterId(5),
         "pCO₂ SST", "pCO₂ In Water", "PCO2TK02", "μatm", true, false));
 
-      calculationParameters.add(new CalculationParameter(makeParameterId(10),
+      calculationParameters.add(new CalculationParameter(makeParameterId(7),
         "fCO₂", "fCO₂ In Water", "FCO2XXXX", "μatm", true, false));
     }
 
