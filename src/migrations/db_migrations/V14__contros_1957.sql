2c47f1887352a9cf92b0c9be25a11455cce62cb1
Steve Jones <steve@squaregoldfish.co.uk>
2021-03-24 19:43:03 +0100

    (#1957) Fix QC flag cascades

--- src/migrations/db_migrations/V14__contros_1957.sql
+++ src/migrations/db_migrations/V14__contros_1957.sql
@@ -70,21 +70,21 @@ INSERT INTO variable_sensors (variable_id, sensor_type, core, questionable_casca
   VALUES (
     (SELECT id FROM variables WHERE name = 'CONTROS pCO₂'),
     (SELECT id FROM sensor_types WHERE column_code = 'CONTROSZERO'),
-    0, 3, 4
+    0, 2, 2
   );
 
 INSERT INTO variable_sensors (variable_id, sensor_type, core, questionable_cascade, bad_cascade)
   VALUES (
     (SELECT id FROM variables WHERE name = 'CONTROS pCO₂'),
     (SELECT id FROM sensor_types WHERE column_code = 'CONTROSFLUSH'),
-    0, 3, 4
+    0, 2, 2
   );
 
 INSERT INTO variable_sensors (variable_id, sensor_type, core, questionable_cascade, bad_cascade)
   VALUES (
     (SELECT id FROM variables WHERE name = 'CONTROS pCO₂'),
     (SELECT id FROM sensor_types WHERE column_code = 'CONTROSRUNTIME'),
-    0, 3, 4
+    0, 2, 2
   );
 
 INSERT INTO variable_sensors (variable_id, sensor_type, core, questionable_cascade, bad_cascade)
