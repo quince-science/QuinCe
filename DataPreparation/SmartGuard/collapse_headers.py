843b1a2586d055bef430e6cc16a23748928cae13
Steve Jones <steve@squaregoldfish.co.uk>
2021-04-29 16:56:32 +0200

    (#2092) addcolumn option

--- DataPreparation/SmartGuard/collapse_headers.py
+++ DataPreparation/SmartGuard/collapse_headers.py
@@ -9,7 +9,7 @@ HEADER_LENGTH = 7
 SEPARATOR = ";"
 COLUMN_ROW_COUNT = 3
 
-def main(in_file_name, out_file_name):
+def main(in_file_name, out_file_name, new_columns):
 
   in_file = open(in_file_name, 'r')
   out_file = open(out_file_name, 'w')
@@ -39,7 +39,10 @@ def main(in_file_name, out_file_name):
       headers.append(header)
       last_header = header
 
-  out_file.write(';'.join(headers))
+  for col in new_columns:
+    headers.append(col[0])
+
+  out_file.write(SEPARATOR.join(headers))
   out_file.write('\n')
 
   # Now copy the data lines
@@ -49,18 +52,47 @@ def main(in_file_name, out_file_name):
     if line.strip() == '':
       eof = True
     else:
-      out_file.write(line)
+      # The last column is empty because of trailing separators. Remove it.
+      fields = line.split(SEPARATOR)[:-1]
+      for col in new_columns:
+        fields.append(col[1])
+
+      out_file.write(f'{SEPARATOR.join(fields)}\n')
 
   # Close files
   out_file.close()
   in_file.close()
 
-if __name__ == '__main__':
-  if len(sys.argv) != 2:
-    print("Usage: python collapse_headers.py <in_file>")
-    exit()
+def usage():
+  print('Usage: python collapse_headers [-addcolumn <name> <value>] <infile>')
+  exit()
 
-  in_file = sys.argv[1]
+
+if __name__ == '__main__':
+
+  in_file = None
+  out_file = None
+  new_columns = []
+
+  current_arg = 1
+
+  while current_arg < len(sys.argv):
+    if sys.argv[current_arg] == '-addcolumn':
+      if len(sys.argv) < current_arg + 3:
+        usage()
+      else:
+        colname = sys.argv[current_arg + 1]
+        colvalue = sys.argv[current_arg + 2]
+        new_columns.append((colname, colvalue))
+        current_arg += 2
+    elif sys.argv[current_arg].startswith('-'):
+      usage()
+    else:
+      in_file = sys.argv[current_arg]
+      current_arg += 1
+
+
   root, ext = os.path.splitext(in_file)
   out_file = f'{root}.singleheader{ext}'
-  main(in_file, out_file)
+
+  main(in_file, out_file, new_columns)
