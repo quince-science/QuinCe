b157a1bf1ec1de2bf02bbe6e5ad4537c97e7ed33
Steve Jones <steve@squaregoldfish.co.uk>
2021-09-16 18:07:30 +0200

    (#1978) PyCharm reformatting and other PEP fixes

--- external_scripts/NRT/PreprocessorFactory.py
+++ external_scripts/NRT/PreprocessorFactory.py
@@ -1,49 +1,51 @@
 from Preprocessor import Preprocessor
-import NonePreprocessor, AddSalinityPreprocessor
+
 
 # Factory for Preprocessor instances
 
 # Get the list of preprocessor types
 def get_preprocessor_names():
-  result = []
+    result = []
 
-  for clazz in Preprocessor.__subclasses__():
-    result.append(clazz.get_name())
+    for clazz in Preprocessor.__subclasses__():
+        result.append(clazz.get_name())
 
-  return result
+    return result
+
 
 # Ask the user to select a preprocessor
 def ask_preprocessor():
-  entries = get_preprocessor_names()
+    entries = get_preprocessor_names()
 
-  selected = -1
-  while selected == -1:
-    print("Select preprocessor:")
+    selected = -1
+    while selected == -1:
+        print("Select preprocessor:")
 
-    for i in range(0, len(entries)):
-      print("  %d. %s" % (i, entries[i]))
+        for i in range(0, len(entries)):
+            print("  %d. %s" % (i, entries[i]))
 
-    try:
-      selection = input("Selection: ")
-      selection = int(selection)
-      if selection > -1 and selection <= len(entries):
-        selected = selection
-    except ValueError:
-      pass
+        try:
+            selection = input("Selection: ")
+            selection = int(selection)
+            if -1 < selection <= len(entries):
+                selected = selection
+        except ValueError:
+            pass
 
-  result = entries[selected]
+    result = entries[selected]
 
-  return result
+    return result
+
 
 def get_new_instance(preprocessor_type):
-  result = None
+    result = None
 
-  for clazz in Preprocessor.__subclasses__():
-    if clazz.get_name() == preprocessor_type:
-      result = clazz()
-      break
+    for clazz in Preprocessor.__subclasses__():
+        if clazz.get_name() == preprocessor_type:
+            result = clazz()
+            break
 
-  if result is None:
-    raise ValueError("Cannot find retriever of type %s" % (preprocessor_type, ))
+    if result is None:
+        raise ValueError("Cannot find retriever of type %s" % (preprocessor_type,))
 
-  return result
+    return result
--- external_scripts/NRT/quince.py
+++ external_scripts/NRT/quince.py
@@ -1,58 +1,78 @@
-import urllib.request, urllib.error
+import urllib.request
+import urllib.error
 import requests
 import base64
 import json
 
-# Retrieve the NRT instruments registered in QuinCe
+'''
+Retrieve the NRT instruments registered in QuinCe
+'''
+
+
 def get_instruments(config):
-  quince_url = config["QuinCe"]["url"]
-  user = config["QuinCe"]["user"]
-  password = config["QuinCe"]["password"]
+    quince_url = config["QuinCe"]["url"]
+    user = config["QuinCe"]["user"]
+    password = config["QuinCe"]["password"]
 
-  request = urllib.request.Request(quince_url + "/api/nrt/GetInstruments")
+    request = urllib.request.Request(quince_url + "/api/nrt/GetInstruments")
 
-  auth_string = "%s:%s" % (user, password)
-  base64_auth_string = base64.standard_b64encode(auth_string.encode("utf-8"))
-  request.add_header("Authorization", "Basic %s" % base64_auth_string.decode("utf-8"))
+    auth_string = "%s:%s" % (user, password)
+    base64_auth_string = base64.standard_b64encode(auth_string.encode("utf-8"))
+    request.add_header("Authorization", "Basic %s" % base64_auth_string.decode("utf-8"))
 
-  conn = urllib.request.urlopen(request)
-  instruments = conn.read().decode("utf-8")
-  conn.close()
+    conn = urllib.request.urlopen(request)
+    instruments = conn.read().decode("utf-8")
+    conn.close()
 
-  return json.loads(instruments)
+    return json.loads(instruments)
 
-# See if an instrument ID exists in QuinCe. Compares to a previously
-# obtained output from get_instruments
+
+'''
+See if an instrument ID exists in QuinCe. Compares to a previously
+obtained output from get_instruments
+'''
+
+
 def is_quince_instrument(instrument_id, quince_instruments):
-  result = False
+    result = False
 
-  for instrument in quince_instruments:
-    if str(instrument["id"]) == str(instrument_id):
-      result = True
-      break
+    for instrument in quince_instruments:
+        if str(instrument["id"]) == str(instrument_id):
+            result = True
+            break
 
-  return result
+    return result
 
-# Upload a file to QuinCe
+
+'''
+Upload a file to QuinCe
+'''
+
+
 def upload_file(config, instrument_id, filename, contents):
-  quince_url = config["QuinCe"]["url"]
-  user = config["QuinCe"]["user"]
-  password = config["QuinCe"]["password"]
+    quince_url = config["QuinCe"]["url"]
+    user = config["QuinCe"]["user"]
+    password = config["QuinCe"]["password"]
 
-  url = quince_url + "/api/nrt/UploadFile"
+    url = quince_url + "/api/nrt/UploadFile"
 
-  files = {"file" : (filename, contents)}
-  params = {"instrument" : instrument_id}
-  response = requests.post(url, data=params, files=files, auth=(user, password))
-  return response
+    files = {"file": (filename, contents)}
+    params = {"instrument": instrument_id}
+    response = requests.post(url, data=params, files=files, auth=(user, password))
+    return response
 
-# Trigger creation of an NRT dataset
+
+'''
+Trigger creation of an NRT dataset
+'''
+
+
 def make_nrt_dataset(config, instrument_id):
-  quince_url = config["QuinCe"]["url"]
-  user = config["QuinCe"]["user"]
-  password = config["QuinCe"]["password"]
+    quince_url = config["QuinCe"]["url"]
+    user = config["QuinCe"]["user"]
+    password = config["QuinCe"]["password"]
 
-  url = quince_url + "/api/nrt/MakeNrtDataset"
-  params = {"instrument" : instrument_id}
-  response = requests.post(url, data=params, auth=(user, password))
-  return response
+    url = quince_url + "/api/nrt/MakeNrtDataset"
+    params = {"instrument": instrument_id}
+    response = requests.post(url, data=params, auth=(user, password))
+    return response
