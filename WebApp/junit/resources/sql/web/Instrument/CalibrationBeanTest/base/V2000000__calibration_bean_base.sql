c4fb8d0aae0b2695817922b682a1b470bddd5f7a
Steve Jones <steve@squaregoldfish.co.uk>
2021-10-20 18:00:19 +0200

    (#1664) Fix junit

--- WebApp/junit/resources/sql/web/Instrument/CalibrationBeanTest/base/V2000000__calibration_bean_base.sql
+++ WebApp/junit/resources/sql/web/Instrument/CalibrationBeanTest/base/V2000000__calibration_bean_base.sql
@@ -16,19 +16,36 @@ INSERT INTO file_definition (id, instrument_id, description, column_separator,
     '{"valueColumn":3,"hemisphereColumn":-1,"format":0}',
     '{"assignments":{"0":{"assignmentIndex":0,"column":1,"properties":{"formatString":"yyyy-MM-dd HH:mm:ss"}},"1":{"assignmentIndex":1,"column":-1,"properties":{}},"2":{"assignmentIndex":2,"column":-1,"properties":{}},"3":{"assignmentIndex":3,"column":-1,"properties":{}},"4":{"assignmentIndex":4,"column":-1,"properties":{}},"5":{"assignmentIndex":5,"column":-1,"properties":{}},"6":{"assignmentIndex":6,"column":-1,"properties":{}},"7":{"assignmentIndex":7,"column":-1,"properties":{}},"8":{"assignmentIndex":8,"column":-1,"properties":{}},"9":{"assignmentIndex":9,"column":-1,"properties":{}},"10":{"assignmentIndex":10,"column":-1,"properties":{}},"11":{"assignmentIndex":11,"column":-1,"properties":{}},"12":{"assignmentIndex":12,"column":-1,"properties":{}}},"fileHasHeader":false}'
   );
-
+  
+-- Variable
+INSERT INTO instrument_variables (instrument_id, variable_id) VALUES (1000000, 1);
+  
 -- Some file columns with calibratable sensors
+
+-- Intake Temp
 INSERT INTO file_column (id, file_definition_id, file_column, primary_sensor,
     sensor_type, sensor_name)
   VALUES (1001, 1000000, 4, 1, 1, 'SENSOR1');
 
+-- Salinity
 INSERT INTO file_column (id, file_definition_id, file_column, primary_sensor,
     sensor_type, sensor_name)
   VALUES (1002, 1000000, 5, 1, 2, 'SENSOR2');
 
+-- Equilibrator Temp
 INSERT INTO file_column (id, file_definition_id, file_column, primary_sensor,
     sensor_type, sensor_name)
   VALUES (1003, 1000000, 6, 1, 3, 'SENSOR3');
+
+-- xCO₂ (with standards)
+INSERT INTO file_column (id, file_definition_id, file_column, primary_sensor,
+    sensor_type, sensor_name)
+  VALUES (1004, 1000000, 7, 1, 9, 'SENSOR4');
+
+-- xH₂O (with standards)
+INSERT INTO file_column (id, file_definition_id, file_column, primary_sensor,
+    sensor_type, sensor_name)
+  VALUES (1005, 1000000, 8, 1, 8, 'SENSOR5');
 
 
 -- External standard run types
