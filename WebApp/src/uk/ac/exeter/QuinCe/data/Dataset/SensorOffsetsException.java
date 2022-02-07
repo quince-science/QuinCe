280151f5aa3b9023025d92da64296509aefd7116
Steve Jones <steve@squaregoldfish.co.uk>
2022-02-07 15:53:19 +0100

    (#268) Sensor Offset tests

---
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/SensorOffsetsException.java
@@ -0,0 +1,10 @@
+package uk.ac.exeter.QuinCe.data.Dataset;
+
+@SuppressWarnings("serial")
+public class SensorOffsetsException extends Exception {
+
+  public SensorOffsetsException(String message) {
+    super(message);
+  }
+
+}
