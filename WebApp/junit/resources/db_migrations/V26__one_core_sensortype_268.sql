1a625614ed3cdea3a59c7c82501737d76e4a0301
Steve Jones <steve@squaregoldfish.co.uk>
2022-02-10 13:47:50 +0100

    (#268) Merge fixes

--- WebApp/junit/resources/db_migrations/V26__one_core_sensortype_268.sql
+++ WebApp/junit/resources/db_migrations/V26__one_core_sensortype_268.sql
@@ -1,7 +1,8 @@
 -- Each variable must have exactly one core sensor type
 
--- Soderman
+-- Sodemann
 UPDATE variable_sensors SET core = 1 WHERE variable_id = 5 AND sensor_type = 29;
+UPDATE variable_sensors SET core = 0 WHERE variable_id = 16 AND sensor_type = 30;
 
 -- CONTROS pCO2 had two core sensor types
 UPDATE variable_sensors SET core = 0 WHERE variable_id = 6 AND sensor_type = 34;
