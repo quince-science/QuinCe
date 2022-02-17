79991d0ff3e3223e2bce4ad58682a50839a1f0aa
Steve Jones <steve@squaregoldfish.co.uk>
2022-02-17 16:40:24 +0100

    (#2326) SFTP Retriever

---
+++ external_scripts/NRT/SFTPRetriever.py
@@ -0,0 +1,94 @@
+import logging
+import traceback
+import pysftp
+from pathlib import PurePath
+from io import BytesIO
+
+from FileListRetriever import FileListRetriever
+
+
+class SFTPRetriever(FileListRetriever):
+    """
+    File retriever for files held on an FTP server.
+    """
+
+    def __init__(self, instrument_id, logger, configuration=None):
+        super().__init__(instrument_id, logger, configuration)
+        self._conn = None
+
+    @staticmethod
+    def _get_config_entries():
+        return ['Server', 'Port', 'User', 'Private Key File', 'Private Key Password',
+                'Source Folder', 'File Specification']
+
+    @staticmethod
+    def get_type():
+        return "SFTP"
+
+    # noinspection PyBroadException
+    def test_configuration(self):
+        config_ok = True
+        conn = None
+
+        try:
+            conn = pysftp.Connection(host=self._configuration["Server"],
+                                     port=int(self._configuration["Port"]),
+                                     username=self._configuration["User"],
+                                     private_key=self._configuration["Private Key File"],
+                                     private_key_pass=self._configuration["Private Key Password"])
+
+            if config_ok:
+                try:
+                    conn.chdir(self._configuration['Source Folder'])
+                except Exception:
+                    config_ok = False
+                    print("Cannot access source folder: " + traceback.format_exc())
+                    self.log(logging.CRITICAL, "Cannot access source folder: "
+                             + traceback.format_exc())
+        except Exception as e:
+            config_ok = False
+            print(f"Cannot connect to FTP server: {e}")
+            self.log(logging.CRITICAL, f"Cannot connect to SFTP server: {traceback.format_exc()}")
+        finally:
+            if conn is not None:
+                conn.close()
+
+        return config_ok
+
+    # noinspection PyBroadException
+    def startup(self):
+        result = True
+
+        try:
+            self._conn = pysftp.Connection(host=self._configuration["Server"],
+                                           port=int(self._configuration["Port"]),
+                                           username=self._configuration["User"],
+                                           private_key=self._configuration["Private Key File"],
+                                           private_key_pass=self._configuration["Private Key Password"])
+            self._conn.chdir(self._configuration['Source Folder'])
+        except Exception:
+            self.log(logging.CRITICAL, "Cannot log in to FTP server: "
+                     + traceback.format_exc())
+            result = False
+
+        return result
+
+    # noinspection PyBroadException
+    def shutdown(self):
+        if self._conn is not None:
+            try:
+                self._conn.close()
+            except Exception:
+                pass
+            finally:
+                self._conn = None
+
+    def _get_all_files(self):
+        all_files = self._conn.listdir()
+        return list(filter(lambda name: PurePath(name).match(self._configuration["File Specification"]), all_files))
+
+    def _load_file(self, filename):
+        data = BytesIO()
+        self._conn.getfo(filename, data)
+        data.seek(0)
+        return data.read()
