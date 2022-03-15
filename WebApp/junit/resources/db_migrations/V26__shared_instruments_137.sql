9938784b1a3b29f3f1d0dbbef097dc18a617c1cb
Steve Jones <steve@squaregoldfish.co.uk>
2022-03-15 12:42:26 +0100

    (#137) Instrument sharing (no UI)

---
+++ WebApp/junit/resources/db_migrations/V26__shared_instruments_137.sql
@@ -0,0 +1,16 @@
+CREATE TABLE shared_instruments (
+  instrument_id INT(11) NOT NULL,
+  shared_with INT(11) NOT NULL,
+  PRIMARY KEY (instrument_id, shared_with),
+  CONSTRAINT sharedinstruments_instrument
+    FOREIGN KEY (instrument_id)
+    REFERENCES instrument (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT sharedinstruments_sharedwith
+    FOREIGN KEY (shared_with)
+    REFERENCES user (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+    
+CREATE INDEX sharedinstruments_sharedwith_idx ON shared_instruments(shared_with);
---
+++ src/migrations/db_migrations/V26__shared_instruments_137.sql
@@ -0,0 +1,15 @@
+CREATE TABLE shared_instruments (
+  instrument_id INT(11) NOT NULL,
+  shared_with INT(11) NOT NULL,
+  PRIMARY KEY (instrument_id, shared_with),
+  INDEX sharedinstruments_sharedwith_idx (shared_with ASC),
+  CONSTRAINT sharedinstruments_instrument
+    FOREIGN KEY (instrument_id)
+    REFERENCES instrument (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT sharedinstruments_sharedwith
+    FOREIGN KEY (shared_with)
+    REFERENCES user (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
