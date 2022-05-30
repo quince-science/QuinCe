ccd2da2a97dfc13003fce9965be9db63dcc89949
Steve Jones <steve@squaregoldfish.co.uk>
2022-05-30 14:25:04 +0200

    (#1512) SOCAT reformatter updated; First KPH platform info

--- external_scripts/export/socat_formatter.py
+++ external_scripts/export/socat_formatter.py
@@ -1,4 +1,4 @@
-'''
+"""
 NB THIS IS NOT PART OF THE MAIN EXPORT SCRIPT - IT JUST USES SOME OF
 THE LIBRARIES
 
@@ -7,88 +7,121 @@ Formats export files from QuinCe for submission to SOCAT
 Files exported from QuinCe in the SOCAT format need some changes before they
 can be sent to the SOCAT upload dashboard:
 
-- Date/Time column reformatted
-  from 'YYYY-MM-DDTHH:MM:SS.SSSZ' to 'YYYY-MM-DD HH:MM:SS.SSS'
 - Add header containing simple metadata
   (EXPO Code, Vessel Name, PIs, Vessel Type)
   This information will be read from the input filename and platforms.toml
 - Rename QC columns
 
-Input files are expected to have a filename of <expocode>-SOCAT.tsv
+The script will take an QuinCe export ZIP file
 Output files will be named <expocode>.tsv
 
 Usage:
 
-python socat_formatter.py <input_file>
+python socat_formatter.py <zip_file>
 
-Steve Jones 2021-01-06
-'''
-import sys
+Steve Jones
+"""
+import argparse
+import json
+
 import toml
 import re
-import pandas as pd
+import zipfile
+import os.path
+
 
 def main(infile):
-  metadata = build_metadata(infile)
 
-  # Load the input file
-  data = pd.read_table(infile)
+    manifest, file_content = extract_zip(infile)
+    metadata = build_metadata(manifest)
 
-  # Fix column data types - read_table doesn't get them all right
-  for col in data.columns:
-    if col.endswith('QC Flag'):
-      data = data.astype({col : 'Int64'})
+    # Write header
+    out_file = f'{metadata["expocode"]}.tsv'
+    with open(out_file, 'w') as out:
+        out.write(f'Expocode: {metadata["expocode"]}\n')
+        out.write(f'Vessel Name: {metadata["vessel_name"]}\n')
+        out.write(f'PIs: {metadata["pis"]}\n')
+        out.write(f'Org: {metadata["org"]}\n')
+        out.write(f'Vessel type: {metadata["vessel_type"]}\n')
+        out.write('Suggested QC: 2\n')
 
-  # Reformat date/time
-  data.iloc[:, 0] = pd.to_datetime(data.iloc[:,0]) \
-    .dt.strftime('%Y-%m-%d %H:%M:%S.%f')
+    with open(out_file, 'ab') as out:
+        out.write(file_content)
 
-  # Write header
-  out_file = f'{metadata["expocode"]}.tsv'
-  with open(out_file, 'w') as out:
-    out.write(f'Expocode: {metadata["expocode"]}\n')
-    out.write(f'Vessel Name: {metadata["vessel_name"]}\n')
-    out.write(f'PIs: {metadata["pis"]}\n')
-    out.write(f'Vessel Type: {metadata["vessel_type"]}\n')
 
-  data.to_csv(out_file, sep='\t', index=False, mode='a')
+def extract_zip(infile):
+    """
+    Extract the required parts of the QuinCe export ZIP
+    """
 
-def build_metadata(filename):
-  with open('platforms.toml') as f: platforms = toml.load(f)
+    manifest = None
+    content = None
 
-  metadata = {}
+    if not zipfile.is_zipfile(infile):
+        raise zipfile.BadZipFile('Supplied file is not a ZIP file')
 
-  m = re.match('(.*)([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])-SOCAT.tsv',
-    filename)
+    basename = os.path.splitext(os.path.basename(infile))[0]
 
-  platform = m[1]
-  date = m[2]
-  metadata['expocode'] = f'{platform}{date}'
+    with zipfile.ZipFile(infile) as in_zip:
+        manifest_path = zipfile.Path(in_zip, f'{basename}/manifest.json')
+        if not manifest_path.exists():
+            raise KeyError('ZIP file is missing manifest.json')
 
-  try:
-    platform_info = platforms[platform]
-    metadata['vessel_name'] = platform_info['name']
-    metadata['vessel_type'] = \
-      get_platform_type_name(platform_info['category_code'])
+        manifest = json.loads(manifest_path.read_text())
+        dataset_name = manifest['manifest']['metadata']['name']
 
-    metadata['pis'] = platform_info['author_list']
-  except KeyError:
-    raise Exception(f'Platform {platform} not found')
+        dataset_filename = f'{basename}/dataset/SOCAT/{dataset_name}.tsv'
+        dataset_path = zipfile.Path(in_zip, dataset_filename)
+        if not dataset_path.exists():
+            raise KeyError('ZIP file does not contain SOCAT export')
+        content = in_zip.read(dataset_filename)
 
-  return metadata
+    return manifest, content
+
+
+def build_metadata(manifest):
+    with open('platforms.toml') as f:
+        platforms = toml.load(f)
+
+    metadata = {}
+
+    dataset_name = manifest['manifest']['metadata']['name']
+
+    m = re.match('(.*)(\\d\\d\\d\\d\\d\\d\\d\\d)',
+                 dataset_name)
+
+    platform = m[1]
+    date = m[2]
+    metadata['expocode'] = f'{platform}{date}'
+
+    try:
+        platform_info = platforms[platform]
+        metadata['vessel_name'] = platform_info['name']
+        metadata['vessel_type'] = \
+            get_platform_type_name(platform_info['category_code'])
+
+        metadata['pis'] = platform_info['author_list']
+        metadata['org'] = platform_info['affiliation']
+    except KeyError:
+        raise Exception(f'Platform {platform} not found')
+
+    return metadata
+
 
 def get_platform_type_name(code):
-  if code == '31':
-    return 'Ship-based time series (vessel)'
-  elif code == '32':
-    return 'Voluntary Observing Ship '
-  elif code == '3B':
-    return 'Saildrone'
-  else:
-    raise Exception(f'Unrecognised platform code {code}')
+    if code == '31':
+        return 'Ship'
+    elif code == '32':
+        return 'Ship '
+    elif code == '3B':
+        return 'Saildrone'
+    else:
+        raise Exception(f'Unrecognised platform code {code}')
+
 
 if __name__ == '__main__':
-  if len(sys.argv) != 2:
-   print('Usage: python socat_formatter.py <input_file>')
-  else:
-    main(sys.argv[1])
+    parser = argparse.ArgumentParser(description='Prepare a SOCAT export from QuinCe for submission')
+    parser.add_argument('zipfile', type=str, help='the QuinCe export file')
+    args = parser.parse_args()
+    main(args.zipfile)
+    exit()
