cf3d7948af5d0d92c399067ea33afdafa7f0ed83
Steve Jones <steve@squaregoldfish.co.uk>
2022-06-08 11:07:27 +0200

    (#1967) SQL changes - add new message fields

--- WebApp/junit/resources/sql/testbase/dataset/V1000003__dataset.sql
+++ WebApp/junit/resources/sql/testbase/dataset/V1000003__dataset.sql
@@ -7,7 +7,7 @@
 INSERT INTO dataset (
     id, instrument_id, name, start, end, min_longitude,
     max_longitude, min_latitude, max_latitude, status, nrt, status_date,
-    properties, messages_json, last_touched
+    properties, error_messages, last_touched
   ) VALUES (
     1, 1, 'BSBS20210101', 1609459200, 1609545600, 0, 0, 0, 0, 0, 0, 1609718400,
     '{"_INSTRUMENT":{"depth":"5","postFlushingTime":"0","preFlushingTime":"0"},"Underway Marine pCO₂":{},"Underway Atmospheric pCO₂":{"atm_pres_sensor_height":"10.0"}}',
