fd58ecd347c2d1682288144752647888eeb89686
Steve Jones <steve@squaregoldfish.co.uk>
2022-09-08 15:30:24 +0200

    (#2397) Improved Pro Oceanus QC

---
+++ WebApp/junit/resources/db_migrations/V32__pro_oceanus_update_2397.sql
@@ -0,0 +1,33 @@
+-- Add the Pro Oceanus Current Count.
+-- Not used for calculations, but can be useful for QC
+INSERT INTO sensor_types (
+    name, vargroup, parent, depends_on, depends_question, internal_calibration,
+    diagnostic, display_order, column_code, column_heading, units
+  ) VALUES (
+    'ProOceanus Current Count', 'Sensor Internal', NULL, NULL, NULL, 0, 0, 1255,
+    'PROCO2CURR', 'Zero Count', NULL
+  );
+
+
+
+INSERT INTO variable_sensors (
+    variable_id, sensor_type, core, questionable_cascade, bad_cascade,
+    export_column_short, export_column_long, export_column_code
+  )
+  VALUES (
+    (SELECT id FROM variables WHERE name = 'Pro Oceanus CO₂ Water'),
+    (SELECT id FROM sensor_types WHERE column_code = 'PROCO2CURR'),
+    0, 3, 4, NULL, NULL, NULL
+  );
+
+-- Add Salinity requirement to Pro Oceanus. Not used in calculations,
+-- but it should still be recorded as a matter of course.
+INSERT INTO variable_sensors (
+    variable_id, sensor_type, core, questionable_cascade, bad_cascade,
+    export_column_short, export_column_long, export_column_code
+  )
+  VALUES (
+    (SELECT id FROM variables WHERE name = 'Pro Oceanus CO₂ Water'),
+    (SELECT id FROM sensor_types WHERE column_code = 'PSALPR01'),
+    0, 2, 2, NULL, NULL, NULL
+  );
---
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/QC/DataReduction/ZeroCurrentComparisonRoutine.java
@@ -0,0 +1,71 @@
+package uk.ac.exeter.QuinCe.data.Dataset.QC.DataReduction;
+
+import java.sql.Connection;
+import java.util.Map;
+import java.util.TreeMap;
+
+import uk.ac.exeter.QuinCe.data.Dataset.DataSet;
+import uk.ac.exeter.QuinCe.data.Dataset.DatasetSensorValues;
+import uk.ac.exeter.QuinCe.data.Dataset.Measurement;
+import uk.ac.exeter.QuinCe.data.Dataset.MeasurementValue;
+import uk.ac.exeter.QuinCe.data.Dataset.DataReduction.ReadOnlyDataReductionRecord;
+import uk.ac.exeter.QuinCe.data.Dataset.QC.Flag;
+import uk.ac.exeter.QuinCe.data.Dataset.QC.RoutineException;
+import uk.ac.exeter.QuinCe.data.Dataset.QC.RoutineFlag;
+import uk.ac.exeter.QuinCe.data.Dataset.QC.SensorValues.FlaggedItems;
+import uk.ac.exeter.QuinCe.data.Instrument.Instrument;
+import uk.ac.exeter.QuinCe.data.Instrument.SensorDefinition.SensorType;
+import uk.ac.exeter.QuinCe.data.Instrument.SensorDefinition.Variable;
+import uk.ac.exeter.QuinCe.web.system.ResourceManager;
+
+public class ZeroCurrentComparisonRoutine extends DataReductionQCRoutine {
+
+  @Override
+  public String getShortMessage() {
+    return "Current count above zero count";
+  }
+
+  @Override
+  public String getLongMessage(RoutineFlag flag) {
+    return "Current count should be below zero count";
+  }
+
+  @Override
+  protected void qcAction(Connection conn, Instrument instrument,
+    DataSet dataSet, Variable variable,
+    TreeMap<Measurement, ReadOnlyDataReductionRecord> dataReductionRecords,
+    DatasetSensorValues allSensorValues, FlaggedItems flaggedItems)
+    throws RoutineException {
+
+    try {
+      // All values detected by this routine get the same flag
+      RoutineFlag flag = new RoutineFlag(this, Flag.BAD, null, null);
+
+      SensorType zeroCountType = ResourceManager.getInstance()
+        .getSensorsConfiguration().getSensorType("ProOceanus Zero Count");
+      SensorType currentCountType = ResourceManager.getInstance()
+        .getSensorsConfiguration().getSensorType("ProOceanus Current Count");
+
+      for (Map.Entry<Measurement, ReadOnlyDataReductionRecord> entry : dataReductionRecords
+        .entrySet()) {
+
+        Measurement measurement = entry.getKey();
+
+        MeasurementValue zeroCount = measurement
+          .getMeasurementValue(zeroCountType);
+        MeasurementValue currentValue = measurement
+          .getMeasurementValue(currentCountType);
+
+        if (currentValue.getCalculatedValue() >= zeroCount
+          .getCalculatedValue()) {
+          flagSensors(instrument, measurement, entry.getValue(),
+            allSensorValues, flag, flaggedItems);
+        }
+      }
+
+    } catch (Exception e) {
+      throw new RoutineException(e);
+    }
+  }
+
+}
