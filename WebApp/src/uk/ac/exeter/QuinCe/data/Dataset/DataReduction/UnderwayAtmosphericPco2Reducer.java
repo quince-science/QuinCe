e2b7638c1f95789fb42af47fd872650b9ce1e1ce
Steve Jones <steve@squaregoldfish.co.uk>
2023-02-15 16:11:09 +0100

    (#2598) xCO2 for Pro Oceanus and underway CO2
    Export config updates, plus bug fix for instruments with no diagnostics

--- WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/UnderwayAtmosphericPco2Reducer.java
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/UnderwayAtmosphericPco2Reducer.java
@@ -53,6 +53,7 @@ public class UnderwayAtmosphericPco2Reducer extends DataReducer {
 
     record.put("Sea Level Pressure", seaLevelPressure);
     record.put("pH₂O", pH2O);
+    record.put("xCO₂", co2InGas);
     record.put("pCO₂", pCO2);
     record.put("fCO₂", fCO2);
   }
@@ -66,7 +67,7 @@ public class UnderwayAtmosphericPco2Reducer extends DataReducer {
   @Override
   public List<CalculationParameter> getCalculationParameters() {
     if (null == calculationParameters) {
-      calculationParameters = new ArrayList<CalculationParameter>(4);
+      calculationParameters = new ArrayList<CalculationParameter>(5);
 
       calculationParameters.add(new CalculationParameter(makeParameterId(0),
         "Sea Level Pressure", "Sea Level Pressure", "CAPASS01", "hPa", false));
@@ -75,9 +76,12 @@ public class UnderwayAtmosphericPco2Reducer extends DataReducer {
         "pH₂O", "Atmosphere Water Vapour Pressure", "CPVPZZ01", "hPa", false));
 
       calculationParameters.add(new CalculationParameter(makeParameterId(2),
-        "pCO₂", "pCO₂ In Atmosphere", "ACO2XXXX", "μatm", true));
+        "xCO₂", "xCO₂ In Atmosphere", "XCO2DRAT", "μmol mol⁻¹", true));
 
       calculationParameters.add(new CalculationParameter(makeParameterId(3),
+        "pCO₂", "pCO₂ In Atmosphere", "ACO2XXXX", "μatm", true));
+
+      calculationParameters.add(new CalculationParameter(makeParameterId(4),
         "fCO₂", "fCO₂ In Atmosphere", "FCO2WTAT", "μatm", true));
     }
 
