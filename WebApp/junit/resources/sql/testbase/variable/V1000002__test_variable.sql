36f09dacab4a10af4d93822a3f4a8b164ac34d49
Steve Jones <steve@squaregoldfish.co.uk>
2023-04-17 23:50:27 +0200

    (junit) First Variable tests

--- WebApp/junit/resources/sql/testbase/variable/V1000002__test_variable.sql
+++ WebApp/junit/resources/sql/testbase/variable/V1000002__test_variable.sql
@@ -1,11 +1,11 @@
 -- Test variable
-INSERT INTO variables (name) VALUES ('testVar');
+INSERT INTO variables (id, name) VALUES (1000000, 'testVar');
 
 -- Test sensor type
-INSERT INTO sensor_types (name, vargroup, display_order) VALUES ('testSensor', 'testGroup', 1000);
+INSERT INTO sensor_types (id, name, vargroup, display_order) VALUES (1000000, 'testSensor', 'testGroup', 1000);
 
 -- An unused sensor type
-INSERT INTO sensor_types (name, vargroup, display_order) VALUES ('Unused sensor', 'Unusded group', 1001);
+INSERT INTO sensor_types (id, name, vargroup, display_order) VALUES (1000001, 'Unused sensor', 'Unusded group', 1001);
 
 -- Variables for Test - SST, Salinity and the test sensor
 INSERT INTO variable_sensors
@@ -21,7 +21,7 @@ INSERT INTO variable_sensors
   VALUES (
     (SELECT id FROM variables WHERE name = 'testVar'),
     (SELECT id FROM sensor_types WHERE name = 'Salinity'),
-    0, 3, 4
+    0, 4, 3
   );
 
 INSERT INTO variable_sensors
