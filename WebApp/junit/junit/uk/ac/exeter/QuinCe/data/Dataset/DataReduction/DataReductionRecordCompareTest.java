75fe7c681541929df289b8bcacef7d0a92316e30
Steve Jones <steve@squaregoldfish.co.uk>
2023-08-11 15:55:30 +0200

    JavaDoc

--- WebApp/junit/junit/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/DataReductionRecordCompareTest.java
+++ WebApp/junit/junit/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/DataReductionRecordCompareTest.java
@@ -18,41 +18,123 @@ import uk.ac.exeter.QuinCe.data.Dataset.DataReduction.DataReductionRecord;
 import uk.ac.exeter.QuinCe.data.Instrument.SensorDefinition.Variable;
 
 /**
- * Tests for the {@link DataReductionRecord#compareTo(DataReductionRecord)
+ * Tests for the {@link DataReductionRecord#compareTo(DataReductionRecord)}
  * method.
- *
- * @author stevej
- *
  */
 @TestInstance(Lifecycle.PER_CLASS)
 public class DataReductionRecordCompareTest extends TestSetTest {
 
+  /**
+   * A column index in the Test Set file for {@link #compareTests(TestSetLine)}.
+   */
   private static final int MEASUREMENT_ID_COL = 0;
 
+  /**
+   * A column index in the Test Set file for {@link #compareTests(TestSetLine)}.
+   */
   private static final int VARIABLE_ID_COL = 1;
 
+  /**
+   * A column index in the Test Set file for {@link #compareTests(TestSetLine)}.
+   */
   private static final int COMPARE_MEASUREMENT_ID_COL = 2;
 
+  /**
+   * A column index in the Test Set file for {@link #compareTests(TestSetLine)}.
+   */
   private static final int COMPARE_VARIABLE_ID_COL = 3;
 
+  /**
+   * A column index in the Test Set file for {@link #compareTests(TestSetLine)}.
+   */
   private static final int COMPARE_SIGN_COL = 4;
 
+  /**
+   * Create a mock {@link Measurement} object with the specified database ID.
+   * 
+   * @param id
+   *          The database ID.
+   * @return The Measurement object.
+   */
   private Measurement makeMeasurement(long id) {
     Measurement measurement = Mockito.mock(Measurement.class);
     Mockito.when(measurement.getId()).thenReturn(id);
     return measurement;
   }
 
+  /**
+   * Create a mock {@link Variable} object with the specified database ID.
+   * 
+   * @param id
+   *          The database ID.
+   * @return The Variable object.
+   */
   private Variable makeVariable(long id) {
     Variable variable = Mockito.mock(Variable.class);
     Mockito.when(variable.getId()).thenReturn(id);
     return variable;
   }
 
+  /**
+   * Create a dummy set of parameters for a constructed
+   * {@link DataReductionRecord}.
+   * 
+   * @return The parameters.
+   */
   private List<String> makeParameters() {
     return Arrays.asList(new String[] { "Param 1" });
   }
 
+  /**
+   * Run the comparison tests defined in the test set specified by
+   * {@link #getTestSetName()}.
+   * 
+   * <p>
+   * These are defined in the file
+   * {@code WebApp/junit/resources/testsets/DataReductionRecordCompare.csv}. The
+   * file defines a number of different {@link DataReductionRecord}s to be
+   * compared and their expected outcomes. The column headers in the file are as
+   * follows:
+   * </p>
+   *
+   * <table>
+   * <caption>Columns required for the {@code DataReductionRecordCompare} test
+   * set</caption>
+   * <tr>
+   * <th>Column Name</th>
+   * <th>Purpose</th>
+   * </tr>
+   * <tr>
+   * <td style="vertical-align: top">Measurement ID</td>
+   * <td>The record's {@link Measurement} ID of the base
+   * {@link DataReductionRecord}</td>
+   * </tr>
+   * <tr>
+   * <td style="vertical-align: top">Variable ID</td>
+   * <td>The {@link Variable} being measured by the {@link Measurement}</td>
+   * </tr>
+   * <tr>
+   * <td style="vertical-align: top">Compare Measurement ID</td>
+   * <td>The {@link Measurement} ID of the {@link DataReductionRecord} to be
+   * compared</td>
+   * </tr>
+   * <tr>
+   * <td style="vertical-align: top">Compare Variable ID</td>
+   * <td>The {@link Variable} measured by the {@link Measurement} being
+   * compared</td>
+   * </tr>
+   * <tr>
+   * <td style="vertical-align: top">Compare Sign</td>
+   * <td>The expected result of the comparison: One of Positive ({@code 1}),
+   * Zero ({@code 0} for equality, or Negative (@{code -1}).</td>
+   * </tr>
+   * </table>
+   *
+   * @param line
+   *          The line number for the test.
+   * @throws Exception
+   *           For any unexpected errors.
+   */
   @ParameterizedTest
   @MethodSource("getLines")
   public void compareTests(TestSetLine line) throws Exception {
