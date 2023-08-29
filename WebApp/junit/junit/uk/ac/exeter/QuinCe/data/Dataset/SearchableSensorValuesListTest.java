fa82f7b91c3a9c325b1017b8f365bde9e1878871
Steve Jones <steve@squaregoldfish.co.uk>
2023-08-29 11:49:33 +0200

    (#2455) SearchableSensorValuesList is self-ordering

--- WebApp/junit/junit/uk/ac/exeter/QuinCe/data/Dataset/SearchableSensorValuesListTest.java
+++ WebApp/junit/junit/uk/ac/exeter/QuinCe/data/Dataset/SearchableSensorValuesListTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -19,31 +20,29 @@ import uk.ac.exeter.QuinCe.data.Dataset.QC.SensorValues.AutoQCResult;
 
 public class SearchableSensorValuesListTest extends BaseTest {
 
-  private SearchableSensorValuesList rangeTestList() {
-    SearchableSensorValuesList list = new SearchableSensorValuesList(1);
+  private SearchableSensorValuesList makePopulatedList() {
+    SearchableSensorValuesList list = new SearchableSensorValuesList(1L);
 
-    list.add(makeSensorValue(1, 20));
-    list.add(makeSensorValue(2, 25));
-    list.add(makeSensorValue(3, 30));
-    list.add(makeSensorValue(4, 35));
-    list.add(makeSensorValue(5, 40));
-    list.add(makeSensorValue(6, 45));
+    list.add(makeSensorValue(1L, 20));
+    list.add(makeSensorValue(2L, 25));
+    list.add(makeSensorValue(3L, 30));
+    list.add(makeSensorValue(4L, 35));
+    list.add(makeSensorValue(5L, 40));
+    list.add(makeSensorValue(6L, 45));
 
     return list;
   }
 
   private SensorValue makeSensorValue(long id, int minute) {
-    return makeSensorValue(id, minute, Flag.GOOD, false);
+    return makeSensorValue(id, 1L, minute, Flag.GOOD, false);
   }
 
-  private SensorValue mockSensorValueWithColumnId(long columnId) {
-    SensorValue value = Mockito.mock(SensorValue.class);
-    Mockito.when(value.getColumnId()).thenReturn(4L);
-    return value;
+  private SensorValue makeSensorValue(long valueId, long columnId, int minute) {
+    return makeSensorValue(valueId, columnId, minute, Flag.GOOD, false);
   }
 
-  private SensorValue makeSensorValue(long id, int minute, Flag flag,
-    boolean needed) {
+  private SensorValue makeSensorValue(long id, long columnId, int minute,
+    Flag flag, boolean needed) {
 
     AutoQCResult autoQC;
     if (!needed) {
@@ -54,7 +53,7 @@ public class SearchableSensorValuesListTest extends BaseTest {
 
     Flag userFlag = needed ? Flag.NEEDED : flag;
 
-    return new SensorValue(id, 1, 1, makeTime(minute), "29.111", autoQC,
+    return new SensorValue(id, 1L, columnId, makeTime(minute), "29.111", autoQC,
       userFlag, "Dummy");
   }
 
@@ -102,93 +101,93 @@ public class SearchableSensorValuesListTest extends BaseTest {
   @Test
   public void listConstructorTest() {
     SearchableSensorValuesList list = SearchableSensorValuesList
-      .newFromSensorValueCollection(rangeTestList());
-    assertEquals(rangeTestList().size(), list.size());
+      .newFromSensorValueCollection(makePopulatedList());
+    assertEquals(makePopulatedList().size(), list.size());
   }
 
   @Test
   public void rangeSearchAllBeforeTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(0),
-      makeTime(10));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(0), makeTime(10));
     assertEquals(0, searchResult.size());
   }
 
   @Test
   public void rangeSearchAllAfterTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(50),
-      makeTime(55));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(50), makeTime(55));
     assertEquals(0, searchResult.size());
   }
 
   @Test
   public void rangeSearchFullRangeTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(10),
-      makeTime(55));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(10), makeTime(55));
     assertTrue(
       checkSearchResultIds(searchResult, new long[] { 1, 2, 3, 4, 5, 6 }));
   }
 
   @Test
   public void rangeSearchMiddleMissTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(31),
-      makeTime(34));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(31), makeTime(34));
     assertEquals(0, searchResult.size());
   }
 
   @Test
   public void rangeSearchInexactStartTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(27),
-      makeTime(50));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(27), makeTime(50));
     assertTrue(checkSearchResultIds(searchResult, new long[] { 3, 4, 5, 6 }));
   }
 
   @Test
   public void rangeSearchExactStartTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(30),
-      makeTime(50));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(30), makeTime(50));
     assertTrue(checkSearchResultIds(searchResult, new long[] { 3, 4, 5, 6 }));
   }
 
   @Test
   public void rangeSearchInexactEndTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(30),
-      makeTime(42));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(30), makeTime(42));
     assertTrue(checkSearchResultIds(searchResult, new long[] { 3, 4, 5 }));
   }
 
   @Test
   public void rangeSearchExactEndTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(30),
-      makeTime(40));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(30), makeTime(40));
     assertTrue(checkSearchResultIds(searchResult, new long[] { 3, 4 }));
   }
 
   @Test
   public void rangeSearchStartBeforeTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(10),
-      makeTime(40));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(10), makeTime(40));
     assertTrue(checkSearchResultIds(searchResult, new long[] { 1, 2, 3, 4 }));
   }
 
   @Test
   public void rangeSearchEndAfterTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(25),
-      makeTime(55));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(25), makeTime(55));
     assertTrue(
       checkSearchResultIds(searchResult, new long[] { 2, 3, 4, 5, 6 }));
   }
 
   @Test
   public void rangeSearchLastEntryTest() throws Exception {
-    List<SensorValue> searchResult = rangeTestList().rangeSearch(makeTime(45),
-      makeTime(55));
+    List<SensorValue> searchResult = makePopulatedList()
+      .rangeSearch(makeTime(45), makeTime(55));
     assertTrue(checkSearchResultIds(searchResult, new long[] { 6 }));
   }
 
   @Test
   public void rangeSearchBadArgumentOrderTest() {
     assertThrows(IllegalArgumentException.class, () -> {
-      rangeTestList().rangeSearch(makeTime(55), makeTime(25));
+      makePopulatedList().rangeSearch(makeTime(55), makeTime(25));
     });
 
   }
@@ -196,41 +195,13 @@ public class SearchableSensorValuesListTest extends BaseTest {
   @Test
   public void rangeSearchBadEqualArgumentTest() {
     assertThrows(IllegalArgumentException.class, () -> {
-      rangeTestList().rangeSearch(makeTime(30), makeTime(30));
+      makePopulatedList().rangeSearch(makeTime(30), makeTime(30));
     });
-  }
-
-  /**
-   * Make sure we can have more than one column ID
-   */
-  @Test
-  public void multipleColumnIdsTest() {
-    List<Long> columnIds = new ArrayList<Long>();
-    columnIds.add(4L);
-    columnIds.add(8L);
-
-    SearchableSensorValuesList list = new SearchableSensorValuesList(columnIds);
-    list.add(mockSensorValueWithColumnId(8));
-    list.add(mockSensorValueWithColumnId(4));
-  }
-
-  /**
-   * Test creating a list from sensor values with multiple column IDs
-   */
-  @Test
-  public void newFromListMultipleColumnIdsTest() {
-
-    List<SensorValue> values = new ArrayList<SensorValue>();
-    values.add(mockSensorValueWithColumnId(4));
-    values.add(mockSensorValueWithColumnId(8));
-
-    SearchableSensorValuesList.newFromSensorValueCollection(values);
-
   }
 
   @Test
   public void getClosestExactTest() {
-    SearchableSensorValuesList list = rangeTestList();
+    SearchableSensorValuesList list = makePopulatedList();
     List<SensorValue> found = list.getClosest(makeTime(35));
     assertEquals(1, found.size());
     assertEquals(4L, found.get(0).getId());
@@ -245,7 +216,7 @@ public class SearchableSensorValuesListTest extends BaseTest {
 
   @Test
   public void getClosestBetweenTest() {
-    SearchableSensorValuesList list = rangeTestList();
+    SearchableSensorValuesList list = makePopulatedList();
     List<SensorValue> found = list.getClosest(makeTime(32));
     assertEquals(2, found.size());
     assertEquals(3L, found.get(0).getId());
@@ -254,7 +225,7 @@ public class SearchableSensorValuesListTest extends BaseTest {
 
   @Test
   public void getClosestStartTest() {
-    SearchableSensorValuesList list = rangeTestList();
+    SearchableSensorValuesList list = makePopulatedList();
     List<SensorValue> found = list.getClosest(makeTime(15));
     assertEquals(1, found.size());
     assertEquals(1L, found.get(0).getId());
@@ -262,9 +233,149 @@ public class SearchableSensorValuesListTest extends BaseTest {
 
   @Test
   public void getClosestEndTest() {
-    SearchableSensorValuesList list = rangeTestList();
+    SearchableSensorValuesList list = makePopulatedList();
     List<SensorValue> found = list.getClosest(makeTime(50));
     assertEquals(1, found.size());
     assertEquals(6L, found.get(0).getId());
+  }
+
+  /**
+   * Test creating a list from sensor values with multiple column IDs
+   */
+  @Test
+  public void newFromListMultipleColumnIdsTest() {
+    List<SensorValue> values = new ArrayList<SensorValue>();
+    values.add(makeSensorValue(1L, 4L, 30));
+    values.add(makeSensorValue(2L, 8L, 31));
+    SearchableSensorValuesList.newFromSensorValueCollection(values);
+  }
+
+  @Test
+  public void addToEmptyListTest() {
+    SearchableSensorValuesList list = new SearchableSensorValuesList(1L);
+    list.add(makeSensorValue(1L, 30));
+    assertEquals(1, list.size());
+  }
+
+  @Test
+  public void addNullTest() {
+    SearchableSensorValuesList list = new SearchableSensorValuesList(1L);
+    assertThrows(IllegalArgumentException.class, () -> {
+      list.add(null);
+    });
+  }
+
+  @Test
+  public void singleColumnInvalidColumnTest() {
+    SearchableSensorValuesList list = new SearchableSensorValuesList(1L);
+    assertThrows(IllegalArgumentException.class, () -> {
+      list.add(makeSensorValue(1L, 2L, 30));
+    });
+  }
+
+  @Test
+  public void addMultipleColumnsTest() {
+    List<Long> columns = Arrays.asList(1L, 2L);
+    SearchableSensorValuesList list = new SearchableSensorValuesList(columns);
+    list.add(makeSensorValue(1L, 10));
+    list.add(makeSensorValue(2L, 20));
+    assertEquals(2, list.size());
+  }
+
+  @Test
+  public void invalidMultipleColumnsTest() {
+    List<Long> columns = Arrays.asList(1L, 2L);
+    SearchableSensorValuesList list = new SearchableSensorValuesList(columns);
+    assertThrows(IllegalArgumentException.class, () -> {
+      list.add(makeSensorValue(1L, 3L, 30));
+    });
+  }
+
+  @Test
+  public void sameTimeAsLastTest() {
+    SearchableSensorValuesList list = makePopulatedList();
+    assertThrows(IllegalArgumentException.class, () -> {
+      list.add(makeSensorValue(10L, 45));
+    });
+  }
+
+  @Test
+  public void sameTimeAsFirstTest() {
+    SearchableSensorValuesList list = makePopulatedList();
+    assertThrows(IllegalArgumentException.class, () -> {
+      list.add(makeSensorValue(10L, 20));
+    });
+  }
+
+  @Test
+  public void sameTimeAsMidTest() {
+    SearchableSensorValuesList list = makePopulatedList();
+    assertThrows(IllegalArgumentException.class, () -> {
+      list.add(makeSensorValue(10L, 30));
+    });
+  }
+
+  private boolean listIsOrdered(SearchableSensorValuesList list) {
+    boolean result = true;
+
+    for (int i = 1; i < list.size(); i++) {
+      LocalDateTime prev = list.get(i - 1).getTime();
+      LocalDateTime current = list.get(i).getTime();
+      if (!prev.isBefore(current)) {
+        result = false;
+        break;
+      }
+    }
+
+    return result;
+  }
+
+  @Test
+  public void addBeforeFirst() {
+    SearchableSensorValuesList list = makePopulatedList();
+    list.add(makeSensorValue(10L, 0));
+    assertTrue(listIsOrdered(list));
+  }
+
+  @Test
+  public void addAfterLast() {
+    SearchableSensorValuesList list = makePopulatedList();
+    list.add(makeSensorValue(10L, 59));
+    assertTrue(listIsOrdered(list));
+  }
+
+  @Test
+  public void addInMiddle() {
+    SearchableSensorValuesList list = makePopulatedList();
+    list.add(makeSensorValue(10L, 32));
+    assertTrue(listIsOrdered(list));
+  }
+
+  @Test
+  public void addInPositionForbidden() {
+    SearchableSensorValuesList list = makePopulatedList();
+    assertThrows(UnsupportedOperationException.class, () -> {
+      list.add(1, makeSensorValue(10L, 50));
+    });
+  }
+
+  @Test
+  public void addMultipleInPositionForbidden() {
+    SearchableSensorValuesList list = makePopulatedList();
+    List<SensorValue> values = new ArrayList<SensorValue>();
+    assertThrows(UnsupportedOperationException.class, () -> {
+      list.addAll(1, values);
+    });
+  }
+
+  @Test
+  public void addMultiple() {
+    SearchableSensorValuesList list = makePopulatedList();
+    List<SensorValue> values = new ArrayList<SensorValue>();
+    values.add(makeSensorValue(10L, 0));
+    values.add(makeSensorValue(11L, 50));
+    values.add(makeSensorValue(12L, 32));
+    list.addAll(values);
+    assertTrue(listIsOrdered(list));
   }
 }
