ff8cba267cf452b74246e738caf02999d2eb1bb8
Steve Jones <steve@squaregoldfish.co.uk>
2024-02-09 19:28:46 +0100

    (#2813) Add run types

--- WebApp/junit/resources/sql/testbase/DataReduction/base/V1000000__data_reduction_base.sql
+++ WebApp/junit/resources/sql/testbase/DataReduction/base/V1000000__data_reduction_base.sql
@@ -66,27 +66,45 @@ INSERT INTO file_definition
    '{"valueColumn":-1,"hemisphereColumn":-1,"format":-1}',
    '{"assignments":{"0":{"assignmentIndex":0,"column":0,"properties":{"formatString":"yyyy-MM-dd HH:mm:ss.SSS"}},"1":{"assignmentIndex":1,"column":-1,"properties":{}},"2":{"assignmentIndex":2,"column":-1,"properties":{}},"3":{"assignmentIndex":3,"column":-1,"properties":{}},"4":{"assignmentIndex":4,"column":-1,"properties":{}},"5":{"assignmentIndex":5,"column":-1,"properties":{}},"6":{"assignmentIndex":6,"column":-1,"properties":{}},"7":{"assignmentIndex":7,"column":-1,"properties":{}},"8":{"assignmentIndex":8,"column":-1,"properties":{}},"9":{"assignmentIndex":9,"column":-1,"properties":{}},"10":{"assignmentIndex":10,"column":-1,"properties":{}},"11":{"assignmentIndex":11,"column":-1,"properties":{}},"12":{"assignmentIndex":12,"column":-1,"properties":{}}},"fileHasHeader":false}'
   );
+  
+-- Run Types
+-- Two different run types refer to our variable - the other is a calibration
+-- The calibration is used for diagnostic flagging tests (no calibration performed)
+INSERT INTO run_type
+  (file_definition_id, run_name, category_code)
+  VALUES (1, 'var_1', 1000000);
+
+INSERT INTO run_type
+  (file_definition_id, run_name, category_code)
+  VALUES (1, 'var_2', 1000000);
+
+INSERT INTO run_type
+  (file_definition_id, run_name, category_code)
+  VALUES (1, 'calib', -3);
 
 -- File columns
 INSERT INTO file_column
   (id, file_definition_id, file_column, primary_sensor, sensor_type, sensor_name)
+  VALUES (1, 1, 3, 1, -1, 'Run Type');
+
+INSERT INTO file_column
+  (id, file_definition_id, file_column, primary_sensor, sensor_type, sensor_name)
   VALUES
-  (1, 1, 3, 1,
+  (2, 1, 3, 1,
    (SELECT id FROM sensor_types WHERE name = 'Intake Temperature'),
    'SST');
 
 INSERT INTO file_column
   (id, file_definition_id, file_column, primary_sensor, sensor_type, sensor_name)
   VALUES
-  (2, 1, 4, 1,
+  (3, 1, 4, 1,
    (SELECT id FROM sensor_types WHERE name = 'Salinity'),
    'SAL');
-
 
 INSERT INTO file_column
   (id, file_definition_id, file_column, primary_sensor, sensor_type, sensor_name)
   VALUES
-  (3, 1, 5, 1,
+  (4, 1, 5, 1,
    (SELECT id FROM sensor_types WHERE name = 'xCO₂ (wet, no standards)'),
    'CO2');
 
--- WebApp/junit/resources/sql/testbase/DataReduction/singleMeasurement/V1000001__single_measurement.sql
+++ WebApp/junit/resources/sql/testbase/DataReduction/singleMeasurement/V1000001__single_measurement.sql
@@ -1,19 +1,25 @@
 -- Create sensor values and a measurement all for a single timestamp
 
+-- Run Type
+INSERT INTO sensor_values
+  (id, dataset_id, file_column, date, value, auto_qc, user_qc_flag, user_qc_message)
+  VALUES
+  (1, 1, 1, 1704067200000, 'var_1', NULL, -2, NULL);
+
 -- SST value
 INSERT INTO sensor_values
   (id, dataset_id, file_column, date, value, auto_qc, user_qc_flag, user_qc_message)
   VALUES
-  (1, 1, 1, 1704067200000, '20', NULL, -2, NULL);
+  (2, 1, 2, 1704067200000, '20', NULL, -2, NULL);
   
 -- Salinity value
 INSERT INTO sensor_values
   (id, dataset_id, file_column, date, value, auto_qc, user_qc_flag, user_qc_message)
   VALUES
-  (2, 1, 2, 1704067200000, '35', NULL, -2, NULL);
+  (3, 1, 3, 1704067200000, '35', NULL, -2, NULL);
 
 -- CO2 value
 INSERT INTO sensor_values
   (id, dataset_id, file_column, date, value, auto_qc, user_qc_flag, user_qc_message)
   VALUES
-  (3, 1, 3, 1704067200000, '375', NULL, -2, NULL);
+  (4, 1, 4, 1704067200000, '375', NULL, -2, NULL);
