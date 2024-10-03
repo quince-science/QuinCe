7a9113a7f6266615590c988f137d93f84304dc7c
Steve Jones <steve@squaregoldfish.co.uk>
2024-10-03 18:00:19 +0200

    (#1884) First multi-edit tests

--- WebApp/junit/uk/ac/exeter/QuinCe/web/Instrument/ExternalStandardsBeanTest.java
+++ WebApp/junit/uk/ac/exeter/QuinCe/web/Instrument/ExternalStandardsBeanTest.java
@@ -1,24 +1,17 @@
 package uk.ac.exeter.QuinCe.web.Instrument;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 import org.flywaydb.test.annotation.FlywayTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
 
-import uk.ac.exeter.QuinCe.TestBase.TestSetLine;
-import uk.ac.exeter.QuinCe.TestBase.TestSetTest;
+import uk.ac.exeter.QuinCe.TestBase.BaseTest;
 
 /**
  * Tests of calibration edits for the {@link ExternalStandardsBean}.
@@ -29,21 +22,7 @@ import uk.ac.exeter.QuinCe.TestBase.TestSetTest;
  * this test file).
  */
 @TestInstance(Lifecycle.PER_CLASS)
-public class ExternalStandardsBeanTest extends TestSetTest {
-
-  private static final int ACTION_COL = 0;
-
-  private static final int CALIBRATION_ID_COL = 1;
-
-  private static final int MONTH_COL = 2;
-
-  private static final int TARGET_COL = 3;
-
-  private static final int CHANGE_VALUE_COL = 4;
-
-  private static final int AFFECTED_DATASETS_COL = 5;
-
-  private static final int CAN_REPROCESS_COL = 6;
+public class ExternalStandardsBeanTest extends BaseTest {
 
   private static final long USER_ID = 1L;
 
@@ -53,9 +32,9 @@ public class ExternalStandardsBeanTest extends TestSetTest {
 
   private static final String TARGET_2 = "std2";
 
-  private static final String REPLACEMENT_VALUE = "1000";
+  protected static final String REPLACEMENT_VALUE = "1000";
 
-  private ExternalStandardsBean init() throws Exception {
+  protected ExternalStandardsBean init() throws Exception {
     initResourceManager();
     loginUser(USER_ID);
     ExternalStandardsBean bean = new ExternalStandardsBean();
@@ -64,104 +43,11 @@ public class ExternalStandardsBeanTest extends TestSetTest {
     return bean;
   }
 
-  private Map<String, String> makeCoefficients(String value) {
+  protected static Map<String, String> makeCoefficients(String value) {
     Map<String, String> result = new HashMap<String, String>();
     result.put("xH₂O (with standards)", "0");
     result.put("xCO₂ (with standards)", value);
     return result;
-  }
-
-  /**
-   * <b>NB:</b> The expected results in the {@link TestSet} file must be in
-   * ascending Dataset ID order.
-   *
-   * @param line
-   * @throws Exception
-   */
-  @FlywayTest(locationsForMigrate = { "resources/sql/testbase/user",
-    "resources/sql/testbase/instrument", "resources/sql/testbase/variable",
-    "resources/sql/web/Instrument/CalibrationBeanTest/base",
-    "resources/sql/web/Instrument/CalibrationBeanTest/externalStandardsEdit" })
-  @ParameterizedTest
-  @MethodSource("getLines")
-  public void singleCalibrationEditTest(TestSetLine line) throws Exception {
-
-    // Initialise bean
-    ExternalStandardsBean bean = init();
-
-    int action = getAction(line);
-    long calbrationId = line.getLongField(CALIBRATION_ID_COL);
-    LocalDateTime calibrationTime = getCalibrationTime(line);
-    String target = line.getStringField(TARGET_COL, true);
-    boolean changeValue = line.getBooleanField(CHANGE_VALUE_COL);
-    List<Long> affectedDatasetIds = line
-      .getLongListField(AFFECTED_DATASETS_COL);
-    List<Boolean> canReprocessDatasets = line
-      .getBooleanListField(CAN_REPROCESS_COL);
-
-    if (affectedDatasetIds.size() != canReprocessDatasets.size()) {
-      throw new IllegalArgumentException(
-        "Affected Datasets and Can Reprocess lists are different sizes");
-    }
-
-    bean.setAction(action);
-    if (action == CalibrationEdit.ADD) {
-      bean.newCalibration();
-    } else {
-      bean.setSelectedCalibrationId(calbrationId);
-      bean.loadSelectedCalibration();
-    }
-
-    if (action != CalibrationEdit.DELETE) {
-      bean.getEditedCalibration().setDeploymentDate(calibrationTime);
-      bean.getEditedCalibration().setTarget(target);
-
-      if (changeValue || action == CalibrationEdit.ADD) {
-        bean.getEditedCalibration()
-          .setCoefficients(makeCoefficients(REPLACEMENT_VALUE));
-      }
-    }
-
-    bean.saveCalibration();
-    assertTrue(bean.editedCalibrationValid());
-
-    TreeMap<Long, Boolean> affectedDatasets = bean.getAffectedDatasets();
-
-    assertEquals(affectedDatasetIds.size(), affectedDatasets.size(),
-      "Incorrect number of affected datasets");
-
-    String affectedDatasetsTestString = makeTestString(
-      bean.getAffectedDatasets());
-    String expectedDatasetsTestString = makeTestString(affectedDatasetIds,
-      canReprocessDatasets);
-
-    assertEquals(expectedDatasetsTestString, affectedDatasetsTestString);
-  }
-
-  private String makeTestString(TreeMap<Long, Boolean> input) {
-    StringBuilder result = new StringBuilder();
-
-    for (Map.Entry<Long, Boolean> entry : input.entrySet()) {
-      result.append(entry.getKey());
-      result.append(':');
-      result.append(entry.getValue());
-      result.append(';');
-    }
-
-    return result.toString();
-  }
-
-  private String makeTestString(List<Long> ids, List<Boolean> canReprocess) {
-    StringBuilder result = new StringBuilder();
-
-    for (int i = 0; i < ids.size(); i++) {
-      result.append(ids.get(i));
-      result.append(':');
-      result.append(canReprocess.get(i));
-      result.append(';');
-    }
-
-    return result.toString();
   }
 
   @FlywayTest(locationsForMigrate = { "resources/sql/testbase/user",
@@ -231,41 +117,5 @@ public class ExternalStandardsBeanTest extends TestSetTest {
 
     bean.saveCalibration();
     assertFalse(bean.editedCalibrationValid());
-  }
-
-  @Override
-  protected String getTestSetName() {
-    return "ExternalStandardsSingleCalibrationEditTest";
-  }
-
-  private int getAction(TestSetLine line) {
-    int result;
-
-    switch (line.getStringField(ACTION_COL, false)) {
-    case "ADD": {
-      result = CalibrationEdit.ADD;
-      break;
-    }
-    case "EDIT": {
-      result = CalibrationEdit.EDIT;
-      break;
-    }
-    case "DELETE": {
-      result = CalibrationEdit.DELETE;
-      break;
-    }
-    default: {
-      throw new IllegalArgumentException(
-        "Invalid action '" + line.getStringField(ACTION_COL, false));
-    }
-    }
-
-    return result;
-  }
-
-  private LocalDateTime getCalibrationTime(TestSetLine line) {
-    int month = line.getIntField(MONTH_COL);
-
-    return month == 0 ? null : LocalDateTime.of(2023, month, 1, 0, 0, 0);
   }
 }
