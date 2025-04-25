76f7294f5996a1581e56c03d1be3e64f48cfeb49
Steve Jones <steve@squaregoldfish.co.uk>
2025-04-25 09:04:57 +0200

    (----) Script to find which instrument a data file belongs to

---
+++ scripts/database_maintenance/find_file_usage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+################################################################################
+#
+# This script deletes a data set and all related records from the database
+#
+# The script takes the database url, username and password as parameters
+# It will prompt the user for the ID of the data set to be deleted
+#
+################################################################################
+
+db_name="$(scripts/get_setup_property.sh db_database)"
+db_user="$(scripts/get_setup_property.sh db_username)"
+db_password="$(scripts/get_setup_property.sh db_password)"
+filestore_folder="$(scripts/get_setup_property.sh filestore_folder)"
+
+filename=$1
+
+mysql -u $db_user -p"$db_password" $db_name <<EOF
+	SELECT i.id, i.platform_name, i.name FROM instrument i
+	  INNER JOIN file_definition fd ON fd.instrument_id = i.id
+	  INNER JOIN data_file df ON df.file_definition_id = fd.id
+	  WHERE df.filename = '${filename}';
+EOF
