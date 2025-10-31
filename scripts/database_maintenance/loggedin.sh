6ccec0018f7c3752af79fa25df783551e1899634
Steve Jones <steve@squaregoldfish.co.uk>
2025-10-31 14:54:51 +0100

    (#3200) Spotless

--- scripts/database_maintenance/loggedin.sh
+++ scripts/database_maintenance/loggedin.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 mysql --user=quince --password=quince quince -e "SET time_zone='+0:00';SELECT id,firstname,surname,FROM_UNIXTIME(last_login/1000) FROM user ORDER BY last_login DESC limit 10"
-
