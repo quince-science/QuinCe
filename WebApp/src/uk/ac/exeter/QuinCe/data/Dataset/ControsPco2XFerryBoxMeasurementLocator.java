cb441151c8c4177b3fbc61579adcbc50ecb8863b
Steve Jones <steve@squaregoldfish.co.uk>
2026-02-12 14:55:20 +0100

    (#3216) Spotless

--- WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/ControsPco2XFerryBoxMeasurementLocator.java
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/ControsPco2XFerryBoxMeasurementLocator.java
@@ -1,7 +1,7 @@
 package uk.ac.exeter.QuinCe.data.Dataset;
 
 public class ControsPco2XFerryBoxMeasurementLocator extends ControsPco2MeasurementLocator {
-  
+
   @Override
   protected String getVariableName() {
     return "CONTROS pCO₂ via FerryBox";
