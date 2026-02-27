319823dca67fd6b56c0da950ca26964f560585c0
Steve Jones <steve@squaregoldfish.co.uk>
2026-02-27 10:47:46 +0100

    (#3199) Get database config dynamically

--- scripts/database_maintenance/loggedin.sh
+++ scripts/database_maintenance/loggedin.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-mysql --user=quince --password=quince quince -e "SET time_zone='+0:00';SELECT id,firstname,surname,FROM_UNIXTIME(last_login/1000) FROM user ORDER BY last_login DESC limit 10"
+db_name="$(scripts/get_setup_property.sh db_database)"
+db_user="$(scripts/get_setup_property.sh db_username)"
+db_password="$(scripts/get_setup_property.sh db_password)"
+
+mysql --user="$db_user" --password="$db_password" "$db_name" -e "SET time_zone='+0:00';SELECT id,firstname,surname,FROM_UNIXTIME(last_login/1000) FROM user ORDER BY last_login DESC limit 10"
