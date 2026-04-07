cfc77e363d10728c235df217b30877619012db4f
Steve Jones <steve@squaregoldfish.co.uk>
2026-04-07 15:12:47 +0200

    (#3216) Resolve clashes between reducer classes

--- WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/ControsPco2XFerryBoxReducer.java
+++ WebApp/src/uk/ac/exeter/QuinCe/data/Dataset/DataReduction/ControsPco2XFerryBoxReducer.java
@@ -1,5 +1,7 @@
 package uk.ac.exeter.QuinCe.data.Dataset.DataReduction;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -11,6 +13,8 @@ import uk.ac.exeter.QuinCe.data.Instrument.SensorDefinition.SensorTypeNotFoundEx
 import uk.ac.exeter.QuinCe.data.Instrument.SensorDefinition.Variable;
 
 public class ControsPco2XFerryBoxReducer extends ControsPco2Reducer {
+
+  private static List<CalculationParameter> calculationParameters = null;
 
   Double priorBeam = null;
   Double priorRuntime = null;
@@ -50,5 +54,38 @@ public class ControsPco2XFerryBoxReducer extends ControsPco2Reducer {
     return Calculators.interpolate(priorRuntime, priorBeam, postRuntime,
       postBeam,
       measurement.getMeasurementValue("Runtime").getCalculatedValue());
+  }
+
+  @Override
+  public List<CalculationParameter> getCalculationParameters() {
+    /*
+     * This is a direct copy of the parent method. However, this is required to
+     * enable use of the static calculationParameters. Otherwise the two classes
+     * fight with each other.
+     */
+
+    if (null == calculationParameters) {
+      calculationParameters = new ArrayList<CalculationParameter>(8);
+
+      calculationParameters.add(new CalculationParameter(makeParameterId(0),
+        "Zero S₂beam", "Interpolated Zero Signal", "CONZERO2BEAM", "", false));
+
+      calculationParameters.add(new CalculationParameter(makeParameterId(1),
+        "S₂beam", "Two-beam Signal", "CON2BEAM", "", false));
+
+      calculationParameters.add(new CalculationParameter(makeParameterId(2),
+        "Sproc", "Drift-corrected Signal", "CONSPROC", "", false));
+
+      calculationParameters.add(new CalculationParameter(makeParameterId(3),
+        "xCO₂", "xCO₂ In Water", "XCO2WBDY", "μmol/mol", false));
+
+      calculationParameters.add(new CalculationParameter(makeParameterId(4),
+        "pCO₂ SST", "pCO₂ In Water", "PCO2TK02", "μatm", true));
+
+      calculationParameters.add(new CalculationParameter(makeParameterId(5),
+        "fCO₂", "fCO₂ In Water", "FCO2XXXX", "μatm", true));
+    }
+
+    return calculationParameters;
   }
 }
